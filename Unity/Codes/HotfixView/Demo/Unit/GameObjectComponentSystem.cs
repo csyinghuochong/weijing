981c7992f481673390f7ea208e31a947bf2554f3
tangchunguang <394266315@qq.com>
2022-09-14 17:06:51 +0800

    no message

--- Unity/Codes/HotfixView/Demo/Unit/GameObjectComponentSystem.cs
+++ Unity/Codes/HotfixView/Demo/Unit/GameObjectComponentSystem.cs
@@ -77,6 +77,7 @@ namespace ET
                 if (self.BaiTan != null)
                 {
                     GameObjectPool.Instance.InternalPut(ABPathHelper.GetUnitPath("Player/BaiTan"), self.BaiTan);
+                    self.BaiTan.SetActive(false);
                     self.BaiTan = null;
                 }
                 self.GameObject.transform.Find("BaseModel").gameObject.SetActive(true);
