e912493410cf166fc1f3ed308f8eb12a5441c25f
tangchunguang <394266315@qq.com>
2022-09-20 19:33:28 +0800

    boss特效

---
+++ Unity/Assets/Mono/Custom/EffectNode.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public enum eUpdateMode
+{
+    NONE,
+    BOTH,
+    POSITION,
+    ROTATION,
+    LINK
+}
+public enum eNodeType
+{
+    StartNode,
+    MainNode,
+    EndNode,
+    SpecialNode,
+    Yujing
+}
+namespace Assets.Scripts.Com.Game.Mono
+{
+    public class EffectNode : MonoBehaviour
+    {
+        public eUpdateMode updateMode;
+        public eNodeType node;
+       
+        public bool activateByAnimationEvent = true;
+        public float activateTime;
+        public float duration = 5;
+
+        public bool isBullet = false;
+        public float flyHeight;
+        public float flySpeed;
+        public int flyMode;
+    }
+}
---
+++ Unity/Assets/Mono/Custom/MonsterActRange.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterActRange : MonoBehaviour {
+
+    public float RangeSize;
+    public GameObject Obj_LightEffect_1;
+
+    //private float timeSum;
+
+	// Use this for initialization
+	void Start () {
+        //Obj_LightEffect_1.GetComponent<Projector>().fieldOfView = RangeSize;
+        Obj_LightEffect_1.GetComponent<Projector>().orthographicSize = RangeSize * 1.2f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        timeSum = timeSum + Time.deltaTime;
+        //超过10秒直接注销
+        if (timeSum >= 10) {
+            Destroy(this.gameObject);
+        }
+        */
+	}
+}
