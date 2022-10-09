320d705a1fc849980a6d7c5f0a9800c6f0712909
tangchunguang <394266315@qq.com>
2022-10-09 12:15:08 +0800

    no message

--- Server/Hotfix/Danger/Handler/Map/Role/C2M_RoleAddPointHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/Role/C2M_RoleAddPointHandler.cs
@@ -9,25 +9,38 @@ namespace ET
     {
 
         protected override async ETTask Run(Unit unit, C2M_RoleAddPointRequest request, M2C_RoleAddPointResponse response, Action reply)
-        { 
-            int totalPoint = 0;
-            for (int i = 0;i < request.PointList.Count; i++)
+        {
+            try
             {
-                totalPoint += request.PointList[i];
+                int totalPoint = 0;
+                for (int i = 0; i < request.PointList.Count; i++)
+                {
+                    totalPoint += request.PointList[i];
+                }
+                int remainPoint = (unit.GetComponent<UserInfoComponent>().UserInfo.Lv - 1) * 5 - totalPoint;
+                if (remainPoint < 0)
+                {
+                    response.Error = ErrorCore.ERR_Error;
+                    reply();
+                    return;
+                }
+
+                NumericComponent numericComponent = unit.GetComponent<NumericComponent>();
+                numericComponent.ApplyValue(NumericType.PointLiLiang, request.PointList[0]);
+                numericComponent.ApplyValue(NumericType.PointZhiLi, request.PointList[1]);
+                numericComponent.ApplyValue(NumericType.PointTiZhi, request.PointList[2]);
+                numericComponent.ApplyValue(NumericType.PointNaiLi, request.PointList[3]);
+                numericComponent.ApplyValue(NumericType.PointMinJie, request.PointList[4]);
+                numericComponent.ApplyValue(NumericType.PointRemain, remainPoint);
+                Function_Fight.GetInstance().UnitUpdateProperty_Base(unit);
+
+                reply();
+                await ETTask.CompletedTask;
             }
-            int remainPoint = (unit.GetComponent<UserInfoComponent>().UserInfo.Lv - 1) * 5 - totalPoint;
-
-            NumericComponent numericComponent = unit.GetComponent<NumericComponent>();
-            numericComponent.ApplyValue( NumericType.PointLiLiang, request.PointList[0]);
-            numericComponent.ApplyValue(NumericType.PointZhiLi, request.PointList[1]);
-            numericComponent.ApplyValue(NumericType.PointTiZhi, request.PointList[2]);
-            numericComponent.ApplyValue(NumericType.PointNaiLi, request.PointList[3]);
-            numericComponent.ApplyValue(NumericType.PointMinJie, request.PointList[4]);
-            numericComponent.ApplyValue(NumericType.PointRemain, remainPoint);
-            Function_Fight.GetInstance().UnitUpdateProperty_Base(unit);
-
-            reply();
-            await ETTask.CompletedTask;
+            catch (Exception ex)
+            {
+                Log.Debug("C2M_RoleAddPointError: " + ex.ToString());
+            }
         }
     }
 }
