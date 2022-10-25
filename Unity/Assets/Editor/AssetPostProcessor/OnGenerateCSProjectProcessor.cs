cc9b7743baeed453fe209f2a69aeb5c222e8cb11
tangchunguang <394266315@qq.com>
2022-10-25 16:35:06 +0800

    no message

--- Unity/Assets/Editor/AssetPostProcessor/OnGenerateCSProjectProcessor.cs
+++ Unity/Assets/Editor/AssetPostProcessor/OnGenerateCSProjectProcessor.cs
@@ -61,48 +61,47 @@ namespace ET
 
         private static string GenerateCustomProject(string path, string content, string codesPath)
         {
-            return "";
-            //XmlDocument doc = new XmlDocument();
-            //doc.LoadXml(content);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(content);
 
-            //var newDoc = doc.Clone() as XmlDocument;
+            var newDoc = doc.Clone() as XmlDocument;
 
-            //var rootNode = newDoc.GetElementsByTagName("Project")[0];
+            var rootNode = newDoc.GetElementsByTagName("Project")[0];
 
-            //var itemGroup = newDoc.CreateElement("ItemGroup", newDoc.DocumentElement.NamespaceURI);
-            //var compile = newDoc.CreateElement("Compile", newDoc.DocumentElement.NamespaceURI);
+            var itemGroup = newDoc.CreateElement("ItemGroup", newDoc.DocumentElement.NamespaceURI);
+            var compile = newDoc.CreateElement("Compile", newDoc.DocumentElement.NamespaceURI);
 
-            //compile.SetAttribute("Include", codesPath);
-            //itemGroup.AppendChild(compile);
+            compile.SetAttribute("Include", codesPath);
+            itemGroup.AppendChild(compile);
 
-            //var projectReference = newDoc.CreateElement("ProjectReference", newDoc.DocumentElement.NamespaceURI);
+            var projectReference = newDoc.CreateElement("ProjectReference", newDoc.DocumentElement.NamespaceURI);
             //projectReference.SetAttribute("Include", @"..\Tools\Analyzer\Analyzer.csproj");
             //projectReference.SetAttribute("OutputItemType", @"Analyzer");
             //projectReference.SetAttribute("ReferenceOutputAssembly", @"false");
 
-            //var project = newDoc.CreateElement("Project", newDoc.DocumentElement.NamespaceURI);
-            //project.InnerText = @"{d1f2986b-b296-4a2d-8f12-be9f470014c3}";
-            //projectReference.AppendChild(project);
+            var project = newDoc.CreateElement("Project", newDoc.DocumentElement.NamespaceURI);
+            project.InnerText = @"{d1f2986b-b296-4a2d-8f12-be9f470014c3}";
+            projectReference.AppendChild(project);
 
-            //var name = newDoc.CreateElement("Name", newDoc.DocumentElement.NamespaceURI);
-            //name.InnerText = "Analyzer";
-            //projectReference.AppendChild(project);
+            var name = newDoc.CreateElement("Name", newDoc.DocumentElement.NamespaceURI);
+            name.InnerText = "Analyzer";
+            projectReference.AppendChild(project);
 
-            //itemGroup.AppendChild(projectReference);
+            itemGroup.AppendChild(projectReference);
 
-            //rootNode.AppendChild(itemGroup);
+            rootNode.AppendChild(itemGroup);
 
-            //using (StringWriter sw = new StringWriter())
-            //{
+            using (StringWriter sw = new StringWriter())
+            {
 
-            //    using (XmlTextWriter tx = new XmlTextWriter(sw))
-            //    {
-            //        tx.Formatting = Formatting.Indented;
-            //        newDoc.WriteTo(tx);
-            //        tx.Flush();
-            //        return sw.GetStringBuilder().ToString();
-            //    }
-            //}
+                using (XmlTextWriter tx = new XmlTextWriter(sw))
+                {
+                    tx.Formatting = Formatting.Indented;
+                    newDoc.WriteTo(tx);
+                    tx.Flush();
+                    return sw.GetStringBuilder().ToString();
+                }
+            }
         }
     }
 }
