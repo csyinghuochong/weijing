d8c333f8ad49bed5dd22d797892947742ed7a7e3
tangchunguang <394266315@qq.com>
2022-11-18 10:45:17 +0800

    坐骑展示

---
+++ Unity/Assets/HotfixView/Danger/UI/Common/UIModelDynamicComponent.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ET
+{
+    public class UIModelDynamicComponent : Entity, IAwake, IAwake<GameObject>
+    {
+        public GameObject Model;
+        public Transform ModelParent;
+        public Camera Camera;
+        public GameObject GameObject;
+
+        public Vector2 StartPosition;
+        public Action ClickHandler;
+        public bool draged = false;
+    }
+
+    [ObjectSystem]
+    public class UIModelDynamicComponentAwake : AwakeSystem<UIModelDynamicComponent, GameObject>
+    {
+        public override void Awake(UIModelDynamicComponent self, GameObject gameObject)
+        {
+            self.draged = false;
+            self.Model = null;
+            self.ClickHandler = null;
+            self.GameObject = gameObject;   
+            self.ModelParent = gameObject.transform.Find("Model");
+            self.Camera = gameObject.transform.Find("Camera").GetComponent<Camera>();
+        }
+    }
+
+    public static class UIModelDynamicComponentSystem
+    {
+        public static void OnInitUI(this UIModelDynamicComponent self, GameObject parentImage, RenderTexture rc)
+        {
+            UICommonHelper.SetParent(self.GameObject, parentImage);
+            ButtonHelp.AddEventTriggers(parentImage, (PointerEventData pdata) => { self.Draging(pdata); }, EventTriggerType.Drag);
+            ButtonHelp.AddEventTriggers(parentImage, (PointerEventData pdata) => { self.PointerDown(pdata); }, EventTriggerType.PointerDown);
+            ButtonHelp.AddEventTriggers(parentImage, (PointerEventData pdata) => { self.PointerUp(pdata); }, EventTriggerType.PointerUp);
+            self.Camera.targetTexture = rc;
+        }
+
+        public static void ReleaseRenderTexture(this UIModelDynamicComponent self)
+        {
+            self.Camera.targetTexture = null;
+        }
+
+        public static void Draging(this UIModelDynamicComponent self, PointerEventData eventData)
+        {
+            self.draged = true;
+            float eulerY = self.ModelParent.transform.localEulerAngles.y;
+            if (eventData.position.x > self.StartPosition.x)
+                self.ModelParent.transform.localEulerAngles = new Vector3(0f, eulerY - 10, 0f);
+            else
+                self.ModelParent.transform.localEulerAngles = new Vector3(0f, eulerY + 10, 0f);
+
+            self.StartPosition = eventData.position;
+        }
+        public static void PointerUp(this UIModelDynamicComponent self, PointerEventData eventData)
+        {
+            if (self.draged)
+                return;
+            self.ClickHandler?.Invoke();
+        }
+
+        public static void PointerDown(this UIModelDynamicComponent self, PointerEventData eventData)
+        {
+            self.draged = false;
+            self.StartPosition = eventData.position;
+        }
+
+        public static async ETTask ShowModel(this UIModelDynamicComponent self, string assetPath)
+        {
+            if (self.Model != null)
+            {
+                GameObject.Destroy(self.Model);
+                self.Model = null;
+            }
+            long instanceId = self.InstanceId;
+            var path = ABPathHelper.GetUnitPath(assetPath);
+            GameObject prefab = await ResourcesComponent.Instance.LoadAssetAsync<GameObject>(path);
+            if (instanceId != self.InstanceId)
+            {
+                return;
+            }
+            GameObject go = UnityEngine.Object.Instantiate(prefab, GlobalComponent.Instance.Unit, true);
+            LayerHelp.ChangeLayer(go.transform, LayerEnum.RenderTexture);
+            go.transform.SetParent(self.ModelParent);
+            go.transform.localScale = Vector3.one;
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localEulerAngles = Vector3.zero;
+            self.Model = go;
+        }
+
+        public static void PlayAnimate(this UIModelDynamicComponent self, string animate)
+        {
+            Animator animator = self.Model.GetComponentInChildren<Animator>();
+            if (animator != null)
+            {
+                animator.Play(animate);
+            }
+        }
+    }
+
+}
--- Unity/Assets/HotfixView/Danger/UI/UIJiaYuan/UIJiaYuanZuoQiItemComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIJiaYuan/UIJiaYuanZuoQiItemComponent.cs
@@ -4,13 +4,16 @@ using UnityEngine.UI;
 
 namespace ET
 {
-    public class UIJiaYuanZuoQiItemComponent : Entity, IAwake<GameObject>
+    public class UIJiaYuanZuoQiItemComponent : Entity, IAwake<GameObject>, IDestroy
     {
         public GameObject GameObject;
         public Text Text_Attribute_1;
         public Text TextName;
         public GameObject RawImage;
         public ZuoQiShowConfig ZuoQiConfig;
+
+        public RenderTexture RenderTexture;
+        public UIModelDynamicComponent UIModelShowComponent;
     }
 
     [ObjectSystem]
@@ -26,13 +29,38 @@ namespace ET
         }
     }
 
+    [ObjectSystem]
+    public class UIJiaYuanZuoQiItemComponentDestroy : DestroySystem<UIJiaYuanZuoQiItemComponent>
+    {
+        public override void Destroy(UIJiaYuanZuoQiItemComponent self)
+        {
+            self.UIModelShowComponent.ReleaseRenderTexture();
+            self.RenderTexture.Release();
+            GameObject.Destroy(self.RenderTexture);
+            self.RenderTexture = null;
+            //RenderTexture.ReleaseTemporary(self.RenderTexture);
+        }
+    }
+
     public static class UIJiaYuanZuoQiItemComponentSystem
     {
         public static void OnInitUI(this UIJiaYuanZuoQiItemComponent self, ZuoQiShowConfig zuoQiConfig)
         {
             self.ZuoQiConfig    = zuoQiConfig;
-
             self.TextName.text  = zuoQiConfig.Name;
+            self.RenderTexture  = null;
+            self.RenderTexture  = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+            self.RenderTexture.Create();
+            self.RawImage.GetComponent<RawImage>().texture = self.RenderTexture;
+
+            var path = ABPathHelper.GetUGUIPath("Common/UIModelDynamic");
+            GameObject bundleGameObject =  ResourcesComponent.Instance.LoadAsset<GameObject>(path);
+            GameObject gameObject = UnityEngine.Object.Instantiate(bundleGameObject);
+            self.UIModelShowComponent = self.AddChild<UIModelDynamicComponent, GameObject>(gameObject);
+            self.UIModelShowComponent.OnInitUI(self.RawImage, self.RenderTexture);
+            self.UIModelShowComponent.ShowModel("ZuoQi/" + zuoQiConfig.ModelID).Coroutine();
+            gameObject.transform.Find("Camera").localPosition = new Vector3(0f, 70f, 260f);
+            gameObject.transform.localPosition = new Vector2(zuoQiConfig.Id % 10 * 1000, 0);
         }
     }
 }
