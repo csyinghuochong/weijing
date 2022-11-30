ed57880cec42dbf997c894295be975ea25a7d779
tangchunguang <394266315@qq.com>
2022-11-30 17:14:14 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/GMHelp.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/GMHelp.cs
@@ -12,10 +12,11 @@ namespace ET
             "test01",
             "testcn01",
             "18319670288",
+            "13752404158",
             "wxoyHedvzGgPkynfiq3fNJInZcM76Q",
             "qqUID_E345FB495D13CA8792AA967F39F35045",//神棍
             "qqUID_423B58B6669878EF00C927A48822C230",   //双剑冰
-            "13752404158",
+            "qqUID_C650A5945C70607035C21BB839AC10CB",
         };
 
         public static List<long> BanChatPlayer = new List<long>()
