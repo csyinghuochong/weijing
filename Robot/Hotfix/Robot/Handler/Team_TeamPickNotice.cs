88e6b8ee7b1eee9f50e8b181e846cbedf56d2396
tangchunguang <394266315@qq.com>
2022-12-02 15:29:08 +0800

    机器人 1.当玩家有拾取道具时,则参加全部道具的R点 2.玩家拾取的道具弹出R点窗口时，机器人全部参与R点 3.全部道具包含紫色以上品质全部R

---
+++ Robot/Hotfix/Robot/Handler/Team_TeamPickNotice.cs
@@ -0,0 +1,13 @@
+﻿namespace ET
+{
+    public class Team_TeamPickNotice : AEventClass<EventType.TeamPickNotice>
+    {
+        protected override void Run(object cls)
+        {
+            EventType.TeamPickNotice args = (EventType.TeamPickNotice)cls;
+            DropInfo dropInfo = args.m2C_TeamPickMessage.DropItems[0];
+            C2M_TeamPickRequest request = new C2M_TeamPickRequest() { DropItem = dropInfo, Need = 1 };
+            args.ZoneScene.GetComponent<SessionComponent>().Session.Send(request);
+        }
+    }
+}
