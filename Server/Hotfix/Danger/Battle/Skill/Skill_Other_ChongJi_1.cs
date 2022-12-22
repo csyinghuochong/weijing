7e412b2136610b4bd1e47d70a1f429bd9719fa6d
tangchunguang <394266315@qq.com>
2022-12-22 18:02:59 +0800

    no message

--- Server/Hotfix/Danger/Battle/Skill/Skill_Other_ChongJi_1.cs
+++ Server/Hotfix/Danger/Battle/Skill/Skill_Other_ChongJi_1.cs
@@ -18,7 +18,9 @@ namespace ET
             //1-10 表示 10%-100%
             double addPro = (double)theUnitFrom.GetComponent<NumericComponent>().GetAsInt(NumericType.Now_JumpDisAdd) / 10;
             float newSpeed = (float)(this.SkillConf.SkillMoveSpeed * (1 + addPro));
-            this.AddSpeed_Mul = (newSpeed - oldSpeed) / oldSpeed - theUnitFrom.GetComponent<NumericComponent>().GetAsFloat(NumericType.Extra_Buff_Speed_Mul);
+            float addMul = (newSpeed - oldSpeed) / oldSpeed;
+            float curMul = theUnitFrom.GetComponent<NumericComponent>().GetAsFloat(NumericType.Extra_Buff_Speed_Mul);
+            this.AddSpeed_Mul = addMul - curMul;
             theUnitFrom.GetComponent<NumericComponent>().Set(NumericType.Extra_Buff_Speed_Mul, this.AddSpeed_Mul);
             Unit targetUnit = theUnitFrom.GetParent<UnitComponent>().Get(skillId.TargetID);
             if (targetUnit!=null)
@@ -59,7 +61,8 @@ namespace ET
 
         public override void OnFinished()
         {
-            float speed_mul = TheUnitFrom.GetComponent<NumericComponent>().GetAsFloat(NumericType.Extra_Buff_Speed_Mul) - this.AddSpeed_Mul;
+            float cur_mul = TheUnitFrom.GetComponent<NumericComponent>().GetAsFloat(NumericType.Extra_Buff_Speed_Mul);
+            float speed_mul = cur_mul - this.AddSpeed_Mul;
             TheUnitFrom.GetComponent<NumericComponent>().Set(NumericType.Extra_Buff_Speed_Mul, Mathf.Max(0, speed_mul));
         }
     }
