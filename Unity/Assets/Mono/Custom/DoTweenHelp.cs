7209491f05f4b695bd2925fa2add1efeac032ca2
tangchunguang <394266315@qq.com>
2022-12-22 19:33:46 +0800

    no message

--- Unity/Assets/Mono/Custom/DoTweenHelp.cs
+++ Unity/Assets/Mono/Custom/DoTweenHelp.cs
@@ -6,15 +6,15 @@ namespace ET
     public static class DoTweenHelp
     {
 
-        //public static void DOScale(Transform transform, Vector3 vector3, float time)
-        //{
-        //    //transform.DOScale(new Vector3(1f, 1f, 1f), time / 2).SetEase(Ease.OutCubic).SetDelay(time / 2);
-        //    transform.DOScale(vector3, time).SetEase(Ease.OutCubic);  //.SetDelay(time / 2);
-        //}
+        public static void DOScale(Transform transform, Vector3 vector3, float time)
+        {
+            //transform.DOScale(new Vector3(1f, 1f, 1f), time / 2).SetEase(Ease.OutCubic).SetDelay(time / 2);
+            transform.DOScale(vector3, time).SetEase(Ease.OutCubic);  //.SetDelay(time / 2);
+        }
 
-        //public static void DOMove(Transform transform, Vector3 vector3, float time)
-        //{
-        //    transform.DOMove(vector3, time).SetEase(Ease.OutCubic);
-        //}
+        public static void DOLocalMove(Transform transform, Vector3 vector3, float time)
+        {
+            transform.DOLocalMove(vector3, time).SetEase(Ease.OutCubic);
+        }
     }
 }
