a86c648def27af6d0b7d04b1c557c7d71469c604
tangchunguang <394266315@qq.com>
2022-12-23 18:47:45 +0800

    no message

--- Server/Hotfix/Danger/Handler/Map/PaiMai/C2M_PaiMaiSellHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/PaiMai/C2M_PaiMaiSellHandler.cs
@@ -26,28 +26,36 @@ namespace ET
 				request.PaiMaiItemInfo.SellTime = currentTime;
 
 				//对比出售数量和道具是否匹配
-				if (request.PaiMaiItemInfo.BagInfo.ItemNum <= unit.GetComponent<BagComponent>().GetItemNumber(request.PaiMaiItemInfo.BagInfo.ItemID))
-				{
-					//发送对应拍卖行信息
-					long paimaiServerId = StartSceneConfigCategory.Instance.GetBySceneName(unit.DomainZone(), Enum.GetName(SceneType.PaiMai)).InstanceId;
-					P2M_PaiMaiSellResponse r_GameStatusResponse = (P2M_PaiMaiSellResponse)await ActorMessageSenderComponent.Instance.Call
-						(paimaiServerId, new M2P_PaiMaiSellRequest()
-						{
-							PaiMaiItemInfo = request.PaiMaiItemInfo
-						});
-
-					if (r_GameStatusResponse.Error == ErrorCore.ERR_Success)
-					{
-						//扣除对应道具
-						unit.GetComponent<BagComponent>().OnCostItemData(request.PaiMaiItemInfo.BagInfo.BagInfoID, request.PaiMaiItemInfo.BagInfo.ItemNum);
-						response.PaiMaiItemInfo = request.PaiMaiItemInfo;
-						Log.Info(response.PaiMaiItemInfo.PlayerName + "上架道具：" + request.PaiMaiItemInfo.BagInfo.ItemID + "数量" + request.PaiMaiItemInfo.BagInfo.ItemNum + "时间戳:" + currentTime.ToString());
-					}
-				}
-				else
+				long bagInfoId = request.PaiMaiItemInfo.BagInfo.BagInfoID;
+				BagInfo bagInfo = unit.GetComponent<BagComponent>().GetItemByLoc(ItemLocType.ItemLocBag, bagInfoId);
+				if (bagInfo == null)
 				{
 					response.Error = ErrorCore.ERR_ItemNotEnoughError;      //道具不足
+					reply();
+					return;
 				}
+				if (bagInfo.ItemNum < request.PaiMaiItemInfo.BagInfo.ItemNum)
+				{
+					response.Error = ErrorCore.ERR_ItemNotEnoughError;      //道具不足
+					reply();
+					return;
+				}
+				//发送对应拍卖行信息
+				long paimaiServerId = StartSceneConfigCategory.Instance.GetBySceneName(unit.DomainZone(), Enum.GetName(SceneType.PaiMai)).InstanceId;
+				P2M_PaiMaiSellResponse r_GameStatusResponse = (P2M_PaiMaiSellResponse)await ActorMessageSenderComponent.Instance.Call
+					(paimaiServerId, new M2P_PaiMaiSellRequest()
+					{
+						PaiMaiItemInfo = request.PaiMaiItemInfo
+					});
+
+				if (r_GameStatusResponse.Error == ErrorCore.ERR_Success)
+				{
+					//扣除对应道具
+					unit.GetComponent<BagComponent>().OnCostItemData(request.PaiMaiItemInfo.BagInfo.BagInfoID, request.PaiMaiItemInfo.BagInfo.ItemNum);
+					response.PaiMaiItemInfo = request.PaiMaiItemInfo;
+					Log.Info(response.PaiMaiItemInfo.PlayerName + "上架道具：" + request.PaiMaiItemInfo.BagInfo.ItemID + "数量" + request.PaiMaiItemInfo.BagInfo.ItemNum + "时间戳:" + currentTime.ToString());
+				}
+				response.Error = r_GameStatusResponse.Error;
 				reply();
 				await ETTask.CompletedTask;
 			}
--- Server/Hotfix/Danger/Handler/Map/PaiMai/C2M_PaiMaiXiaJiaHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/PaiMai/C2M_PaiMaiXiaJiaHandler.cs
@@ -9,23 +9,27 @@ namespace ET
     {
         protected override async ETTask Run(Unit unit, C2M_PaiMaiXiaJiaRequest request, M2C_PaiMaiXiaJiaResponse response, Action reply)
         {
-            long chargeServerId = StartSceneConfigCategory.Instance.GetBySceneName(unit.DomainZone(), Enum.GetName(SceneType.PaiMai)).InstanceId;
-            P2M_PaiMaiXiaJiaResponse r_GameStatusResponse = (P2M_PaiMaiXiaJiaResponse)await ActorMessageSenderComponent.Instance.Call
-                (chargeServerId, new M2P_PaiMaiXiaJiaRequest()
-                {
-                    PaiMaiItemInfoId = request.PaiMaiItemInfoId
-                });
+            using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.Sell, unit.Id))
+            {
+                long chargeServerId = StartSceneConfigCategory.Instance.GetBySceneName(unit.DomainZone(), Enum.GetName(SceneType.PaiMai)).InstanceId;
+                P2M_PaiMaiXiaJiaResponse r_GameStatusResponse = (P2M_PaiMaiXiaJiaResponse)await ActorMessageSenderComponent.Instance.Call
+                    (chargeServerId, new M2P_PaiMaiXiaJiaRequest()
+                    {
+                        PaiMaiItemInfoId = request.PaiMaiItemInfoId
+                    });
 
-            if (r_GameStatusResponse.Error == ErrorCore.ERR_Success && r_GameStatusResponse.PaiMaiItemInfo != null)
-            {
-                unit.GetComponent<BagComponent>().OnAddItemData(r_GameStatusResponse.PaiMaiItemInfo.BagInfo, $"{ItemGetWay.PaiMaiShop}_{TimeHelper.ServerNow()}");
+                if (r_GameStatusResponse.Error == ErrorCore.ERR_Success && r_GameStatusResponse.PaiMaiItemInfo != null)
+                {
+                    unit.GetComponent<BagComponent>().OnAddItemData(r_GameStatusResponse.PaiMaiItemInfo.BagInfo, $"{ItemGetWay.PaiMaiShop}_{TimeHelper.ServerNow()}");
+                }
+                else
+                {
+                    Log.Info($"C2M_PaiMaiXiaJiaHandler==null  {unit.Id} {request.PaiMaiItemInfoId}");
+                }
+
+                reply();
+                await ETTask.CompletedTask;
             }
-            else
-            {
-                Log.Info($"C2M_PaiMaiXiaJiaHandler==null  {unit.Id} {request.PaiMaiItemInfoId}");
-            }
-            
-            reply();
         }
     }
 }
