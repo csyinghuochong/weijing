24ca84b84b9f9287738be64b4e525f86f07a9c58
tangchunguang <394266315@qq.com>
2023-01-12 13:06:08 +0800

    no message

--- Unity/Assets/HotfixView/Danger/Help/TapSDKHelper.cs
+++ Unity/Assets/HotfixView/Danger/Help/TapSDKHelper.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+#if UNITY_ANDROID
 using TapTap.Bootstrap;
 using TapTap.Common;
 using TapTap.TapDB;
+#endif
 
 namespace ET
 {
