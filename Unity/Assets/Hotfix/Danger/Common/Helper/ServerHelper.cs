2d3cf04fa16885482fa81951849814bb7b11245c
tangchunguang <394266315@qq.com>
2023-02-02 15:42:01 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -61,19 +61,22 @@ namespace ET
                 else
                 {
                     serverItems_1.Add(new ServerItem() { ServerId = 1, ServerIp = "127.0.0.1:20305", ServerName = "封测一区", ServerOpenTime = 1662189906681, Show = 0 });
+
                     serverItems_1.Add(new ServerItem() { ServerId = 2, ServerIp = "127.0.0.1:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 0 });
                     serverItems_1.Add(new ServerItem() { ServerId = 3, ServerIp = "127.0.0.1:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 4, ServerIp = "127.0.0.1:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 0 });
+                    
                     serverItems_1.Add(new ServerItem() { ServerId = 5, ServerIp = "127.0.0.1:20335", ServerName = "先锋一区", ServerOpenTime = 1669435366360, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 6, ServerIp = "127.0.0.1:20335", ServerName = "先锋二区", ServerOpenTime = 1669435366360, Show = 1 });
-                    serverItems_1.Add(new ServerItem() { ServerId = 7, ServerIp = "127.0.0.1:20355", ServerName = "先锋三区", ServerOpenTime = 1670580000000, Show = 1 });
-                    serverItems_1.Add(new ServerItem() { ServerId = 8, ServerIp = "127.0.0.1:20355", ServerName = "先锋四区", ServerOpenTime = 1671188400000, Show = 1 });
+                    serverItems_1.Add(new ServerItem() { ServerId = 7, ServerIp = "127.0.0.1:20335", ServerName = "先锋三区", ServerOpenTime = 1669435366360, Show = 1 });
+                    serverItems_1.Add(new ServerItem() { ServerId = 8, ServerIp = "127.0.0.1:20335", ServerName = "先锋四区", ServerOpenTime = 1669435366360, Show = 1 });
+
                     serverItems_1.Add(new ServerItem() { ServerId = 9, ServerIp = "127.0.0.1:20375", ServerName = "先锋五区", ServerOpenTime = 1671793200000, Show = 1 });
-                    serverItems_1.Add(new ServerItem() { ServerId = 10, ServerIp = "127.0.0.1:20385", ServerName = "先锋六区", ServerOpenTime = 1672398000000, Show = 1 });
+                    serverItems_1.Add(new ServerItem() { ServerId = 10, ServerIp = "127.0.0.1:20375", ServerName = "先锋六区", ServerOpenTime = 1671793200000, Show = 1 });
+
                     serverItems_1.Add(new ServerItem() { ServerId = 11, ServerIp = "127.0.0.1:20395", ServerName = "先锋七区", ServerOpenTime = 1673002800000, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 12, ServerIp = "127.0.0.1:20405", ServerName = "兔年大吉", ServerOpenTime = 1674212400000, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 13, ServerIp = "127.0.0.1:20415", ServerName = "玉兔新春", ServerOpenTime = 1674903600000, Show = 1 });
-                    
                     serverItems_1.Add(new ServerItem() { ServerId = 14, ServerIp = "127.0.0.1:20425", ServerName = "先锋十区", ServerOpenTime = 0, Show = 0 });
                 }
             }
@@ -86,19 +89,22 @@ namespace ET
                 else
                 {
                     serverItems_1.Add(new ServerItem() { ServerId = 1, ServerIp = "39.96.194.143:20305", ServerName = "封测一区", ServerOpenTime = 1662189906681,Show = 0 });
+                    
                     serverItems_1.Add(new ServerItem() { ServerId = 2, ServerIp = "39.96.194.143:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 0 });
                     serverItems_1.Add(new ServerItem() { ServerId = 3, ServerIp = "39.96.194.143:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 4, ServerIp = "39.96.194.143:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 0 });
+                    
                     serverItems_1.Add(new ServerItem() { ServerId = 5, ServerIp = "39.96.194.143:20335", ServerName = "先锋一区", ServerOpenTime = 1669435366360, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 6, ServerIp = "39.96.194.143:20335", ServerName = "先锋二区", ServerOpenTime = 1669435366360, Show = 1 });
-                    serverItems_1.Add(new ServerItem() { ServerId = 7, ServerIp = "39.96.194.143:20355", ServerName = "先锋三区", ServerOpenTime = 1670580000000, Show = 1 });
-                    serverItems_1.Add(new ServerItem() { ServerId = 8, ServerIp = "39.96.194.143:20355", ServerName = "先锋四区", ServerOpenTime = 1671188400000, Show = 1 });
+                    serverItems_1.Add(new ServerItem() { ServerId = 7, ServerIp = "39.96.194.143:20335", ServerName = "先锋三区", ServerOpenTime = 1669435366360, Show = 1 });
+                    serverItems_1.Add(new ServerItem() { ServerId = 8, ServerIp = "39.96.194.143:20335", ServerName = "先锋四区", ServerOpenTime = 1669435366360, Show = 1 });
+                    
                     serverItems_1.Add(new ServerItem() { ServerId = 9, ServerIp = "39.96.194.143:20375", ServerName = "先锋五区", ServerOpenTime = 1671793200000, Show = 1 });
-                    serverItems_1.Add(new ServerItem() { ServerId = 10, ServerIp = "39.96.194.143:20385", ServerName = "先锋六区", ServerOpenTime = 1672398000000, Show = 1 });
+                    serverItems_1.Add(new ServerItem() { ServerId = 10, ServerIp = "39.96.194.143:20375", ServerName = "先锋六区", ServerOpenTime = 1671793200000, Show = 1 });
+
                     serverItems_1.Add(new ServerItem() { ServerId = 11, ServerIp = "39.96.194.143:20395", ServerName = "先锋七区", ServerOpenTime = 1673002800000, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 12, ServerIp = "39.96.194.143:20405", ServerName = "兔年大吉", ServerOpenTime = 1674212400000, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 13, ServerIp = "39.96.194.143:20415", ServerName = "玉兔新春", ServerOpenTime = 1674903600000, Show = 1 });
-                    
                     serverItems_1.Add(new ServerItem() { ServerId = 14, ServerIp = "39.96.194.143:20425", ServerName = "先锋十区", ServerOpenTime = 0, Show = 0 });
                 }
             }
--- start_merge_zone.bat
+++ start_merge_zone.bat
@@ -1,4 +1,4 @@
 ::先备份数据库     要合併的区_新区     在修改物理区
 cd bin
-dotnet Server.dll --AppType=MergeZone --Console=1 --StartConfig=StartConfig/Localhost --Parameters=6_5 --Process 1
+dotnet Server.dll --AppType=MergeZone --Console=1 --StartConfig=StartConfig/Localhost --Parameters=10_9 --Process 1
 pause
