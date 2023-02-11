70e7670f89252bb29cf79ed8d6dca318ec2d3bd0
tangchunguang <394266315@qq.com>
2023-02-11 18:25:52 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIShouJi/UIShouJiComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIShouJi/UIShouJiComponent.cs
@@ -1,15 +1,25 @@
-﻿using UnityEngine;
-using System.Linq;
-using System.Collections.Generic;
+﻿using System;
+using UnityEngine;
+
 
 namespace ET
 {
 
+    public enum ShouJiPageEnum : int
+    {
+        ShouJiList = 0,
+        ShouJiTreasure = 1,
+      
+        Number ,
+    }
+
     public class UIShouJiComponent : Entity, IAwake
     {
-        public GameObject ShoujiContent;
-        public GameObject ScrollView;
+        public GameObject FunctionSetBtn;
+        public GameObject SubViewNode;
 
+        public UIPageViewComponent UIPageView;
+        public UIPageButtonComponent UIPageButtonComponent;
     }
 
     [ObjectSystem]
@@ -19,8 +29,30 @@ namespace ET
         {
             ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
 
-            self.ShoujiContent = rc.Get<GameObject>("ShoujiContent");
-            self.ScrollView = rc.Get<GameObject>("ScrollView");
+            self.FunctionSetBtn = rc.Get<GameObject>("FunctionSetBtn");
+            self.SubViewNode = rc.Get<GameObject>("SubViewNode");
+
+            UI uiPageView = self.AddChild<UI, string, GameObject>("SubViewNode", self.SubViewNode);
+            UIPageViewComponent pageViewComponent = uiPageView.AddComponent<UIPageViewComponent>();
+            pageViewComponent.UISubViewList = new UI[(int)ShouJiPageEnum.Number];
+            pageViewComponent.UISubViewPath = new string[(int)ShouJiPageEnum.Number];
+            pageViewComponent.UISubViewType = new Type[(int)ShouJiPageEnum.Number];
+
+            pageViewComponent.UISubViewPath[(int)ShouJiPageEnum.ShouJiList] = ABPathHelper.GetUGUIPath("Main/ShouJi/UIShouJiList");
+            pageViewComponent.UISubViewPath[(int)ShouJiPageEnum.ShouJiTreasure] = ABPathHelper.GetUGUIPath("Main/ShouJi/UIShouJiTreasure");
+          
+            pageViewComponent.UISubViewType[(int)ShouJiPageEnum.ShouJiList] = typeof(UIShouJiListComponent);
+            pageViewComponent.UISubViewType[(int)ShouJiPageEnum.ShouJiTreasure] = typeof(UIShouJiTreasureComponent);
+            self.UIPageView = pageViewComponent;
+
+            UI uiPageButton = self.AddChild<UI, string, GameObject>("FunctionSetBtn", self.FunctionSetBtn);
+            UIPageButtonComponent uIPageButtonComponent = uiPageButton.AddComponent<UIPageButtonComponent>();
+            uIPageButtonComponent.SetClickHandler((int page) =>
+            {
+                self.OnClickPageButton(page);
+            });
+            self.UIPageButtonComponent = uIPageButtonComponent;
+            self.UIPageButtonComponent.OnSelectIndex(0);
 
             self.OnUpdateUI().Coroutine();
         }
@@ -28,23 +60,14 @@ namespace ET
 
     public static class UIShouJiComponentSystem
     {
+        public static void OnClickPageButton(this UIShouJiComponent self, int page)
+        {
+            self.UIPageView.OnSelectIndex(page).Coroutine();
+        }
+
         public static async ETTask OnUpdateUI(this UIShouJiComponent self)
         {
-            var path = ABPathHelper.GetUGUIPath("Main/ShouJi/UIShouJiChapter");
-            var bundleGameObject = await ResourcesComponent.Instance.LoadAssetAsync<GameObject>(path);
-            List<ShouJiConfig> shouJiConfigs = ShouJiConfigCategory.Instance.GetAll().Values.ToList();
-            long instanceId = self.InstanceId;
-            for (int i = 0; i < shouJiConfigs.Count; i++)
-            {
-                if (instanceId != self.InstanceId)
-                {
-                    return;
-                }
-                GameObject go = GameObject.Instantiate(bundleGameObject);
-                UICommonHelper.SetParent(go, self.ShoujiContent);
-                self.AddChild<UIShouJiChapterComponent, GameObject>(go).OnInitUI(shouJiConfigs[i]).Coroutine();
-                await TimerComponent.Instance.WaitAsync(200);
-            }
+            await ETTask.CompletedTask;
         }
     }
 }
---
+++ Unity/Assets/HotfixView/Danger/UI/UIShouJi/UIShouJiListComponent.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace ET
+{
+    public class UIShouJiListComponent : Entity, IAwake
+    {
+        public GameObject ShoujiContent;
+        public GameObject ScrollView;
+
+    }
+
+    [ObjectSystem]
+    public class UIShouJiListComponentAwake : AwakeSystem<UIShouJiListComponent>
+    {
+        public override void Awake(UIShouJiListComponent self)
+        {
+            ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
+
+            self.ShoujiContent = rc.Get<GameObject>("ShoujiContent");
+            self.ScrollView = rc.Get<GameObject>("ScrollView");
+
+            self.OnUpdateUI().Coroutine();
+        }
+    }
+
+    public static class UIShouJiListComponentSystem
+    {
+        public static async ETTask OnUpdateUI(this UIShouJiListComponent self)
+        {
+            var path = ABPathHelper.GetUGUIPath("Main/ShouJi/UIShouJiChapter");
+            var bundleGameObject = await ResourcesComponent.Instance.LoadAssetAsync<GameObject>(path);
+            List<ShouJiConfig> shouJiConfigs = ShouJiConfigCategory.Instance.GetAll().Values.ToList();
+            long instanceId = self.InstanceId;
+            for (int i = 0; i < shouJiConfigs.Count; i++)
+            {
+                if (instanceId != self.InstanceId)
+                {
+                    return;
+                }
+                GameObject go = GameObject.Instantiate(bundleGameObject);
+                UICommonHelper.SetParent(go, self.ShoujiContent);
+                self.AddChild<UIShouJiChapterComponent, GameObject>(go).OnInitUI(shouJiConfigs[i]).Coroutine();
+                await TimerComponent.Instance.WaitAsync(200);
+            }
+        }
+    }
+}
