b4c3632d99b9177064af3abe9114d3a9d61f4cb6
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2023-02-11 21:17:55 +0800

    111

--- Unity/Assets/Hotfix/Module/Numeric/NumericWatcher_Update.cs
+++ Unity/Assets/Hotfix/Module/Numeric/NumericWatcher_Update.cs
@@ -25,7 +25,7 @@
 
 #if SERVER
 			Unit unit = args.Parent;
-			unit.OnRideHorse(args.OldValue, false);
+			unit.OnRideHorse((int)args.OldValue, false);
 #else
 			EventType.UnitNumericUpdate.Instance.OldValue = args.OldValue;
 			EventType.UnitNumericUpdate.Instance.Unit = args.Parent;
