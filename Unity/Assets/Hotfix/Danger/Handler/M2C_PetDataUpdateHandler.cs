92703390283ff3eb2201eeef094403f35011007c
tangchunguang <394266315@qq.com>
2023-02-11 14:52:09 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Handler/M2C_PetDataUpdateHandler.cs
+++ Unity/Assets/Hotfix/Danger/Handler/M2C_PetDataUpdateHandler.cs
@@ -7,40 +7,40 @@
 
         protected override void Run(Session session, M2C_PetDataUpdate message)
         {
-            if (message.UpdateType == (int)UserDataType.Lv)
+            switch (message.UpdateType)
             {
-                RolePetInfo rolePetInfo = session.ZoneScene().GetComponent<PetComponent>().GetPetInfoByID(message.PetId);
-                if (rolePetInfo != null && rolePetInfo.Id == message.PetId)
-                {
-                    rolePetInfo.AddPropretyNum += (int.Parse(message.UpdateTypeValue) - rolePetInfo.PetLv) * 5;
-                    rolePetInfo.PetLv = int.Parse(message.UpdateTypeValue);
-                }
-            }
-            if (message.UpdateType == (int)UserDataType.Exp)
-            {
-                RolePetInfo rolePetInfo = session.ZoneScene().GetComponent<PetComponent>().GetPetInfoByID(message.PetId);
-                if (rolePetInfo != null && rolePetInfo.Id == message.PetId)
-                {
+                case (int)UserDataType.Lv:
+                    RolePetInfo rolePetInfo = session.ZoneScene().GetComponent<PetComponent>().GetPetInfoByID(message.PetId);
+                    if (rolePetInfo != null && rolePetInfo.Id == message.PetId)
+                    {
+                        rolePetInfo.AddPropretyNum += (int.Parse(message.UpdateTypeValue) - rolePetInfo.PetLv) * 5;
+                        rolePetInfo.PetLv = int.Parse(message.UpdateTypeValue);
+                    }
+                    break;
+                case (int)UserDataType.Exp:
+                    rolePetInfo = session.ZoneScene().GetComponent<PetComponent>().GetPetInfoByID(message.PetId);
+                    if (rolePetInfo != null && rolePetInfo.Id == message.PetId)
+                    {
 
-                    rolePetInfo.PetExp = int.Parse(message.UpdateTypeValue);
-                }
-            }
-            //宠物出战状态
-            if (message.UpdateType == (int)UserDataType.PetStatus)
-            {
-                RolePetInfo rolePetInfo = session.ZoneScene().GetComponent<PetComponent>().GetPetInfoByID(message.PetId);
-                if (rolePetInfo != null)
-                {
-                    rolePetInfo.PetStatus = int.Parse(message.UpdateTypeValue);
-                }
-            }
-            if (message.UpdateType == (int)UserDataType.Name)
-            {
-                RolePetInfo rolePetInfo = session.ZoneScene().GetComponent<PetComponent>().GetPetInfoByID(message.PetId);
-                if (rolePetInfo != null)
-                {
-                    rolePetInfo.PetName = message.UpdateTypeValue;
-                }
+                        rolePetInfo.PetExp = int.Parse(message.UpdateTypeValue);
+                    }
+                    break;
+                case (int)UserDataType.PetStatus:
+                    rolePetInfo = session.ZoneScene().GetComponent<PetComponent>().GetPetInfoByID(message.PetId);
+                    if (rolePetInfo != null)
+                    {
+                        rolePetInfo.PetStatus = int.Parse(message.UpdateTypeValue);
+                    }
+                    break;
+                case (int)UserDataType.Name:
+                     rolePetInfo = session.ZoneScene().GetComponent<PetComponent>().GetPetInfoByID(message.PetId);
+                    if (rolePetInfo != null)
+                    {
+                        rolePetInfo.PetName = message.UpdateTypeValue;
+                    }
+                    break;
+                default:
+                    break;
             }
         }
     }
