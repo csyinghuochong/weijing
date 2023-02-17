f5d9f34c34a48aa85b1272b91b52bf46bc6f829c
tangchunguang <394266315@qq.com>
2023-02-17 18:08:36 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -76,7 +76,8 @@ namespace ET
                     serverItems_1.Add(new ServerItem() { ServerId = 11, ServerIp = "127.0.0.1:20375", ServerName = "先锋七区", ServerOpenTime = 1671793200000, Show = 1 });
                    
                     serverItems_1.Add(new ServerItem() { ServerId = 12, ServerIp = "127.0.0.1:20405", ServerName = "兔年大吉", ServerOpenTime = 1674212400000, Show = 1 });
-                    serverItems_1.Add(new ServerItem() { ServerId = 13, ServerIp = "127.0.0.1:20415", ServerName = "玉兔新春", ServerOpenTime = 1674903600000, Show = 1 });
+                    serverItems_1.Add(new ServerItem() { ServerId = 13, ServerIp = "127.0.0.1:20405", ServerName = "玉兔新春", ServerOpenTime = 1674212400000, Show = 1 });
+                    
                     serverItems_1.Add(new ServerItem() { ServerId = 14, ServerIp = "127.0.0.1:20425", ServerName = "元宵佳节", ServerOpenTime = 1675580400000, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 15, ServerIp = "127.0.0.1:20435", ServerName = "金戈铁马", ServerOpenTime = 1676026800000, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 16, ServerIp = "127.0.0.1:20445", ServerName = "世外桃源", ServerOpenTime = 1676631600000, Show = 0 });
@@ -108,7 +109,8 @@ namespace ET
                     serverItems_1.Add(new ServerItem() { ServerId = 11, ServerIp = "39.96.194.143:20375", ServerName = "先锋七区", ServerOpenTime = 1671793200000, Show = 1 });
                     
                     serverItems_1.Add(new ServerItem() { ServerId = 12, ServerIp = "39.96.194.143:20405", ServerName = "兔年大吉", ServerOpenTime = 1674212400000, Show = 1 });
-                    serverItems_1.Add(new ServerItem() { ServerId = 13, ServerIp = "39.96.194.143:20415", ServerName = "玉兔新春", ServerOpenTime = 1674903600000, Show = 1 });
+                    serverItems_1.Add(new ServerItem() { ServerId = 13, ServerIp = "39.96.194.143:20405", ServerName = "玉兔新春", ServerOpenTime = 1674903600000, Show = 1 });
+                    
                     serverItems_1.Add(new ServerItem() { ServerId = 14, ServerIp = "39.96.194.143:20425", ServerName = "元宵佳节", ServerOpenTime = 1675580400000, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 15, ServerIp = "39.96.194.143:20435", ServerName = "金戈铁马", ServerOpenTime = 1676026800000, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 16, ServerIp = "39.96.194.143:20445", ServerName = "世外桃源", ServerOpenTime = 0, Show = 0 });
