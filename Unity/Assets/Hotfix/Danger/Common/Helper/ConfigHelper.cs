2814989386404322d1f92a10bd8f579db6077105
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2023-02-17 15:09:47 +0800

    更新数据

--- Unity/Assets/Hotfix/Danger/Common/Helper/ConfigHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ConfigHelper.cs
@@ -12,6 +12,7 @@ namespace ET
         //游戏公告
         public static List<WorldSayConfig> WorldSayList = new List<ET.WorldSayConfig>
         {
+            new WorldSayConfig(){ Time = 1230, Conent = "巨龙神已经准时出现在宝藏之地,想要挑战我的就带上你们的武器过来挑战我吧!"  },
             new WorldSayConfig(){ Time = 1930, Conent = "一波红包雨已经来临,赶紧来看看自己是否是那个幸运玩家!"  },
             new WorldSayConfig(){ Time = 2000, Conent = "世界领主已经出现在密境中,赶紧过来看看吧!"  },
             new WorldSayConfig(){ Time = 2030, Conent = "战场活动已经开启,可以通过右上角的战场按钮快速加入哦!"  },
