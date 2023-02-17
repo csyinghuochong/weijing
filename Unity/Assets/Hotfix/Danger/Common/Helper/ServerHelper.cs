a7306d41ab72462f6c567720f2a652b6e76a2172
tangchunguang <394266315@qq.com>
2023-02-17 18:17:44 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -60,8 +60,6 @@ namespace ET
                 }
                 else
                 {
-                    serverItems_1.Add(new ServerItem() { ServerId = 1, ServerIp = "127.0.0.1:20305", ServerName = "封测一区", ServerOpenTime = 1662189906681, Show = 0 });
-
                     serverItems_1.Add(new ServerItem() { ServerId = 2, ServerIp = "127.0.0.1:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 0 });
                     serverItems_1.Add(new ServerItem() { ServerId = 3, ServerIp = "127.0.0.1:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 4, ServerIp = "127.0.0.1:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 0 });
@@ -93,8 +91,6 @@ namespace ET
                 }
                 else
                 {
-                    serverItems_1.Add(new ServerItem() { ServerId = 1, ServerIp = "39.96.194.143:20305", ServerName = "封测一区", ServerOpenTime = 1662189906681,Show = 0 });
-                    
                     serverItems_1.Add(new ServerItem() { ServerId = 2, ServerIp = "39.96.194.143:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 0 });
                     serverItems_1.Add(new ServerItem() { ServerId = 3, ServerIp = "39.96.194.143:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 1 });
                     serverItems_1.Add(new ServerItem() { ServerId = 4, ServerIp = "39.96.194.143:20325", ServerName = "封测区", ServerOpenTime = 1662189906681, Show = 0 });
