e03260dc8ba8647e2e80435926e0c116aa63fce1
tangchunguang <394266315@qq.com>
2023-02-20 15:57:18 +0800

    no message

---
+++ Server/Model/Generate/Config/TitleConfig.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+using ProtoBuf;
+
+namespace ET
+{
+    [ProtoContract]
+    [Config]
+    public partial class TitleConfigCategory : ProtoObject, IMerge
+    {
+        public static TitleConfigCategory Instance;
+		
+        [ProtoIgnore]
+        [BsonIgnore]
+        private Dictionary<int, TitleConfig> dict = new Dictionary<int, TitleConfig>();
+		
+        [BsonElement]
+        [ProtoMember(1)]
+        private List<TitleConfig> list = new List<TitleConfig>();
+		
+        public TitleConfigCategory()
+        {
+            Instance = this;
+        }
+        
+        public void Merge(object o)
+        {
+            TitleConfigCategory s = o as TitleConfigCategory;
+            this.list.AddRange(s.list);
+        }
+		
+        public override void EndInit()
+        {
+            foreach (TitleConfig config in list)
+            {
+                config.EndInit();
+                this.dict.Add(config.Id, config);
+            }            
+            this.AfterEndInit();
+        }
+		
+        public TitleConfig Get(int id)
+        {
+            this.dict.TryGetValue(id, out TitleConfig item);
+
+            if (item == null)
+            {
+                throw new Exception($"配置找不到，配置表名: {nameof (TitleConfig)}，配置id: {id}");
+            }
+
+            return item;
+        }
+		
+        public bool Contain(int id)
+        {
+            return this.dict.ContainsKey(id);
+        }
+
+        public Dictionary<int, TitleConfig> GetAll()
+        {
+            return this.dict;
+        }
+
+        public TitleConfig GetOne()
+        {
+            if (this.dict == null || this.dict.Count <= 0)
+            {
+                return null;
+            }
+            return this.dict.Values.GetEnumerator().Current;
+        }
+    }
+
+    [ProtoContract]
+	public partial class TitleConfig: ProtoObject, IConfig
+	{
+		/// <summary>Id</summary>
+		[ProtoMember(1)]
+		public int Id { get; set; }
+		/// <summary>名称</summary>
+		[ProtoMember(2)]
+		public string Name { get; set; }
+		/// <summary>图标显示</summary>
+		[ProtoMember(3)]
+		public int Icon { get; set; }
+		/// <summary>额外属性</summary>
+		[ProtoMember(4)]
+		public string AddProperty { get; set; }
+		/// <summary>额外属性描述</summary>
+		[ProtoMember(5)]
+		public string Des { get; set; }
+
+	}
+}
