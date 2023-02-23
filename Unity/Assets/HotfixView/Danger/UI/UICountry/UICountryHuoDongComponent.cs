1359f5501cd21a5c16d8c329d27a2b3cf0f5f146
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2023-02-23 12:18:44 +0800

    111

--- Unity/Assets/HotfixView/Danger/UI/UICountry/UICountryHuoDongComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UICountry/UICountryHuoDongComponent.cs
@@ -54,7 +54,7 @@ namespace ET
         {
              UI uI = await UIHelper.Create(self.DomainScene(), UIType.UICountryHuoDongJieShao);
              uI.GetComponent<UICountryHuoDongJieShaoComponent>().OnUpdateJieShao("角斗场规则",
-                "活动暂时没开启\n  活动暂时没开启");
+                " 1.活动开启后,所有12级以上玩家均可进入。\n 2.活动开启10分钟后将禁止所有玩家进入此地图。\n 3.在角斗场内,玩家将互相发起挑战,坚持到最后1名的玩家将会\n 获得丰厚奖励。\n 4.在同一个角斗场内,人数最多达到20人。\n 5.20:00活动结束,如果场内剩余多人,则按照当前最低排名发放奖励。");
         }
 
         public static void On_Btn_HuoDong_Arena(this UICountryHuoDongComponent self)
