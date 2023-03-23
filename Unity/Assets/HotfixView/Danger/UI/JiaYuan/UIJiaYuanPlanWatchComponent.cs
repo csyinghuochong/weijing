98068584d615467cf3a5b77bd962e3ae025bb497
tangchunguang <394266315@qq.com>
2023-03-23 21:21:24 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/JiaYuan/UIJiaYuanPlanWatchComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/JiaYuan/UIJiaYuanPlanWatchComponent.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace ET
 {
     public class UIJiaYuanPlanWatchComponent : Entity, IAwake
     {
+        public GameObject ButtonClose;
+    }
 
+    [ObjectSystem]
+    public class UIJiaYuanPlanWatchComponentAwake : AwakeSystem<UIJiaYuanPlanWatchComponent>
+    {
+        public override void Awake(UIJiaYuanPlanWatchComponent self)
+        {
+            ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
+
+            self.ButtonClose = rc.Get<GameObject>("ButtonClose");
+            self.ButtonClose.GetComponent<Button>().onClick.AddListener(() => { UIHelper.Remove( self.ZoneScene(), UIType.UIJiaYuanPlanWatch ); });
+        }
     }
 }
