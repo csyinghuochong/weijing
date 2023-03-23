038ff78f52cca7da55f2a8bbbb4e509c3006d8a6
tangchunguang <394266315@qq.com>
2023-03-23 21:14:55 +0800

    no message

---
+++ Unity/Assets/HotfixView/Danger/UI/JiaYuan/UIJiaYuanPlanWatchEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace ET
+{
+    [UIEvent(UIType.UIJiaYuanPlanWatch)]
+    public class UIJiaYuanPlanWatchEvent : AUIEvent
+    {
+        public override async ETTask<UI> OnCreate(UIComponent uiComponent)
+        {
+            var path = ABPathHelper.GetUGUIPath(UIType.UIJiaYuanPlanWatch);
+            await ETTask.CompletedTask;
+            var bundleGameObject = ResourcesComponent.Instance.LoadAsset<GameObject>(path);
+            GameObject gameObject = UnityEngine.Object.Instantiate(bundleGameObject);
+            UI ui = uiComponent.AddChild<UI, string, GameObject>(UIType.UIJiaYuanPlanWatch, gameObject);
+            ui.AddComponent<UIJiaYuanPlanWatchComponent>();
+            return ui;
+        }
+
+        public override void OnRemove(UIComponent uiComponent)
+        {
+            var path = ABPathHelper.GetUGUIPath(UIType.UIJiaYuanPlanWatch);
+            ResourcesComponent.Instance.UnLoadAsset(path);
+        }
+    }
+}
