240d619e735628d7170b10eff2a3cd98439ea438
tangchunguang <394266315@qq.com>
2023-03-27 10:35:58 +0800

    no message

---
+++ Server/Hotfix/Demo/AI/AI_Pasture.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ET
+{
+
+    [AIHandler]
+    public class AI_Pasture : AAIHandler
+    {
+        public override bool Check(AIComponent aiComponent, AIConfig aiConfig)
+        {
+            return true;
+        }
+
+        public override async ETTask Execute(AIComponent aiComponent, AIConfig aiConfig, ETCancellationToken cancellationToken)
+        {
+            for (int i = 0; i < 100000; ++i)
+            {
+                bool timeRet = await TimerComponent.Instance.WaitAsync(1000, cancellationToken);
+                if (!timeRet)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
