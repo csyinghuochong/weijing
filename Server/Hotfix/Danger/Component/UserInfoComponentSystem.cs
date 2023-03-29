7470a261c4eee665f9b5dd86c9fc4b82e3c57485
tangchunguang <394266315@qq.com>
2023-03-29 18:13:58 +0800

    no message

--- Server/Hotfix/Danger/Component/UserInfoComponentSystem.cs
+++ Server/Hotfix/Danger/Component/UserInfoComponentSystem.cs
@@ -393,7 +393,7 @@ namespace ET
                     newValue = Math.Min(Math.Max(0, newValue), ComHelp.GetMaxBaoShiDu());
                     self.UserInfo.BaoShiDu = (int)newValue;
                     saveValue = self.UserInfo.BaoShiDu.ToString();
-                    if (self.UserInfo.BaoShiDu < 98)
+                    if (self.UserInfo.BaoShiDu < 80)
                     {
                         unit.GetComponent<BuffManagerComponent>()?.BuffRemove(99001011);
                     }
