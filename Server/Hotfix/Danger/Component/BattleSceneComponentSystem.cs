cf60bad4cbca8669ee40df431f96a1e1be7bc712
tangchunguang <394266315@qq.com>
2023-04-14 15:38:47 +0800

    no message

--- Server/Hotfix/Danger/Component/BattleSceneComponentSystem.cs
+++ Server/Hotfix/Danger/Component/BattleSceneComponentSystem.cs
@@ -178,7 +178,7 @@ namespace ET
             long fubenid = IdGenerater.Instance.GenerateId();
             long fubenInstanceId = IdGenerater.Instance.GenerateInstanceId();
             Scene fubnescene = SceneFactory.Create(Game.Scene, fubenid, fubenInstanceId, self.DomainZone(), "Battle" + fubenid.ToString(), SceneType.Fuben);
-            fubnescene.AddComponent<BattleDungeonComponent>();
+            fubnescene.AddComponent<BattleDungeonComponent>().SendReward = false;
             TransferHelper.NoticeFubenCenter(fubnescene, 1).Coroutine();
             MapComponent mapComponent = fubnescene.GetComponent<MapComponent>();
             mapComponent.SetMapInfo((int)SceneTypeEnum.Battle, sceneId, 0);
