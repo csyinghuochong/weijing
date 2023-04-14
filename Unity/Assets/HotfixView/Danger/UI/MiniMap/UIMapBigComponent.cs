96cde9e3516016defaea80a003c70fdab9e5e543
tangchunguang <394266315@qq.com>
2023-04-14 16:04:43 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/MiniMap/UIMapBigComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/MiniMap/UIMapBigComponent.cs
@@ -41,8 +41,6 @@ namespace ET
         }
     }
 
-
-
     public class UIMapBigComponentAwakeSystem : AwakeSystem<UIMapBigComponent>
     {
 
--- start_merge_zone.bat
+++ start_merge_zone.bat
@@ -1,4 +1,4 @@
 ::先备份数据库     要移除的区->目标区     在修改物理区
 cd bin
-dotnet Server.dll --AppType=MergeZone --Console=1 --StartConfig=StartConfig/Localhost --Parameters=16_14 --Process 1
+dotnet Server.dll --AppType=MergeZone --Console=1 --StartConfig=StartConfig/Localhost --Parameters=21_20 --Process 1
 pause
