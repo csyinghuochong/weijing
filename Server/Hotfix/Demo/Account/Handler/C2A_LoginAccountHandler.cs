2964f858312d1c48be26d648f1b1942cd8292c8c
tangchunguang <394266315@qq.com>
2023-04-22 09:59:20 +0800

    no message

--- Server/Hotfix/Demo/Account/Handler/C2A_LoginAccountHandler.cs
+++ Server/Hotfix/Demo/Account/Handler/C2A_LoginAccountHandler.cs
@@ -36,7 +36,7 @@ namespace ET
 
         protected override async ETTask Run(Session session, C2A_LoginAccount request, A2C_LoginAccount response, Action reply)
         {
-            LogHelper.LogDebug($"LoginTest request.AccountName:{request.AccountName} {request.Password} {session.RemoteAddress}");
+            Log.Debug($"LoginTest request.AccountName:{request.AccountName} {request.Password} {session.RemoteAddress}");
             if (session.DomainScene().SceneType != SceneType.Account)
             {
                 Log.Error($"LoginTest C2A_LoginAccount请求的Scene错误，当前Scene为：{session.DomainScene().SceneType}");
