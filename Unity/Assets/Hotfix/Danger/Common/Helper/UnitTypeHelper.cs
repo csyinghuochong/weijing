1f6b386bed58a9e21b1286dbc23e31824382dbdc
tangchunguang <394266315@qq.com>
2023-04-22 16:43:08 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/UnitTypeHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/UnitTypeHelper.cs
@@ -49,10 +49,10 @@
                     return self.GetBattleCamp() != defend.GetBattleCamp() && !self.IsSameTeam(defend);
                 }
 
-                //全体: 对全部造成伤害
-                //队伍 : 对自己队伍之外的人和怪造成伤害
-                //家族：对自己家族外的人和怪造成伤害
-                //和平：对任何人都不造成伤害
+                //0全体: 对全部造成伤害
+                //1队伍 : 对自己队伍之外的人和怪造成伤害
+                //2家族：对自己家族外的人和怪造成伤害
+                //3和平：对任何人都不造成伤害
                 int attackmode = self.GetAttackMode();
                 if (attackmode == 1 && self.IsSameTeam(defend))
                 {
