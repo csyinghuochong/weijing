7e32a2ea396ae48be73feeb3ec1e4e4eb55c95c4
tangchunguang <394266315@qq.com>
2023-04-25 22:55:50 +0800

    no message

--- Unity/Assets/Model/Generate/ConfigPartial/FuntionConfig.cs
+++ Unity/Assets/Model/Generate/ConfigPartial/FuntionConfig.cs
@@ -4,8 +4,7 @@ namespace ET
 {
     public partial class FuntionConfigCategory
     {
-        public Dictionary<int, List<int>> OpenTimeList = new Dictionary<int, List<int>>();  
-
+     
         public override void AfterEndInit()
         {
             foreach (FuntionConfig functionConfig in this.GetAll().Values)
@@ -20,15 +19,7 @@ namespace ET
 
                 for (int i = 0; i < openTimes.Length; i+=2)
                 {
-                    int openTime_1 = int.Parse(openTimes[i].Split(';')[0]);
-                    int openTime_2 = int.Parse(openTimes[i].Split(';')[1]);
-                    int closeTime_1 = int.Parse(openTimes[i+1].Split(';')[0]);
-                    int closeTime_2 = int.Parse(openTimes[i+1].Split(';')[1]);
-                    openTime.Add(openTime_1);
-                    openTime.Add(openTime_2);
-                    openTime.Add(closeTime_1);
-                    openTime.Add(closeTime_2);
-                    OpenTimeList.Add(functionConfig.Id, openTime);
+                   
                 }
             }
         }
