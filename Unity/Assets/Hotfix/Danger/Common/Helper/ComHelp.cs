ad861f9a5ea6b875fa4141928a31d19c7bf94c0e
tangchunguang <394266315@qq.com>
2023-05-07 17:54:14 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ComHelp.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ComHelp.cs
@@ -16,7 +16,7 @@ namespace ET
 
         public const string RobotPassWord = "et@#robot";
 
-        public const int Version = 20230426;
+        public const int Version = 20230507;
 
         //宠物魔法技能
         public static List<int> PetMagicSkill = new List<int>(2) { };
