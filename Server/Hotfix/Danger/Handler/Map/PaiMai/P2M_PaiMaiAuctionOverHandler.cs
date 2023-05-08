abdc628f3f87e1ad11c3f8a77b4f3d9bed0bec5a
tangchunguang <394266315@qq.com>
2023-05-08 17:27:14 +0800

    no message

--- Server/Hotfix/Danger/Handler/Map/PaiMai/P2M_PaiMaiAuctionOverHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/PaiMai/P2M_PaiMaiAuctionOverHandler.cs
@@ -9,13 +9,15 @@ namespace ET
         protected override async ETTask Run(Unit unit, P2M_PaiMaiAuctionOverRequest request, M2P_PaiMaiAuctionOverResponse response, Action reply)
         {
             Log.Debug($"PaiMaiAuctionOver:  {unit.DomainZone()} {unit.Id}");
-            if (!unit.GetComponent<BagComponent>().OnCostItemData($"1;{request.Price}"))
+            UserInfoComponent userInfoComponent = unit.GetComponent<UserInfoComponent>();
+            if (userInfoComponent.UserInfo.Gold < request.Price)
             {
                 unit.GetComponent<UserInfoComponent>().UpdateRoleData(UserDataType.Message, "金币不足，竞拍失败！");
                 response.Error = ErrorCore.ERR_GoldNotEnoughError;
             }
             else
             {
+                userInfoComponent.UpdateRoleMoneySub( UserDataType.Gold, (request.Price * -1).ToString(), true, ItemGetWay.Auction );
                 Log.Console($"扣除竞拍价：{request.Price}");
             }
             reply();
