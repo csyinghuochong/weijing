ca4dbea4f2d666cf536535e1c5b0a5a10a756a9a
tangchunguang <394266315@qq.com>
2023-05-09 16:05:10 +0800

    no message

--- Server/Hotfix/Danger/Helper/LogHelper.cs
+++ Server/Hotfix/Danger/Helper/LogHelper.cs
@@ -16,11 +16,11 @@ namespace ET
         /// <summary>
         /// 0 无日志 1 info  2debug  3 waring 4 error
         /// </summary>
-        public static int LogLevel = 0;
+        public static int LogLevel = 3;
 
         public static void LogWarning(string msg, bool log = false)
         {
-            if (LogLevel >= 3 && !log)
+            if (LogLevel >= 3 && log)
             {
                 Log.Warning(msg);
             }
