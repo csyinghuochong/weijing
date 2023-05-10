f4b7039de821e22a0231dd3ecf3bb29a8a70036c
tangchunguang <394266315@qq.com>
2023-05-10 14:31:56 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/JiaYuan/UIJiaYuanMysteryItemComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/JiaYuan/UIJiaYuanMysteryItemComponent.cs
@@ -79,7 +79,6 @@ namespace ET
             };
             M2C_JiaYuanMysteryBuyResponse r2c_roleEquip = (M2C_JiaYuanMysteryBuyResponse)await self.DomainScene().GetComponent<SessionComponent>().Session.Call(c2M_MysteryBuyRequest);
             ItemConfig itemConfig = ItemConfigCategory.Instance.Get(mysteryConfig.SellItemID);
-            FloatTipManager.Instance.ShowFloatTip($"购买{itemConfig.ItemName}成功");
             UI uI = UIHelper.GetUI(self.DomainScene(), UIType.UIJiaYuanMystery);
             uI.GetComponent<UIJiaYuanMysteryComponent>().RequestMystery().Coroutine();
         }
