6a11053dbfd9cf2cfd29013ec6efd435d523fc1a
hesongnian <hesongnian123@vip.qq.com>
2023-05-26 09:51:35 +0800

    11111111

--- Server/Hotfix/Danger/Component/SoloSceneComponentSystem.cs
+++ Server/Hotfix/Danger/Component/SoloSceneComponentSystem.cs
@@ -35,7 +35,7 @@ namespace ET
         //每日零点清理竞技场更新数据
         public static void OnZeroClockUpdate(this SoloSceneComponent self)
         {
-            //self.BeginSoloTimer();
+            self.BeginSoloTimer();
         }
 
         public static void BeginSoloTimer(this SoloSceneComponent self)
@@ -58,7 +58,7 @@ namespace ET
             if (curTime < closeTime)
             {
                 //传入还有多少时间结束
-                //self.OnSoloBegin(closeTime - curTime).Coroutine();
+                self.OnSoloBegin(closeTime - curTime).Coroutine();
                 return;
             }
         }
@@ -72,12 +72,18 @@ namespace ET
                 MessageHelper.SendActor(robotSceneId, new G2Robot_MessageRequest() { Zone = self.DomainZone(), MessageType = NoticeType.SoloBegin });
             }
 
+            int trigger = 0;
             //传入定时器,倒计时结束触发OnSoloOver
             for (int i = 0; i < time; i++)
             {
+                trigger++;
                 await TimerComponent.Instance.WaitAsync(1000);
-                //每秒进行一次匹配效验
-                self.CheckMatch(i).Coroutine();
+                //每5秒秒进行一次匹配效验
+                if (trigger >= 5)
+                {
+                    trigger = 0;
+                    self.CheckMatch(i).Coroutine();
+                }
             }
             self.OnSoloOver();
         }
@@ -95,14 +101,16 @@ namespace ET
                 }
             }
             self.MatchList.Add(teamPlayerInfo);
+            self.MatchList.Add(teamPlayerInfo);     //临时加 测试人数不够
         }
 
         //匹配监测机制
         public static async ETTask CheckMatch(this SoloSceneComponent self, int time)
         {
-            LogHelper.LogWarning("竞技场开始匹配", true);
+            LogHelper.LogWarning("竞技场开始匹配 time =" + time, true);
             //30,秒内 低战力/高战力>=0.8 60秒 低战力/高战力>= 0.6 90秒 低战力/高战力>=0)
-            float range = 0f;
+            float range = 1f;  //战力调整系数
+            /*
             if (time < 30)
             {
                 range = 0.8f;
@@ -115,9 +123,11 @@ namespace ET
             {
                 range = 0f;
             }
-
+            */
             //超时移除
             long serverTime = TimeHelper.ServerNow();
+            //匹配超过一定时间移除匹配列表
+            /*
             for (int i = self.MatchList.Count - 1; i >= 0; i--)
             {
                 if (serverTime - self.MatchList[i].MatchTime > 105*1000)
@@ -126,6 +136,9 @@ namespace ET
                     continue;
                 }
             }
+            */
+
+            //定义了一个比较器进行排序
             self.MatchList.Sort(delegate (SoloPlayerInfo a, SoloPlayerInfo b)
             {
                 return (int)a.Combat - (int)b.Combat;
@@ -133,25 +146,35 @@ namespace ET
 
             //通知玩家
             long gateServerId = DBHelper.GetGateServerId(self.DomainZone());
-            List<long> playerlist = new List<long>();
+            List<SoloPlayerInfo> playerlist = new List<SoloPlayerInfo>();
             for (int i = self.MatchList.Count - 1; i >= 0; i--)
             {
-                //两两匹配
+                //两两匹配  i和t进行匹配
                 int t = i - 1;
                 if (t < 0)
                 {
                     break;
                 }
 
+                //获取对应玩家数据
                 SoloPlayerInfo soloPlayerInfo_i = self.MatchList[i];
                 SoloPlayerInfo soloPlayerInfo_t = self.MatchList[t];
+
+                //这里还需要添加判断2个目标是否掉线
+
+
+                //获取双方战力进行匹配
                 if (soloPlayerInfo_i.Combat * 1f / soloPlayerInfo_t.Combat <= range )
                 {
+                    //匹配成功
                     i--;
                     self.MatchList.RemoveAt(t);
-                    playerlist.Add(soloPlayerInfo_i.UnitId);
-                    playerlist.Add(soloPlayerInfo_t.UnitId);
 
+                    //存入匹配缓存数据,方便下面发送消息
+                    playerlist.Add(soloPlayerInfo_i);
+                    playerlist.Add(soloPlayerInfo_t);
+
+                    //把匹配的结果和要进入的副本ID存入缓存
                     if (!self.SoloResult.ContainsKey(soloPlayerInfo_i.UnitId))
                     {
                         self.SoloResult.Add(soloPlayerInfo_i.UnitId, new SoloResultInfo());
@@ -168,16 +191,27 @@ namespace ET
             }
 
             self.m2C_SoloMatchResult.Result = 1;
+            //对缓存的匹配数据进行发送消息
             for (int i = 0; i < playerlist.Count; i++)
             {
+                //循环给每个要进入的玩家发送进入副本的消息
+                //发送消息获取对应的玩家数据
                 G2T_GateUnitInfoResponse g2M_UpdateUnitResponse = (G2T_GateUnitInfoResponse)await ActorMessageSenderComponent.Instance.Call
                     (gateServerId, new T2G_GateUnitInfoRequest()
                     {
-                        UserID = playerlist[i]
+                        UserID = playerlist[i].UnitId
                     });
+                //判断目标是玩家且当前是在线的
                 if (g2M_UpdateUnitResponse.PlayerState == (int)PlayerState.Game && g2M_UpdateUnitResponse.SessionInstanceId > 0)
                 {
+                    //给对应玩家发送进入地图的消息
                     MessageHelper.SendActor(g2M_UpdateUnitResponse.SessionInstanceId, self.m2C_SoloMatchResult);
+
+                    //匹配成功的要移除匹配列表
+                    if (self.MatchList.Contains(playerlist[i]))
+                    {
+                        self.MatchList.Remove(playerlist[i]);
+                    }
                 }
             }
         }
