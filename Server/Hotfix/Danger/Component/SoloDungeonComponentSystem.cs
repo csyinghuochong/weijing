08d2aa205a31b326e79f0e10fcebd49bc674b714
tangchunguang <394266315@qq.com>
2023-05-29 19:16:00 +0800

    no message

--- Server/Hotfix/Danger/Component/SoloDungeonComponentSystem.cs
+++ Server/Hotfix/Danger/Component/SoloDungeonComponentSystem.cs
@@ -5,7 +5,7 @@
 
         public static void OnKillEvent(this SoloDungeonComponent self, Unit defend)
         {
-            Log.Debug($"SoloDungeonComponent {defend.Type}");
+            Log.Debug($"solo场击杀事件 {defend.Type}");
         }
     }
 }
