110c069874ffa867c98d67dbb0e91e9a3fa242e2
tangchunguang <394266315@qq.com>
2023-06-01 12:24:22 +0800

    no message

--- Server/Model/Danger/DB/DBRankInfo.cs
+++ Server/Model/Danger/DB/DBRankInfo.cs
@@ -10,7 +10,7 @@ namespace ET
         public List<RankingInfo> rankingInfos = new List<RankingInfo>();    //战力排行
         public List<RankingInfo> rankingCamp1 = new List<RankingInfo>();    //正派
         public List<RankingInfo> rankingCamp2 = new List<RankingInfo>();    //邪派
-        public List<RankingInfo> rankSoloInfo = new List<RankPetInfo>();
+        public List<RankingInfo> rankSoloInfo = new List<RankingInfo>();
 
         /// <summary>
         /// 家族相关的
