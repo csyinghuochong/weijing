71eb78b1cbaf5c7bb7b0c2d255f8779cd2bde57f
tangchunguang <394266315@qq.com>
2023-06-03 16:43:13 +0800

    no message

---
+++ Server/Model/Generate/ConfigPartial/PaiMaiSellConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+
+namespace ET
+{
+    public partial class PaiMaiSellConfigCategory
+    {
+        public Dictionary<int, int> ItemToSell = new Dictionary<int, int>();
+
+        public override void AfterEndInit()
+        {
+            foreach (PaiMaiSellConfig firstWinConfig in this.GetAll().Values)
+            {
+                if (!ItemToSell.ContainsKey(firstWinConfig.ItemID))
+                {
+                    ItemToSell.Add(firstWinConfig.ItemID, firstWinConfig.Id);
+                }
+            }
+        }
+    }
+}
