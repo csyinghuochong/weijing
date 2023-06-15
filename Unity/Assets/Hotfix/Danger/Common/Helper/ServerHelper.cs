b82976ee2829fecb29a2273f49ff3e0d9e26024c
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2023-06-15 16:46:37 +0800

    111

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -226,6 +226,7 @@ namespace ET
                 serverItems_1.Add(new ServerItem() { ServerId = 31, ServerIp = $"{ip}:20375", ServerName = "遗忘之海", ServerOpenTime = 1685703600000, New = 0, Show = 1 });
 
                 serverItems_1.Add(new ServerItem() { ServerId = 32, ServerIp = $"{ip}:20385", ServerName = "金榜题名", ServerOpenTime = 1686308400000, New = 1, Show = 1 });
+                serverItems_1.Add(new ServerItem() { ServerId = 33, ServerIp = $"{ip}:20395", ServerName = "鱼跃龙门", ServerOpenTime = 1686913200000, New = 1, Show = 1 });
             }
 
             return serverItems_1;
