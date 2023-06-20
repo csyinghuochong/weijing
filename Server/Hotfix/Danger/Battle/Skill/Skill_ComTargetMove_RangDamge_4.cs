2bcd8352a1603ce5105070dfadf4235e02b2beb3
tangchunguang <394266315@qq.com>
2023-06-20 15:51:48 +0800

    no message

---
+++ Server/Hotfix/Danger/Battle/Skill/Skill_ComTargetMove_RangDamge_4.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace ET
+{
+
+    //全地图随机子弹
+    public class Skill_ComTargetMove_RangDamge_4 : SkillHandler
+    {
+        public override void OnInit(SkillInfo skillId, Unit theUnitFrom)
+        {
+            this.BaseOnInit(skillId, theUnitFrom);
+
+            OnExecute();
+        }
+
+        public override void OnExecute()
+        {
+            this.InitSelfBuff();
+
+            string[] paraminfos = this.SkillConf.GameObjectParameter.Split('@');
+            using ListComponent<Vector3> vector3s = new ListComponent<Vector3>(); 
+            for (int i = 0; i < paraminfos.Length; i++)
+            {
+                string[] posinfo = paraminfos[i].Split(';');
+                vector3s.Add(new Vector3(float.Parse(posinfo[0]), float.Parse(posinfo[1]), float.Parse(posinfo[2])));
+            }
+            int startindex = RandomHelper.RandomNumber(0, vector3s.Count);
+            Vector3 startpos = vector3s[startindex];
+
+            vector3s.RemoveAt(startindex);
+
+            int endindex = RandomHelper.RandomNumber(0, vector3s.Count);
+            Vector3 targetpos = vector3s[endindex];
+
+            Unit unit = UnitFactory.CreateBullet(this.TheUnitFrom.DomainScene(), this.TheUnitFrom.Id, this.SkillConf.Id, 0, startpos, new CreateMonsterInfo());
+            unit.AddComponent<RoleBullet1Componnet>().OnBaseBulletInit(this, this.TheUnitFrom.Id);
+            unit.BulletMoveToAsync(targetpos).Coroutine();
+        }
+
+        public override void OnUpdate()
+        {
+            if (TimeHelper.ServerNow() > SkillEndTime)
+            {
+                this.SetSkillState(SkillState.Finished);
+                return;
+            }
+        }
+
+        public override void OnFinished()
+        {
+            this.Clear();
+        }
+    }
+}
---
+++ Unity/Assets/Hotfix/Danger/Skill/Skill_ComTargetMove_RangDamge_4.cs
@@ -0,0 +1,30 @@
+
+namespace ET
+{
+    //全地图随机子弹
+    [SkillHandler]
+    public class Skill_ComTargetMove_RangDamge_4 : Skill_Action_Common
+    {
+        public override void OnInit(SkillInfo skillId, Unit theUnitFrom)
+        {
+            this.BaseOnInit(skillId, theUnitFrom);
+
+            OnExecute();
+        }
+
+        public override void OnExecute()
+        {
+
+        }
+
+        public override void OnUpdate()
+        {
+            this.BaseOnUpdate();
+        }
+
+        public override void OnFinished()
+        {
+
+        }
+    }
+}
