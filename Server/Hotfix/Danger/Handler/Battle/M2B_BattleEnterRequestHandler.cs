07d86462fa843703b4fc92dee9e3ab2cc7da7f3f
tangchunguang <394266315@qq.com>
2023-07-05 16:43:03 +0800

    no message

--- Server/Hotfix/Danger/Handler/Battle/M2B_BattleEnterRequestHandler.cs
+++ Server/Hotfix/Danger/Handler/Battle/M2B_BattleEnterRequestHandler.cs
@@ -7,9 +7,10 @@ namespace ET
     {
         protected override async ETTask Run(Scene scene, M2B_BattleEnterRequest request, B2M_BattleEnterResponse response, Action reply)
         {
-            (long, int) vv = scene.GetComponent<BattleSceneComponent>().GetBattleInstanceId(request.UserID, request.SceneId);
-            response.FubenInstanceId = vv.Item1;
-            response.Camp = vv.Item2;
+            (long fubeninstanceid, int camp)  = scene.GetComponent<BattleSceneComponent>().GetBattleInstanceId(request.UserID, request.SceneId);
+            response.FubenInstanceId = fubeninstanceid;
+            response.Camp = camp;
+
 
             reply();
             await ETTask.CompletedTask;
