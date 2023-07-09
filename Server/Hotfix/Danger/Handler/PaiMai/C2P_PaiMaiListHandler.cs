fedac714160b4a43d7b66e50696c60d317754d49
tangchunguang <394266315@qq.com>
2023-07-09 11:26:20 +0800

    no message

--- Server/Hotfix/Danger/Handler/PaiMai/C2P_PaiMaiListHandler.cs
+++ Server/Hotfix/Danger/Handler/PaiMai/C2P_PaiMaiListHandler.cs
@@ -32,7 +32,7 @@ namespace ET
             {
                 //int page = request.Page;
                 int page = (int)request.ActorId;
-                int pagenum = 300;  //每页的数量
+                int pagenum = 600;  //每页的数量
                 ////新的方式 切页
                 int maxpage = PaiMaiItemInfo.Count / pagenum;
                 int extra = (PaiMaiItemInfo.Count % pagenum) > 0 ? 1 : 0;
