e820fed4a83aec651a9983e74b953ceb0d0fc5c7
tangchunguang <394266315@qq.com>
2023-07-18 11:01:31 +0800

    no message

--- Unity/Assets/Mono/Module/Network/ErrorCore.cs
+++ Unity/Assets/Mono/Module/Network/ErrorCore.cs
@@ -225,7 +225,7 @@
   
         //宠物
         public const int ERR_Pet_Hint_1 = 300901;                                   //宠物星级出错
-        public const int ERR_Pet_UpStar = 300902;                                   //宠物星级失败                                                                 //宠物
+        public const int ERR_Pet_UpStar = 300902;                                   //宠物星级失败                                                              
         public const int ERR_Pet_AddSkillSame = 300903;                             //宠物打书技能ID相同
         public const int ERR_Pet_NoUseItem = 300904;                              //无法给宠物使用道具
         public const int ERR_PetIsFull = 300905;                                    //宠物已满
@@ -233,6 +233,7 @@
         public const int ERR_Pet_NoExist = 300907;
         public const int ERR_CangKu_Already = 300908;
         public const int ERR_CangKu_NotOpen = 300910;
+        public const int ERR_Pet_Hint_2 = 300911;                                   //出战宠物不能被融合
 
         public const int ERR_Union_Same_Name = 301001;                              //同名的家族
         public const int ERR_MysteryItem_Max = 301002;                              //改道具每日购买次数已达上限
