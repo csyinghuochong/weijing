60c4b8304a4c007a334a33b36928dcac617d85a9
tangchunguang <394266315@qq.com>
2023-08-02 14:26:54 +0800

    no message

---
+++ Unity/Assets/Hotfix/Danger/Handler/Team/M2C_SyncMiJingDamageHandler.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ET
+{
+
+    [MessageHandler]
+    public class M2C_SyncMiJingDamageHandler : AMHandler<M2C_SyncMiJingDamage>
+    {
+        protected override void Run(Session session, M2C_SyncMiJingDamage message)
+        {
+            EventType.SyncMiJingDamage.Instance.M2C_SyncMiJingDamage = message;
+            EventType.SyncMiJingDamage.Instance.ZoneScene = session.ZoneScene();
+            EventSystem.Instance.PublishClass(EventType.SyncMiJingDamage.Instance);
+        }
+    }
+}
