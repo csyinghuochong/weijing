a99b58bdb498199c5ebf498894722cb54b255d25
tangchunguang <394266315@qq.com>
2023-08-04 21:32:57 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIFashion/UIFashionShowItemComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIFashion/UIFashionShowItemComponent.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 namespace ET
 {
 
-    public class UIFashionShowItemComponent : Entity, IAwake<GameObject>
+    public class UIFashionShowItemComponent : Entity, IAwake<GameObject>, IDestroy
     {
         public GameObject GameObject;
 
@@ -15,6 +15,9 @@ namespace ET
 
         public GameObject Btn_Active;
         public GameObject RawImage;
+
+        public RenderTexture RenderTexture;
+        public UIModelDynamicComponent UIModelShowComponent;
 
         public int Status;
     }
@@ -30,6 +33,26 @@ namespace ET
             self.Text_222 = rc.Get<GameObject>("Text_222");
             self.Text_111 = rc.Get<GameObject>("Text_111");
             self.Btn_Active = rc.Get<GameObject>("Btn_Active");
+
+            self.RawImage = rc.Get<GameObject>("RawImage");
+            self.RawImage.gameObject.SetActive(true);
+
+            var path = ABPathHelper.GetUGUIPath("Common/UIModelDynamic");
+            GameObject bundleGameObject = ResourcesComponent.Instance.LoadAsset<GameObject>(path);
+            GameObject gameObject = UnityEngine.Object.Instantiate(bundleGameObject);
+            self.UIModelShowComponent = self.AddChild<UIModelDynamicComponent, GameObject>(gameObject);
+        }
+    }
+
+    public class UIFashionShowItemComponentDestroy : DestroySystem<UIFashionShowItemComponent>
+    {
+        public override void Destroy(UIFashionShowItemComponent self)
+        {
+            self.UIModelShowComponent.ReleaseRenderTexture();
+            self.RenderTexture.Release();
+            GameObject.Destroy(self.RenderTexture);
+            self.RenderTexture = null;
+            //RenderTexture.ReleaseTemporary(self.RenderTexture);
         }
     }
 
@@ -37,8 +60,36 @@ namespace ET
     {
 
         public static void OnUpdateUI(this UIFashionShowItemComponent self, int fashionid)
-        { 
-            
+        {
+            int occ = self.ZoneScene().GetComponent<UserInfoComponent>().UserInfo.Occ;
+            int status = 0;
+
+
+            FashionConfig fashionConfig = FashionConfigCategory.Instance.Get( fashionid );
+
+            self.Text_111.GetComponent<Text>().text = fashionConfig.Name;
+
+            if (self.RenderTexture != null)
+            {
+                self.RenderTexture.Release();
+                GameObject.Destroy(self.RenderTexture);
+                self.RenderTexture = null;
+            }
+
+            if (self.RenderTexture == null)
+            {
+                self.RenderTexture = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+                self.RenderTexture.Create();
+                self.RawImage.GetComponent<RawImage>().texture = self.RenderTexture;
+
+                GameObject gameObject = self.UIModelShowComponent.GameObject;
+                self.UIModelShowComponent.OnInitUI(self.RawImage, self.RenderTexture);
+                self.UIModelShowComponent.ShowModel($"Parts/{occ}/" + fashionConfig.Model).Coroutine();
+                gameObject.transform.Find("Camera").localPosition = new Vector3(0f, 20f, 450f);
+                gameObject.transform.Find("Camera").GetComponent<Camera>().fieldOfView = 45;
+                gameObject.transform.localPosition = new Vector2((fashionid % 10) * 1000 + 1000, 0);
+                gameObject.transform.Find("Model").localRotation = Quaternion.Euler(0f, -45f, 0f);
+            }
         }
     }
 }
