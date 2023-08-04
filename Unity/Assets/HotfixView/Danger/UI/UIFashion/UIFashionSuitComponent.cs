7a2ae24a7c13ad79890e452bbde42297a14b9d55
tangchunguang <394266315@qq.com>
2023-08-04 22:02:15 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIFashion/UIFashionSuitComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIFashion/UIFashionSuitComponent.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,10 +5,91 @@ using UnityEngine.UI;
 namespace ET
 {
 
-    public class UIFashionSuitComponent : Entity
-    { 
-        
+    public class UIFashionSuitComponent : Entity, IAwake
+    {
+        public GameObject Btn_Suit_1;
+
+        public GameObject BuildingList;
+
+        public Dictionary<int, GameObject> ButtonList = new Dictionary<int, GameObject>();
+        public List<UIFashionSuitItemComponent> FashionSuitList = new List<UIFashionSuitItemComponent> { };
     }
 
+    public class UIFashionSuitComponentAwake : AwakeSystem<UIFashionSuitComponent >
+    {
+        public override void Awake(UIFashionSuitComponent self)
+        {
+            self.ButtonList.Clear();
+            ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
 
+            self.Btn_Suit_1 = rc.Get<GameObject>("Btn_Suit_1");
+
+            self.BuildingList = rc.Get<GameObject>("BuildingList");
+
+
+            self.OnInitUI();
+        }
+    }
+
+    public static class UIFashionSuitComponentSystem
+    {
+
+        public static void OnClicSuitButton(this UIFashionSuitComponent self, int suitid)
+        {
+            FashionSuitConfig fashionSuitConfig = FashionSuitConfigCategory.Instance.Get(suitid);
+
+            var path = ABPathHelper.GetUGUIPath("Main/Fashion/UIFashionSuitItem");
+            var bundleGameObject = ResourcesComponent.Instance.LoadAsset<GameObject>(path);
+
+
+            for (int i = 0; i < fashionSuitConfig.ActiveCost.Length; i++)
+            {
+                UIFashionSuitItemComponent uiitem = null;
+                if (i < self.FashionSuitList.Count)
+                {
+                    uiitem = self.FashionSuitList[i];
+                    uiitem.GameObject.SetActive(true);
+                }
+                else
+                {
+                    GameObject go = GameObject.Instantiate(bundleGameObject);
+                    UICommonHelper.SetParent(go, self.BuildingList);
+                    uiitem = self.AddChild<UIFashionSuitItemComponent, GameObject>(go);
+                    self.FashionSuitList.Add(uiitem);
+                }
+                uiitem.OnUpdateUI(fashionSuitConfig.ActiveCost[i]);
+            }
+
+            for (int i = fashionSuitConfig.ActiveCost.Length; i < self.FashionSuitList.Count; i++)
+            {
+                self.FashionSuitList[i].GameObject.SetActive(false);
+            }
+        }
+
+        public static void OnInitUI(this UIFashionSuitComponent self)
+        {
+            List<int> suitlist = new List<int>();
+            int occ = self.ZoneScene().GetComponent<UserInfoComponent>().UserInfo.Occ;
+
+            Dictionary<int, FashionSuitConfig> keyValuePairs = FashionSuitConfigCategory.Instance.GetAll();
+            foreach (var item in keyValuePairs)
+            {
+                if (item.Value.Occ != occ)
+                {
+                    continue;
+                }
+                suitlist.Add(item.Key);
+            }
+
+            for (int i = 0; i < suitlist.Count; i++)
+            {
+                int suitid = suitlist[i];   
+                GameObject button = GameObject.Instantiate( self.Btn_Suit_1 );
+                UICommonHelper.SetParent( button, button.transform.gameObject );
+                button.GetComponent<Button>().onClick.AddListener(() => { self.OnClicSuitButton(suitid);  }   );
+            }
+
+            self.OnClicSuitButton(suitlist[0]);
+        }
+    }
 }
---
+++ Unity/Assets/HotfixView/Danger/UI/UIFashion/UIFashionSuitItemComponent.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
+{
+    public class UIFashionSuitItemComponent : Entity, IAwake<GameObject>, IDestroy
+    {
+
+        public GameObject GameObject;
+        public GameObject RawImage;
+
+        public RenderTexture RenderTexture;
+        public UIModelDynamicComponent UIModelShowComponent;
+
+    }
+
+    public class UIFashionSuitItemComponentAwake : AwakeSystem<UIFashionSuitItemComponent, GameObject>
+    {
+        public override void Awake(UIFashionSuitItemComponent self, GameObject a)
+        {
+            self.GameObject = a;
+
+            ReferenceCollector rc = a.GetComponent<ReferenceCollector>();
+
+            self.RawImage = rc.Get<GameObject>("RawImage");
+            self.RawImage.gameObject.SetActive(true);
+
+            var path = ABPathHelper.GetUGUIPath("Common/UIModelDynamic");
+            GameObject bundleGameObject = ResourcesComponent.Instance.LoadAsset<GameObject>(path);
+            GameObject gameObject = UnityEngine.Object.Instantiate(bundleGameObject);
+            self.UIModelShowComponent = self.AddChild<UIModelDynamicComponent, GameObject>(gameObject);
+        }
+    }
+
+
+    public class UIFashionSuitItemComponentDestroy : DestroySystem<UIFashionSuitItemComponent>
+    {
+        public override void Destroy(UIFashionSuitItemComponent self)
+        {
+            self.UIModelShowComponent.ReleaseRenderTexture();
+            self.RenderTexture.Release();
+            GameObject.Destroy(self.RenderTexture);
+            self.RenderTexture = null;
+            //RenderTexture.ReleaseTemporary(self.RenderTexture);
+        }
+    }
+
+    public static class UIFashionSuitItemComponentSytstem
+    {
+        public static void OnUpdateUI(this UIFashionSuitItemComponent self, int fashionid)
+        {
+            int occ = self.ZoneScene().GetComponent<UserInfoComponent>().UserInfo.Occ;
+            FashionConfig fashionConfig = FashionConfigCategory.Instance.Get(fashionid);
+
+            if (self.RenderTexture != null)
+            {
+                self.RenderTexture.Release();
+                GameObject.Destroy(self.RenderTexture);
+                self.RenderTexture = null;
+            }
+
+            if (self.RenderTexture == null)
+            {
+                self.RenderTexture = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+                self.RenderTexture.Create();
+                self.RawImage.GetComponent<RawImage>().texture = self.RenderTexture;
+
+                GameObject gameObject = self.UIModelShowComponent.GameObject;
+                self.UIModelShowComponent.OnInitUI(self.RawImage, self.RenderTexture);
+                self.UIModelShowComponent.ShowModel($"Parts/{occ}/" + fashionConfig.Model).Coroutine();
+                gameObject.transform.Find("Camera").localPosition = new Vector3(0f, 20f, 450f);
+                gameObject.transform.Find("Camera").GetComponent<Camera>().fieldOfView = 45;
+                gameObject.transform.localPosition = new Vector2((fashionid % 10) * 1000 + 1000, 0);
+                gameObject.transform.Find("Model").localRotation = Quaternion.Euler(0f, -45f, 0f);
+            }
+        }
+    }
+}
