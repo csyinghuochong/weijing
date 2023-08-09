4ecbbb2457b8709ccf5e6842c5829cae5a644c1a
tangchunguang <394266315@qq.com>
2023-08-09 16:37:07 +0800

    no message

---
+++ Server/Hotfix/Danger/Handler/Rank/C2R_RankShowLieHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ET
+{
+
+    [ActorMessageHandler]
+    public class C2R_RankShowLieHandler : AMActorRpcHandler<Scene, C2R_RankShowLieRequest, R2C_RankShowLieResponse>
+    {
+        protected override async ETTask Run(Scene scene, C2R_RankShowLieRequest request, R2C_RankShowLieResponse response, Action reply)
+        {
+            for (int i = 11; i >= 1; i--)
+            {
+                response.RankList.Add(new RankShouLieInfo() { UnitID = i, PlayerName = "玩家：" + i.ToString(), KillNumber = i });
+               
+            }
+
+            reply();
+            await ETTask.CompletedTask;
+        }
+    }
+}
