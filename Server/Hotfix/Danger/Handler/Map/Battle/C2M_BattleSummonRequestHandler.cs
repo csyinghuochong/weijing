297b72735f59c20110ec2ab722898b19999c8dbb
tangchunguang <394266315@qq.com>
2023-08-10 18:58:05 +0800

    no message

--- Server/Hotfix/Danger/Handler/Map/Battle/C2M_BattleSummonRequestHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/Battle/C2M_BattleSummonRequestHandler.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MongoDB.Driver.Core.Misc;
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace ET
 {
@@ -13,7 +15,7 @@ namespace ET
 
             BattleSummonConfig battleSummonConfig = BattleSummonConfigCategory.Instance.Get(request.SummonId);
 
-            //检测小孩金币
+            //检测金币
             long costgold = 0;
             long lastsummonTime = 0;
             for (int i = 0; i < BattleSummonList.Count; i++)
@@ -71,6 +73,24 @@ namespace ET
             }
 
             //发兵
+            int camp = unit.GetBattleCamp();
+            int monsterid = battleSummonConfig.MonsterIds[camp - 1];
+            int monsternum = battleSummonConfig.MonsterNumber;
+            int sceneid = unit.DomainScene().GetComponent<MapComponent>().SceneId;
+            SceneConfig sceneConfig = SceneConfigCategory.Instance.Get(sceneid);
+
+            for (int i = 0;i <monsternum; i++)
+            {
+                float range = 1f;
+                //随机坐标
+                float ran_x = RandomHelper.RandomNumberFloat(-1 * range, range);
+                float ran_z = RandomHelper.RandomNumberFloat(-1 * range, range);
+                int startIndex = camp == 1 ? 0 : 3;
+                Vector3 initPosi = new Vector3(sceneConfig.InitPos[startIndex + 0] * 0.01f, sceneConfig.InitPos[startIndex + 1] * 0.01f, sceneConfig.InitPos[startIndex + 2] * 0.01f);
+
+                Unit unitMonster = UnitFactory.CreateMonster(unit.DomainScene(), monsterid, initPosi, new CreateMonsterInfo()
+                { Camp = camp });
+            }
 
             response.BattleSummonList = BattleSummonList;   
             reply();
