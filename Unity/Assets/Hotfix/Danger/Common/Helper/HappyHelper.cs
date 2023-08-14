458cbdf94f7b39710154ee7d0eade7a97d93b800
tangchunguang <394266315@qq.com>
2023-08-14 17:24:29 +0800

    no message

---
+++ Unity/Assets/Hotfix/Danger/Common/Helper/HappyHelper.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ET
+{
+
+    public static class HappyHelper
+    {
+
+
+        /// <summary>
+        /// 2000010场景。 所有格子的位置。 
+        /// </summary>
+        public static List<Vector3> PositionList = new List<Vector3>()
+        {
+            new Vector3(), new Vector3(), new Vector3(),    
+            new Vector3(), new Vector3(),       
+
+        };
+
+    }
+}
