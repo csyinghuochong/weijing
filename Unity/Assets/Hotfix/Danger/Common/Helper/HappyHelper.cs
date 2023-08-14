ca558593f8e7b7c82f49a580c30a1f75c6743371
tangchunguang <394266315@qq.com>
2023-08-14 17:28:45 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/HappyHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/HappyHelper.cs
@@ -13,7 +13,7 @@ namespace ET
         /// </summary>
         public static List<Vector3> PositionList = new List<Vector3>()
         {
-            new Vector3(), new Vector3(), new Vector3(),    
+            new Vector3(0f,0f,0f), new Vector3(1f,1f, 1f), new Vector3(),    
             new Vector3(), new Vector3(),       
 
         };
