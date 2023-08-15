0e78761704bc9acf46014837fabb3879d2149e51
tangchunguang <394266315@qq.com>
2023-08-15 16:57:52 +0800

    no message

--- Unity/Assets/ThirdParty/ShareSdk/ShareSDK.cs
+++ Unity/Assets/ThirdParty/ShareSdk/ShareSDK.cs
@@ -25,13 +25,13 @@ namespace cn.sharesdk.unity3d
 		public string appSecret = "c2b2d9e1afdfd9f6e1cfb3a399d01703";
 #elif UNITY_IPHONE
 		[HideInInspector]
-		public string appKey = "2d21337c7dc80";
+		public string appKey = "36af6e3967670";
 	    [HideInInspector]
-		public string appSecret = "a2c24f564def8b4a450a88491e5cc353";
+		public string appSecret = "c2b2d9e1afdfd9f6e1cfb3a399d01703";
         public List<string> customAssociatedDomains = new List<string>();
 #endif
 
-		public DevInfoSet devInfo;
+        public DevInfoSet devInfo;
 		public ShareSDKImpl shareSDKUtils;
 
 		public EventHandler authHandler;
