9d411964e3e72c1e2f62cc5bd4d075f1d412526d
tangchunguang <394266315@qq.com>
2023-08-23 22:07:02 +0800

    no message

--- Server/Hotfix/Danger/Handler/Rank/C2R_RankUnionRaceHandler.cs
+++ Server/Hotfix/Danger/Handler/Rank/C2R_RankUnionRaceHandler.cs
@@ -8,13 +8,6 @@ namespace ET
         protected override async ETTask Run(Scene scene, C2R_RankUnionRaceRequest request, R2C_RankUnionRaceResponse response, Action reply)
         {
             response.RankList = scene.GetComponent<RankSceneComponent>().DBRankInfo.rankUnionRace;
-
-            if (response.RankList.Count == 0)
-            {
-                response.RankList.Add( new RankShouLieInfo() { KillNumber = 20, Occ = 1, PlayerName = "玩家1", UnitID = 1 } );
-                response.RankList.Add(new RankShouLieInfo() { KillNumber = 10, Occ = 2, PlayerName = "玩家2", UnitID =2 });
-            }
-
             reply();
             await ETTask.CompletedTask;
         }
