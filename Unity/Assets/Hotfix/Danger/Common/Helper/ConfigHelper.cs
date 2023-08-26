43f40f506a20d9666a70b1620b8ebedc0d0634be
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2023-08-26 22:00:27 +0800

    更新配置

--- Unity/Assets/Hotfix/Danger/Common/Helper/ConfigHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ConfigHelper.cs
@@ -479,7 +479,7 @@ namespace ET
             new WorldSayConfig(){ Time = 1230, OpenDay = new List<int>{ -1},  Conent = "巨龙神已经准时出现在宝藏之地,想要挑战我的就带上你们的武器过来挑战我吧!"  },
             new WorldSayConfig(){ Time = 1930, OpenDay = new List<int>{ -1},  Conent = "一波红包雨已经来临,赶紧来看看自己是否是那个幸运玩家!"  },
             new WorldSayConfig(){ Time = 1940, OpenDay = new List<int>{ -1},  Conent = "角斗场已经开启,想要参加的勇士要抓紧时间哦!"  },
-            new WorldSayConfig(){ Time = 2000, OpenDay = new List<int>{ -1},   Conent = "世界领主已经出现在密境中,赶紧过来看看吧!"  },
+            new WorldSayConfig(){ Time = 2000, OpenDay = new List<int>{1,3,5,0},   Conent = "世界领主已经出现在密境中,赶紧过来看看吧!"  },
             new WorldSayConfig(){ Time = 2015, OpenDay = new List<int>{ -1},  Conent = "拍卖特惠已经开启,有需要的玩家可以购买哦!"  },
             new WorldSayConfig(){ Time = 2030, OpenDay = new List<int>{ -1},  Conent = "战场活动已经开启,可以通过右上角的战场按钮快速加入哦!"  },
             new WorldSayConfig(){ Time = 2100, OpenDay = new List<int>{ -1},  Conent = "一大波宝箱出现在宝藏之地,想要去的玩家赶紧前往哦!"  },
