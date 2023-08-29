7b58fd32efd70817f706eeaefc48f4cb18cb2491
tangchunguang <394266315@qq.com>
2023-08-29 14:14:20 +0800

    no message

--- Server/Hotfix/Danger/Component/DemonDungeonComponentSystem.cs
+++ Server/Hotfix/Danger/Component/DemonDungeonComponentSystem.cs
@@ -1,11 +1,26 @@
-﻿namespace ET
+﻿using System.Collections.Generic;
+
+namespace ET
 {
     public static class DemonDungeonComponentSystem
     {
 
         public static void OnClose(this DemonDungeonComponent self)
         {
-            Log.Console("OnClose111");
+            Log.Console("生成恶魔");
+
+            List<Unit> destlist = new List<Unit>();
+            List<Unit> sourcelist = UnitHelper.GetUnitList(self.DomainScene(), UnitType.Player);
+
+            ///开始后会根据当前场景的人数随机生成X个 恶魔
+            int demonNumber = 1;
+
+            RandomHelper.GetRandListByCount(sourcelist, destlist, demonNumber);
+
+            for (int i = 0; i < destlist.Count; i++)
+            { 
+                
+            }
         }
 
         public static void OnOver(this DemonDungeonComponent self)
