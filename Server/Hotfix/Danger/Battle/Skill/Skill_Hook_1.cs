1ef467c8147e9ef9f51da0ec58ec6aa4da3961b3
tangchunguang <394266315@qq.com>
2023-09-01 16:16:06 +0800

    no message

---
+++ Server/Hotfix/Danger/Battle/Skill/Skill_Hook_1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ET
+{
+
+    //钩子技能:瞬间拉至自身/目标点
+    public class Skill_Hook_1 : SkillHandler
+    {
+        //初始化
+        public override void OnInit(SkillInfo skillId, Unit theUnitFrom)
+        {
+            this.BaseOnInit(skillId, theUnitFrom);
+            this.TheUnitTarget = this.TheUnitFrom.GetParent<UnitComponent>().Get(this.SkillInfo.TargetID);
+        }
+
+        public override void OnExecute()
+        {
+            this.InitSelfBuff();
+
+            Vector3 dir = (this.TheUnitTarget.Position - this.TheUnitFrom.Position ).normalized;
+
+            this.TargetPosition = dir * Vector3.one + this.TheUnitFrom.Position;
+
+            this.TheUnitTarget.GetComponent<MoveComponent>().Clear();
+            this.TheUnitTarget.Position = this.TargetPosition + dir * Vector3.one;
+            this.TheUnitTarget.Stop(-2);
+        }
+
+        public override void OnUpdate()
+        {
+            this.BaseOnUpdate();
+        }
+
+        public override void OnFinished()
+        {
+            this.Clear();
+        }
+    }
+}
---
+++ Unity/Assets/Hotfix/Danger/Skill/Skill_Hook_1.cs
@@ -0,0 +1,9 @@
+namespace ET
+{
+
+    //钩子技能:瞬间拉至自身/目标点
+    [SkillHandler]
+    public class Skill_Hook_1 : Skill_Action_Common
+    {
+    }
+}
