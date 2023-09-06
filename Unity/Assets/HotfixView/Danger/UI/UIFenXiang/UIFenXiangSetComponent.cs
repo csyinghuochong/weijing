5e396833d8fbfca829e4f89a19ea9f3dc7dfaff7
tangchunguang <394266315@qq.com>
2023-09-06 17:38:32 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIFenXiang/UIFenXiangSetComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIFenXiang/UIFenXiangSetComponent.cs
@@ -117,8 +117,36 @@ namespace ET
 #if UNITY_EDITOR
             self.OnShareHandler(shareType, true).Coroutine();
 #else
-            GlobalHelp.FenXiang(fenXiangContent);
+            //GlobalHelp.FenXiang(fenXiangContent);
+
+             //auth和getuser接口都可以实现授权登录功能，可以任意调用一个
+            //授权（每次都会跳转到第三方平台进行授权）进行授权
+            //ssdk.Authorize(PlatformType.WeChat);
+            //ssdk.GetUserInfo(PlatformType.WeChat);
+            //设置分享
+            ShareContent content = new ShareContent();
+            //title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+            content.SetTitle(fenXiangContent.FenXiang_Title);
+            //分享文本
+            content.SetText(fenXiangContent.FenXiang_Text);
+            //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+            content.SetImageUrl(fenXiangContent.FenXiang_ImageUrl);
+            // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+            /////content.SetTitleUrl(fenXiangContent.FenXiang_ClickUrl);
+            // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+            //content.SetImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+            // url仅在微信（包括好友和朋友圈）中使用
+            ///////content.SetUrl(fenXiangContent.FenXiang_ClickUrl);
+            // site是分享此内容的网站名称，仅在QQ空间使用
+            //////content.SetSite(fenXiangContent.FenXiang_Title);
+            // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+            ///////content.SetSiteUrl(fenXiangContent.FenXiang_ClickUrl);
+            //设置分享类型
+            content.SetShareType(ContentType.Webpage);
+            //content.SetShareType(ContentType.Text);
+            GameObject.Find("Init").GetComponent<Init>().FenXiangShareContent(content, fenXiangContent.Fenxiangtype);
 #endif
+
         }
 
         public static void OnQQShare(this UIFenXiangSetComponent self)
