6c55a1af526f856d8e27df1bd0657172e72203a7
tangchunguang <394266315@qq.com>
2023-09-09 15:06:57 +0800

    no message

--- Unity/Assets/Mono/Module/Network/AChannel.cs
+++ Unity/Assets/Mono/Module/Network/AChannel.cs
@@ -12,13 +12,13 @@ namespace ET
 
 	public struct Packet
 	{
-		public static int MinPacketSize = 2;
-		public static int OpcodeIndex = 8;
-		public static int KcpOpcodeIndex = 0;
-		public static int OpcodeLength = 2;
-		public static int ActorIdIndex = 0;
-		public static int ActorIdLength = 8;
-		public static int MessageIndex = 10;
+		public const int MinPacketSize = 2;
+		public const int OpcodeIndex = 8;
+		public const int KcpOpcodeIndex = 0;
+		public const int OpcodeLength = 2;
+		public const int ActorIdIndex = 0;
+		public const int ActorIdLength = 8;
+		public const int MessageIndex = 10;
 
 		public ushort Opcode;
 		public long ActorId;
