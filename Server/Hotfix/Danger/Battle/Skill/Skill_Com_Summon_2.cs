8f871bd28b8231e158c48854203b88b619fa39fa
tangchunguang <394266315@qq.com>
2023-09-19 18:32:36 +0800

    no message

--- Server/Hotfix/Danger/Battle/Skill/Skill_Com_Summon_2.cs
+++ Server/Hotfix/Danger/Battle/Skill/Skill_Com_Summon_2.cs
@@ -21,14 +21,34 @@ namespace ET
             {
                 return;
             }
-            //'90000102;1;1;1;0.5,0.5,0.5,0.5,0.5;0,0,0,0,0
-            //召唤ID；是否复刻玩家形象（0不是，1是）；范围；数量；血量比例,攻击比例,魔法比例,物防比例，魔防比例；血量固定值,攻击固定值，魔法固定值，物防固定值，魔防固定值
+            //'90000102;1;1;1;0.5,0.5,0.5,0.5,0.5;0,0,0,0,0;1
+            //召唤ID；是否复刻玩家形象（0不是，1是）；范围；数量；血量比例,攻击比例,魔法比例,物防比例，魔防比例；血量固定值,攻击固定值，魔法固定值，物防固定值，魔防固定值;保留之前的怪
            
             string gameObjectParameter = this.SkillConf.GameObjectParameter;
             string[] summonParList = gameObjectParameter.Split(';');
             int monsterId = int.Parse(summonParList[0]);
             float range = float.Parse(summonParList[2]);
             int number = int.Parse(summonParList[3]);
+            int destoryOldMonster = 0;
+            if (gameObjectParameter.Length >= 7)
+            {
+                destoryOldMonster = int.Parse(summonParList[6]);
+            }
+
+            if (destoryOldMonster == 1)
+            {
+                List<Unit> entities = theUnitFrom.GetParent<UnitComponent>().GetAll();
+                for (int i = entities.Count - 1; i >= 0; i--)
+                {
+                    Unit uu = entities[i];
+                    if (uu.Type == UnitType.Monster && uu.ConfigId == monsterId && uu.MasterId == theUnitFrom.Id)
+                    {
+                        uu.GetComponent<HeroDataComponent>().OnDead(null);
+                        unitInfoComponent.ZhaohuanIds.Remove(uu.Id);
+                    }
+                }
+            }
+
             for (int y = 0; y < number; y++)
             {
                 //随机坐标
