59126ced7e924003ef3d9b088161776b3c5fabf1
tangchunguang2 <2322657661@qq.com>
2023-09-27 13:18:23 +0800

    no message

--- Unity/Assets/Hotfix/Module/Numeric/NumericHelp.cs
+++ Unity/Assets/Hotfix/Module/Numeric/NumericHelp.cs
@@ -17,6 +17,7 @@ namespace ET
             NumericType.Now_Shield_HP,
             NumericType.Now_Stall,
             NumericType.Now_Weapon,
+            NumericType.EquipIndex,
             NumericType.Now_MaxHp,
             NumericType.PetSkin,
             NumericType.UnionLeader,
