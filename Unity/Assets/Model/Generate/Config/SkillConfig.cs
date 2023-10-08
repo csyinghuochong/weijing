0c95f2db696268729ca88a80671a09c2ce9ba908
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2023-10-08 13:58:50 +0800

    更新配置

--- Unity/Assets/Model/Generate/Config/SkillConfig.cs
+++ Unity/Assets/Model/Generate/Config/SkillConfig.cs
@@ -126,152 +126,149 @@ namespace ET
 		/// <summary>连招技能ID</summary>
 		[ProtoMember(17)]
 		public int ComboSkillID { get; set; }
-		/// <summary>伤害目标类型</summary>
-		[ProtoMember(18)]
-		public int DamgeTargetType { get; set; }
 		/// <summary>技能攻击类型</summary>
-		[ProtoMember(19)]
+		[ProtoMember(18)]
 		public int SkillActType { get; set; }
 		/// <summary>伤害类型</summary>
-		[ProtoMember(20)]
+		[ProtoMember(19)]
 		public int DamgeType { get; set; }
 		/// <summary>伤害元素攻击</summary>
-		[ProtoMember(21)]
+		[ProtoMember(20)]
 		public int DamgeElementType { get; set; }
 		/// <summary>攻击系数</summary>
-		[ProtoMember(22)]
+		[ProtoMember(21)]
 		public double ActDamge { get; set; }
 		/// <summary>固定伤害值</summary>
-		[ProtoMember(23)]
+		[ProtoMember(22)]
 		public int DamgeValue { get; set; }
 		/// <summary>是否必中</summary>
-		[ProtoMember(24)]
+		[ProtoMember(23)]
 		public int IfMustAct { get; set; }
 		/// <summary>消耗魔法</summary>
-		[ProtoMember(25)]
+		[ProtoMember(24)]
 		public int SkillUseMP { get; set; }
 		/// <summary>增加魔法</summary>
-		[ProtoMember(26)]
+		[ProtoMember(25)]
 		public int SkillAddMP { get; set; }
 		/// <summary>是否触发公共CD</summary>
-		[ProtoMember(27)]
+		[ProtoMember(26)]
 		public int IfPublicSkillCD { get; set; }
 		/// <summary>是否触发冷却技能CD</summary>
-		[ProtoMember(28)]
+		[ProtoMember(27)]
 		public int IfSkillCD { get; set; }
 		/// <summary>冷却CD</summary>
-		[ProtoMember(29)]
+		[ProtoMember(28)]
 		public double SkillCD { get; set; }
 		/// <summary>伤害范围类型</summary>
-		[ProtoMember(30)]
+		[ProtoMember(29)]
 		public int DamgeRangeType { get; set; }
 		/// <summary>伤害范围</summary>
-		[ProtoMember(31)]
+		[ProtoMember(30)]
 		public double[] DamgeRange { get; set; }
 		/// <summary>技能目标类型</summary>
-		[ProtoMember(32)]
+		[ProtoMember(31)]
 		public int SkillTargetType { get; set; }
 		/// <summary>释放区域类型</summary>
-		[ProtoMember(33)]
+		[ProtoMember(32)]
 		public int SkillZhishiType { get; set; }
 		/// <summary>释放区域目标点类型</summary>
-		[ProtoMember(34)]
+		[ProtoMember(33)]
 		public int SkillZhishiTargetType { get; set; }
 		/// <summary>释放区域大小</summary>
-		[ProtoMember(35)]
+		[ProtoMember(34)]
 		public double SkillRangeSize { get; set; }
 		/// <summary>技能指示器增加范围</summary>
-		[ProtoMember(36)]
+		[ProtoMember(35)]
 		public int SkillRangeZhiShiSize { get; set; }
 		/// <summary>施法前吟唱时间</summary>
-		[ProtoMember(37)]
+		[ProtoMember(36)]
 		public double SkillFrontSingTime { get; set; }
 		/// <summary>施法中吟唱时间</summary>
-		[ProtoMember(38)]
+		[ProtoMember(37)]
 		public double SkillSingTime { get; set; }
 		/// <summary>技能僵直</summary>
-		[ProtoMember(39)]
+		[ProtoMember(38)]
 		public double SkillRigidity { get; set; }
 		/// <summary>技能存在时间[毫秒]</summary>
-		[ProtoMember(40)]
+		[ProtoMember(39)]
 		public int SkillLiveTime { get; set; }
 		/// <summary>技能效果延迟时间</summary>
-		[ProtoMember(41)]
+		[ProtoMember(40)]
 		public double SkillDelayTime { get; set; }
 		/// <summary>技能移动速度</summary>
-		[ProtoMember(42)]
+		[ProtoMember(41)]
 		public double SkillMoveSpeed { get; set; }
 		/// <summary>初始化BUFFID</summary>
-		[ProtoMember(43)]
+		[ProtoMember(42)]
 		public int[] InitBuffID { get; set; }
 		/// <summary>释放BUFFID</summary>
-		[ProtoMember(44)]
+		[ProtoMember(43)]
 		public int[] BuffID { get; set; }
 		/// <summary>施法动作名称</summary>
-		[ProtoMember(45)]
+		[ProtoMember(44)]
 		public string SkillAnimation { get; set; }
 		/// <summary>技能音效</summary>
-		[ProtoMember(46)]
+		[ProtoMember(45)]
 		public string SkillMusic { get; set; }
 		/// <summary>技能特效ID</summary>
-		[ProtoMember(47)]
+		[ProtoMember(46)]
 		public int SkillHitEffectID { get; set; }
 		/// <summary>技能特效ID</summary>
-		[ProtoMember(48)]
+		[ProtoMember(47)]
 		public int[] SkillEffectID { get; set; }
 		/// <summary>脚本名称</summary>
-		[ProtoMember(49)]
+		[ProtoMember(48)]
 		public string GameObjectName { get; set; }
 		/// <summary>每个脚本对应参数</summary>
-		[ProtoMember(50)]
+		[ProtoMember(49)]
 		public string GameObjectParameter { get; set; }
 		/// <summary>所有脚本通用参数</summary>
-		[ProtoMember(51)]
+		[ProtoMember(50)]
 		public string ComObjParameter { get; set; }
 		/// <summary>是否显示</summary>
-		[ProtoMember(52)]
+		[ProtoMember(51)]
 		public int IsShow { get; set; }
 		/// <summary>技能描述</summary>
-		[ProtoMember(53)]
+		[ProtoMember(52)]
 		public string SkillDescribe { get; set; }
 		/// <summary>施法时面对目标时间</summary>
-		[ProtoMember(54)]
+		[ProtoMember(53)]
 		public double IfLookAtTatgetTime { get; set; }
 		/// <summary>触发技能时附带技能</summary>
-		[ProtoMember(55)]
+		[ProtoMember(54)]
 		public int AddSkillID { get; set; }
 		/// <summary>技能触发时间</summary>
-		[ProtoMember(56)]
+		[ProtoMember(55)]
 		public double PassiveSkillTriggerTime { get; set; }
 		/// <summary>施法时是否面对目标</summary>
-		[ProtoMember(57)]
+		[ProtoMember(56)]
 		public int IfLookAtTarget { get; set; }
 		/// <summary>怪物技能延迟</summary>
-		[ProtoMember(58)]
+		[ProtoMember(57)]
 		public double MonsterDelayTime { get; set; }
 		/// <summary>宠物互斥ID</summary>
-		[ProtoMember(59)]
+		[ProtoMember(58)]
 		public int HuChiID { get; set; }
 		/// <summary>触发自身拥有技能</summary>
-		[ProtoMember(60)]
+		[ProtoMember(59)]
 		public int[] TriggerSelfSkillID { get; set; }
 		/// <summary>释放技能是否打断移动</summary>
-		[ProtoMember(61)]
+		[ProtoMember(60)]
 		public int IfStopMove { get; set; }
 		/// <summary>技能持续伤害是否触发Buff</summary>
-		[ProtoMember(62)]
+		[ProtoMember(61)]
 		public int DamgeChiXuTrigerBuff { get; set; }
 		/// <summary>技能持续伤害间隔时间</summary>
-		[ProtoMember(63)]
+		[ProtoMember(62)]
 		public int DamgeChiXuInterval { get; set; }
 		/// <summary>技能持续伤害百分比</summary>
-		[ProtoMember(64)]
+		[ProtoMember(63)]
 		public double DamgeChiXuPro { get; set; }
 		/// <summary>技能持续伤害固定值</summary>
-		[ProtoMember(65)]
+		[ProtoMember(64)]
 		public int DamgeChiXuValue { get; set; }
 		/// <summary>是否显示技能指示器字段</summary>
-		[ProtoMember(66)]
+		[ProtoMember(65)]
 		public int IfShowSkillZhiShi { get; set; }
 
 	}
