bbf07097ecd26fa5aa95f0c4af702331edeb4de4
tangchunguang <394266315@qq.com>
2023-10-09 20:06:04 +0800

    no message

--- Server/Hotfix/Danger/Battle/Skill/Skill_Com_Summon_2.cs
+++ Server/Hotfix/Danger/Battle/Skill/Skill_Com_Summon_2.cs
@@ -21,27 +21,34 @@ namespace ET
             {
                 return;
             }
-            //'90000102;1;1;1;0.5,0.5,0.5,0.5,0.5;0,0,0,0,0;1
+            //'90000102;1;1;1;0.5,0.5,0.5,0.5,0.5;0,0,0,0,0;90000102,90000103
             //召唤ID；是否复刻玩家形象（0不是，1是）；范围；数量；血量比例,攻击比例,魔法比例,物防比例，魔防比例；血量固定值,攻击固定值，魔法固定值，物防固定值，魔防固定值;保留之前的怪
-           
+
             string gameObjectParameter = this.SkillConf.GameObjectParameter;
             string[] summonParList = gameObjectParameter.Split(';');
             int monsterId = int.Parse(summonParList[0]);
             float range = float.Parse(summonParList[2]);
             int number = int.Parse(summonParList[3]);
-            int destoryOldMonster = 0;
+            List<int> destoryOldMonsterList = new List<int>();
             if (summonParList.Length >= 7)
             {
-                destoryOldMonster = int.Parse(summonParList[6]);
+                string[] monsterids = summonParList[6].Split(",");
+                for (int i = 0; i < monsterids.Length; i++)
+                {
+                    if (!ComHelp.IfNull(monsterids[i]))
+                    {
+                        destoryOldMonsterList.Add( int.Parse(monsterids[i]) );
+                    }
+                }
             }
 
-            if (destoryOldMonster == 1)
+            if (destoryOldMonsterList.Count > 0)
             {
                 List<Unit> entities = theUnitFrom.GetParent<UnitComponent>().GetAll();
                 for (int i = entities.Count - 1; i >= 0; i--)
                 {
                     Unit uu = entities[i];
-                    if (uu.Type == UnitType.Monster && uu.ConfigId == monsterId && uu.MasterId == theUnitFrom.Id)
+                    if (uu.Type == UnitType.Monster && destoryOldMonsterList.Contains(uu.ConfigId) && uu.MasterId == theUnitFrom.Id)
                     {
                         uu.GetComponent<HeroDataComponent>().OnDead(null);
                         unitInfoComponent.ZhaohuanIds.Remove(uu.Id);
