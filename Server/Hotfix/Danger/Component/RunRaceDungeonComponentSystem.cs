c869a885c5383c286d0bd2b3417ccc01bd531e2f
tangchunguang <394266315@qq.com>
2023-10-13 18:16:52 +0800

    no message

--- Server/Hotfix/Danger/Component/RunRaceDungeonComponentSystem.cs
+++ Server/Hotfix/Danger/Component/RunRaceDungeonComponentSystem.cs
@@ -85,6 +85,12 @@ namespace ET
             self.OnTransform();
 
             self.OnPullBack();
+
+            //生成怪物
+            int sceneid = self.DomainScene().GetComponent<MapComponent>().SceneId;
+            SceneConfig sceneConfig = SceneConfigCategory.Instance.Get(sceneid);
+            FubenHelp.CreateMonsterList(self.DomainScene(), sceneConfig.CreateMonster);
+            FubenHelp.CreateMonsterList(self.DomainScene(), sceneConfig.CreateMonsterPosi);
         }
 
         /// <summary>
