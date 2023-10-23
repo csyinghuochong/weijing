ae6d49500169dfd89f2c986b4f6fb09d7501dbde
tangchunguang <394266315@qq.com>
2023-10-23 11:41:16 +0800

    no message

--- Unity/Assets/Model/Danger/Common/Component/SeasonComponent.cs
+++ Unity/Assets/Model/Danger/Common/Component/SeasonComponent.cs
@@ -11,26 +11,12 @@ namespace ET
     #endif
     {
 
-        ///赛季任务 赛季晶核
 
+        ////赛季主线任务. 在TaskComponent.RoleTaskList  
 
-        /// <summary>
-        /// 赛季主线任务
-        /// </summary>
-        public List<TaskPro> MainTask = new List<TaskPro>();    
+        /// 赛季每次任务. 在TaskComponent.TaskCountryList
 
-
-        /// <summary>
-        /// 赛季每次任务
-        /// </summary>
-        public List<TaskPro> EverydayTask =new List<TaskPro>();
-
-
-        /// <summary>
-        /// 开启的晶核位置。 对应的装备在bagcomponent。  类似于生肖系统
-        /// </summary>
-        public List<int> OpenJingHeIds = new List<int>();   
-
+        /// 开启的晶核位置，在userfo。 对应的装备在bagcomponent 类似于生肖系统
 
     }
 }
