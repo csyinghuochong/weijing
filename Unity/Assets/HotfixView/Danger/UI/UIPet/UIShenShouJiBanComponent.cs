7f5500f46591d78e57e7aa183561e8d2e38e7faa
tangchunguang <394266315@qq.com>
2023-11-09 20:44:24 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIPet/UIShenShouJiBanComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIPet/UIShenShouJiBanComponent.cs
@@ -114,7 +114,7 @@ namespace ET
                 }
 
                 go.GetComponent<Text>().text = str;
-                go.GetComponent<Text>().color = shenshouNumber >= i ? new Color(110f, 140f, 7f, 255f) : new Color(128f,128f,128f, 255f);
+                go.GetComponent<Text>().color = shenshouNumber >= i ? new Color(110f/225f, 140f / 225f, 7f / 225f, 1f) : new Color(128f / 225f, 128f / 225f, 128f / 225f, 1f);
 
                 UICommonHelper.SetParent(go, self.EffectListNode);
                 go.SetActive(true);
