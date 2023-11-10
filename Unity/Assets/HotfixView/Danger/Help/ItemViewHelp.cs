b6acf6f2c4ce57c4d07d2b442c9a569c46756fc0
FortuneCat0v0 <1142094319@qq.com>
2023-11-10 11:01:04 +0800

    道具 15310011 的技能描述有点错位

--- Unity/Assets/HotfixView/Danger/Help/ItemViewHelp.cs
+++ Unity/Assets/HotfixView/Danger/Help/ItemViewHelp.cs
@@ -1209,17 +1209,23 @@ namespace ET
             //显示描述
             if (itemconf.ItemDes != "" && itemconf.ItemDes != "0" && itemconf.ItemDes != null)
             {
-                int allLength = itemconf.ItemDes.Length;
-                int addNum = Mathf.CeilToInt(allLength / 18f);
-                for (int a = 0; a < addNum; a++)
+                string[] des = itemconf.ItemDes.Split('\n');
+                foreach (string s in des)
                 {
-                    int leftNum = allLength - a * 18;
-                    leftNum = Math.Min(leftNum, 18);
-                    ShowPropertyText(itemconf.ItemDes.Substring(a * 18, leftNum), "1", Obj_EquipPropertyText, Obj_EquipBaseSetList);
+                    int allLength = s.Length;
+                    int addNum = Mathf.CeilToInt(allLength / 18f);
+                    for (int a = 0; a < addNum; a++)
+                    {
+                        int leftNum = allLength - a * 18;
+                        leftNum = Math.Min(leftNum, 18);
+                        ShowPropertyText(s.Substring(a * 18, leftNum), "1", Obj_EquipPropertyText, Obj_EquipBaseSetList);
+                    }
+
+                    properShowNum += addNum;
                 }
+
                 //int zifuLenght = GetNumbers(itemconf.ItemDes) + GetTeShu(itemconf.ItemDes);
                 //int lenght = (allLength - zifuLenght) + (int)(zifuLenght * 0.5f);
-                properShowNum += addNum;
             }
 
             //显示传承技能
