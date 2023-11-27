a896f7413c9c364bf7d46d9fa421f56eeb5744e4
tangchunguang <394266315@qq.com>
2023-11-27 16:17:21 +0800

    no message

--- Server/Hotfix/Danger/Handler/Map/Skill/C2M_SkillInitHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/Skill/C2M_SkillInitHandler.cs
@@ -5,134 +5,134 @@ namespace ET
 {
     //设置技能位置
     [ActorMessageHandler]
-	public class C2M_SkillInitHandler : AMActorLocationRpcHandler<Unit, C2M_SkillInitRequest, M2C_SkillInitResponse>
-	{
-		protected override async ETTask Run(Unit unit, C2M_SkillInitRequest request, M2C_SkillInitResponse response, Action reply)
-		{
+    public class C2M_SkillInitHandler : AMActorLocationRpcHandler<Unit, C2M_SkillInitRequest, M2C_SkillInitResponse>
+    {
+        protected override async ETTask Run(Unit unit, C2M_SkillInitRequest request, M2C_SkillInitResponse response, Action reply)
+        {
 
             int occ = unit.GetComponent<UserInfoComponent>().UserInfo.Occ;
-			int occTwo = unit.GetComponent<UserInfoComponent>().UserInfo.OccTwo;
+            int occTwo = unit.GetComponent<UserInfoComponent>().UserInfo.OccTwo;
             SkillSetComponent skillSetComponent = unit.GetComponent<SkillSetComponent>();
-			response.SkillSetInfo = new SkillSetInfo();
+            response.SkillSetInfo = new SkillSetInfo();
 
-			//for (int i = skillSetComponent.SkillList.Count - 1; i >= 0; i--)
-			//{
-			//	SkillPro skillPro = skillSetComponent.SkillList[i];
-			//	if (skillPro.SkillPosition == 0)
-			//	{
-			//		continue;
-			//	}
-			//	if (skillPro.SkillSetType == (int)SkillSetEnum.Skill)
-			//	{
-			//		if (!SkillConfigCategory.Instance.Contain(skillPro.SkillID))
-			//		{
-			//			skillSetComponent.SkillList.RemoveAt(i);
-			//		}
-			//		continue;
-			//	}
-			//}
+            //for (int i = skillSetComponent.SkillList.Count - 1; i >= 0; i--)
+            //{
+            //	SkillPro skillPro = skillSetComponent.SkillList[i];
+            //	if (skillPro.SkillPosition == 0)
+            //	{
+            //		continue;
+            //	}
+            //	if (skillPro.SkillSetType == (int)SkillSetEnum.Skill)
+            //	{
+            //		if (!SkillConfigCategory.Instance.Contain(skillPro.SkillID))
+            //		{
+            //			skillSetComponent.SkillList.RemoveAt(i);
+            //		}
+            //		continue;
+            //	}
+            //}
 
-			//强化技能可以激活
-			bool haveqianghuaskill = false;
-			for (int i = 0; i < skillSetComponent.SkillList.Count; i++)
-			{
-				if (SkillHelp.IsQiangHuaSkill(occ, skillSetComponent.SkillList[i].SkillID))
-				{
-					haveqianghuaskill = true;
-					break;
-				}
-			}
-
-			if (!haveqianghuaskill)
-			{
-				int[] baseSkilllist = OccupationConfigCategory.Instance.Get(occ).BaseSkill;
-				for( int i = 0; i < baseSkilllist.Length; i++ )
-				{
-					if (skillSetComponent.GetBySkillID(baseSkilllist[i]) !=null)
-					{
-						continue;
-					}
-					skillSetComponent.SkillList.Add(new SkillPro() { SkillID = baseSkilllist[i] });
+            //强化技能可以激活
+            bool haveqianghuaskill = false;
+            for (int i = 0; i < skillSetComponent.SkillList.Count; i++)
+            {
+                if (SkillHelp.IsQiangHuaSkill(occ, skillSetComponent.SkillList[i].SkillID))
+                {
+                    haveqianghuaskill = true;
+                    break;
                 }
             }
 
-			//刷新转职技能
-			if ( occTwo != 0)
-			{
-				///移除重复的转职技能
-
-				OccupationTwoConfig occupationTwo = OccupationTwoConfigCategory.Instance.Get(occTwo);
-
-				List<int> occTwoSkillList = new List<int>(occupationTwo.SkillID) {  };
-				List<int> selfoccTwoSkill = new List<int>() { };
-
-				List<int> removeList = new List<int>() { };
-                for (int i = 0; i < skillSetComponent.SkillList.Count; i++)
-				{
-					int initskillid = SkillConfigCategory.Instance.GetInitSkill(skillSetComponent.SkillList[i].SkillID);
-					if (initskillid == 0)
-					{
-						continue;
-					}
-					if (!occTwoSkillList.Contains(initskillid))
+            if (!haveqianghuaskill)
+            {
+                int[] baseSkilllist = OccupationConfigCategory.Instance.Get(occ).BaseSkill;
+                for (int i = 0; i < baseSkilllist.Length; i++)
+                {
+                    if (skillSetComponent.GetBySkillID(baseSkilllist[i]) != null)
                     {
-						continue;
+                        continue;
+                    }
+                    skillSetComponent.SkillList.Add(new SkillPro() { SkillID = baseSkilllist[i] });
+                }
+            }
+
+            //刷新转职技能
+            if (occTwo != 0)
+            {
+                ///移除重复的转职技能
+
+                OccupationTwoConfig occupationTwo = OccupationTwoConfigCategory.Instance.Get(occTwo);
+
+                List<int> occTwoSkillList = new List<int>(occupationTwo.SkillID) { };
+                List<int> selfoccTwoSkill = new List<int>() { };
+
+                int removeSkillIndex = 0;
+                for (int i = 0; i < skillSetComponent.SkillList.Count; i++)
+                {
+                    int initskillid = SkillConfigCategory.Instance.GetInitSkill(skillSetComponent.SkillList[i].SkillID);
+                    if (initskillid == 0)
+                    {
+                        continue;
+                    }
+                    if (!occTwoSkillList.Contains(initskillid))
+                    {
+                        continue;
                     }
 
-					if (selfoccTwoSkill.Contains(initskillid))
-					{
-                        removeList.Add( i );
+                    if (selfoccTwoSkill.Contains(initskillid))
+                    {
+                        removeSkillIndex = (i);
                     }
-					else
-					{
+                    else
+                    {
                         selfoccTwoSkill.Add(initskillid);
                     }
                 }
 
-				for (int i = 0; i < removeList.Count; i++)
-				{
-					skillSetComponent.SkillList.RemoveAt(removeList[i]);
+                if (removeSkillIndex != 0)
+                {
+                    skillSetComponent.SkillList.RemoveAt(removeSkillIndex);
                 }
 
                 for (int i = 0; i < occTwoSkillList.Count; i++)
-				{
-					if (selfoccTwoSkill.Contains(occTwoSkillList[i]))
-					{
-						continue;
-					}
+                {
+                    if (selfoccTwoSkill.Contains(occTwoSkillList[i]))
+                    {
+                        continue;
+                    }
 
                     skillSetComponent.SkillList.Add(new SkillPro() { SkillID = occTwoSkillList[i] });
                 }
-			}
+            }
 
             response.SkillSetInfo.SkillList = skillSetComponent.SkillList;
-			response.SkillSetInfo.LifeShieldList = skillSetComponent.LifeShieldList;
-			response.SkillSetInfo.TianFuPlan = skillSetComponent.TianFuPlan;
+            response.SkillSetInfo.LifeShieldList = skillSetComponent.LifeShieldList;
+            response.SkillSetInfo.TianFuPlan = skillSetComponent.TianFuPlan;
 
-			List<int> tianfulist = new List<int>();
-			for (int i = 0; i < skillSetComponent.TianFuList.Count; i++)
-			{
-				if (!tianfulist.Contains(skillSetComponent.TianFuList[i]))
-				{
-					tianfulist.Add(skillSetComponent.TianFuList[i]);
-				}
-			}
-			response.SkillSetInfo.TianFuList = tianfulist;
-			skillSetComponent.TianFuList = tianfulist;
+            List<int> tianfulist = new List<int>();
+            for (int i = 0; i < skillSetComponent.TianFuList.Count; i++)
+            {
+                if (!tianfulist.Contains(skillSetComponent.TianFuList[i]))
+                {
+                    tianfulist.Add(skillSetComponent.TianFuList[i]);
+                }
+            }
+            response.SkillSetInfo.TianFuList = tianfulist;
+            skillSetComponent.TianFuList = tianfulist;
 
-			List<int> tianfulist1 = new List<int>();
-			for (int i = 0; i < skillSetComponent.TianFuList1.Count; i++)
-			{
-				if (!tianfulist1.Contains(skillSetComponent.TianFuList1[i]))
-				{
-					tianfulist1.Add(skillSetComponent.TianFuList1[i]);
-				}
-			}
-			response.SkillSetInfo.TianFuList1 = tianfulist1;
-			skillSetComponent.TianFuList1 = tianfulist1;
+            List<int> tianfulist1 = new List<int>();
+            for (int i = 0; i < skillSetComponent.TianFuList1.Count; i++)
+            {
+                if (!tianfulist1.Contains(skillSetComponent.TianFuList1[i]))
+                {
+                    tianfulist1.Add(skillSetComponent.TianFuList1[i]);
+                }
+            }
+            response.SkillSetInfo.TianFuList1 = tianfulist1;
+            skillSetComponent.TianFuList1 = tianfulist1;
 
-			reply();
-			await ETTask.CompletedTask;
-		}
-	}
+            reply();
+            await ETTask.CompletedTask;
+        }
+    }
 }
