13d6396a6bbef01e6c7ce2916fdc79d2ab592c8d
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2023-11-30 20:30:08 +0800

    更新

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -270,7 +270,10 @@ namespace ET
                 serverItems_1.Add(new ServerItem() { ServerId = 54, ServerIp = $"{ip}:20395", ServerName = "百年风华", ServerOpenTime = 1698985800000, New = 0, Show = 1 });
                 serverItems_1.Add(new ServerItem() { ServerId = 55, ServerIp = $"{ip}:20435", ServerName = "盛世如愿", ServerOpenTime = 1699590600000, New = 0, Show = 1 });
                 serverItems_1.Add(new ServerItem() { ServerId = 56, ServerIp = $"{ip}:20455", ServerName = "精灵国度", ServerOpenTime = 1700195400000, New = 0, Show = 1 });
-                serverItems_1.Add(new ServerItem() { ServerId = 57, ServerIp = $"{ip}:20375", ServerName = "梦回华夏", ServerOpenTime = 1700800200000, New = 1, Show = 1 });
+                serverItems_1.Add(new ServerItem() { ServerId = 57, ServerIp = $"{ip}:20375", ServerName = "梦回华夏", ServerOpenTime = 1700800200000, New = 0, Show = 1 });
+
+                serverItems_1.Add(new ServerItem() { ServerId = 58, ServerIp = $"{ip}:20475", ServerName = "璀璨之境", ServerOpenTime = 1701405000000, New = 1, Show = 1 });
+
             }
 
             return serverItems_1;
