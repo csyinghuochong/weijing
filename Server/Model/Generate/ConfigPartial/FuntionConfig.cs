c41dae3fb3dfedc73c9a740e99346f142fb338f8
tangchunguang <394266315@qq.com>
2023-12-04 10:33:25 +0800

    no message

---
+++ Server/Model/Generate/ConfigPartial/FuntionConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+namespace ET
+{
+    public partial class FuntionConfigCategory
+    {
+
+        public override void AfterEndInit()
+        {
+            foreach (FuntionConfig activityConfig in this.GetAll().Values)
+            {
+               
+            }
+        }
+    }
+}
