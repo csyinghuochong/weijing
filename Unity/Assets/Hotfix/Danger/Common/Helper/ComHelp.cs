cf4b771954870805e345a9c6f79170cccc7c1b02
tangchunguang <394266315@qq.com>
2023-12-06 22:27:17 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ComHelp.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ComHelp.cs
@@ -21,7 +21,7 @@ namespace ET
             return zone == 200;
         }
 
-        public const int Version = 20231006;
+        public const int Version = 20231206;
 
         public const string RobotPassWord = "et@#robot";
 
