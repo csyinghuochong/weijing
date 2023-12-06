e14aae44f20e19bd4055ebff685a801ebc53a719
FortuneCat0v0 <1142094319@qq.com>
2023-12-06 19:39:12 +0800

    闪电链3，未完...

---
+++ Unity/Assets/Hotfix/Danger/Skill/Skill_ChainLightning_3.cs
@@ -0,0 +1,11 @@
+﻿namespace ET
+{
+    [SkillHandler]
+    public class Skill_ChainLightning_3: Skill_Action_Common
+    {
+        public override void OnExecute()
+        {
+            this.OnUpdate();
+        }
+    }
+}
--- Unity/Assets/HotfixView/Danger/Handler/Skill/Skill_OnSkillChainLight.cs
+++ Unity/Assets/HotfixView/Danger/Handler/Skill/Skill_OnSkillChainLight.cs
@@ -7,8 +7,8 @@ namespace ET
         {
             EventType.SkillChainLight args = numerice as EventType.SkillChainLight;
             UnitComponent unitComponent = args.ZoneScene.CurrentScene().GetComponent<UnitComponent>();
-            Unit Unit = unitComponent.Get(args.M2C_ChainLightning.UnitId);
-            if (Unit == null)
+            Unit start = unitComponent.Get(args.M2C_ChainLightning.UnitId);
+            if (start == null)
             {
                 return;
             }
@@ -18,16 +18,55 @@ namespace ET
                 return;
             }
 
-            EffectData playEffectBuffData = new EffectData();
-            playEffectBuffData.TargetID = args.M2C_ChainLightning.TargetID;
-            playEffectBuffData.SkillId = args.M2C_ChainLightning.SkillID;                   //技能相关配置
-            SkillConfig skillConfig = SkillConfigCategory.Instance.Get(playEffectBuffData.SkillId);
-            playEffectBuffData.EffectId = skillConfig.SkillEffectID[0];                 //特效相关配置
-            playEffectBuffData.EffectPosition = new UnityEngine.Vector3(args.M2C_ChainLightning.PosX, args.M2C_ChainLightning.PosY, args.M2C_ChainLightning.PosZ);           //技能目标点
-            playEffectBuffData.TargetAngle = 0;         //技能角度
-            playEffectBuffData.EffectTypeEnum = EffectTypeEnum.SkillEffect;              //特效类型
-            playEffectBuffData.InstanceId = target.InstanceId;
-            Unit.GetComponent<EffectViewComponent>()?.EffectFactory(playEffectBuffData);
+            switch (args.M2C_ChainLightning.Type)
+            {
+                case 0:
+                {
+                    EffectData playEffectBuffData = new EffectData();
+                    playEffectBuffData.TargetID = args.M2C_ChainLightning.TargetID;
+                    playEffectBuffData.SkillId = args.M2C_ChainLightning.SkillID; //技能相关配置
+                    SkillConfig skillConfig = SkillConfigCategory.Instance.Get(playEffectBuffData.SkillId);
+                    playEffectBuffData.EffectId = skillConfig.SkillEffectID[0]; //特效相关配置
+                    playEffectBuffData.EffectPosition =
+                            new UnityEngine.Vector3(args.M2C_ChainLightning.PosX, args.M2C_ChainLightning.PosY, args.M2C_ChainLightning.PosZ); //技能目标点
+                    playEffectBuffData.TargetAngle = 0; //技能角度
+                    playEffectBuffData.EffectTypeEnum = EffectTypeEnum.SkillEffect; //特效类型
+                    playEffectBuffData.InstanceId = target.InstanceId;
+                    start.GetComponent<EffectViewComponent>()?.EffectFactory(playEffectBuffData);
+                    break;
+                }
+                case 3:
+                {
+                    EffectViewComponent effectViewComponent = start.GetComponent<EffectViewComponent>();
+                    if (effectViewComponent == null)
+                    {
+                        return;
+                    }
+                    AEffectHandler aEffectHandler = effectViewComponent.GetEffect(args.M2C_ChainLightning.InstanceId);
+
+                    if (aEffectHandler == null)
+                    {
+                        EffectData playEffectBuffData = new EffectData();
+                        playEffectBuffData.TargetID = args.M2C_ChainLightning.TargetID;
+                        playEffectBuffData.SkillId = args.M2C_ChainLightning.SkillID; //技能相关配置
+                        SkillConfig skillConfig = SkillConfigCategory.Instance.Get(playEffectBuffData.SkillId);
+                        playEffectBuffData.EffectId = skillConfig.SkillEffectID[0]; //特效相关配置
+                        playEffectBuffData.EffectPosition =
+                                new UnityEngine.Vector3(args.M2C_ChainLightning.PosX, args.M2C_ChainLightning.PosY, args.M2C_ChainLightning.PosZ); //技能目标点
+                        playEffectBuffData.TargetAngle = 0; //技能角度
+                        playEffectBuffData.EffectTypeEnum = EffectTypeEnum.SkillEffect; //特效类型
+                        playEffectBuffData.InstanceId = args.M2C_ChainLightning.InstanceId;
+                        start.GetComponent<EffectViewComponent>()?.EffectFactory(playEffectBuffData);
+                    }
+                    else
+                    {
+                        aEffectHandler.GetComponent<ChainLightningComponent>().Start = target.GetComponent<HeroTransformComponent>().GetTranform(PosType.Center);
+                        aEffectHandler.GetComponent<ChainLightningComponent>().End = target.GetComponent<HeroTransformComponent>().GetTranform(PosType.Center);
+                    }
+
+                    break;
+                }
+            }
         }
     }
 }
