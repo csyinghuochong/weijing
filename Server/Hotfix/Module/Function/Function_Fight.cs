57840ec1a6279a46b7537f44ab890e2b834b0a6a
tangchunguang <394266315@qq.com>
2023-12-10 22:32:21 +0800

    no message

--- Server/Hotfix/Module/Function/Function_Fight.cs
+++ Server/Hotfix/Module/Function/Function_Fight.cs
@@ -570,6 +570,11 @@ namespace ET
                     }
                 }
 
+                if (attackUnit.Type == UnitType.Monster && attackUnit.MasterIsPlayer())
+                {
+                    Log.Console("玩家召唤的怪物！");
+                }
+
                 //宠物打宠物只造成50%的伤害
                 if (attackUnit.Type == UnitType.Pet && defendUnit.Type == UnitType.Pet)
                 {
