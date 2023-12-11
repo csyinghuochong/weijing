86a4470ca35e8ba366c6f28de5c28f9968cd2ea3
tangchunguang <394266315@qq.com>
2023-12-11 12:02:39 +0800

    no message

--- Server/Hotfix/Danger/Battle/Skill/Skill_Other_ChongJi_1.cs
+++ Server/Hotfix/Danger/Battle/Skill/Skill_Other_ChongJi_1.cs
@@ -30,19 +30,19 @@ namespace ET
             //    Vector3 direction = targetUnit.Position - this.TheUnitFrom.Position;
             //    this.SkillInfo.TargetAngle = (int)Mathf.Rad2Deg(Mathf.Atan2(direction.x, direction.z));
             //}
-
-            float oldSpeed = this.TheUnitFrom.GetComponent<NumericComponent>().GetAsFloat(NumericType.Now_Speed);
-            float oldspeedAdd = this.TheUnitFrom.GetComponent<NumericComponent>().GetAsFloat(NumericType.Extra_Buff_Speed_Add);
+            NumericComponent numericComponent = this.TheUnitFrom.GetComponent<NumericComponent>();
+            float oldSpeed = numericComponent.GetAsFloat(NumericType.Now_Speed);
+            float oldspeedAdd = numericComponent.GetAsFloat(NumericType.Extra_Buff_Speed_Add);
             //float moveDistance = ((float)this.SkillConf.SkillMoveSpeed * this.SkillConf.SkillLiveTime * 0.001f);
             //Quaternion rotation = Quaternion.Euler(0, this.SkillInfo.TargetAngle, 0); //按照Z轴旋转30度的Quaterion
             //this.TargetPosition = theUnitFrom.Position + rotation * Vector3.forward * moveDistance;
             //this.TargetPosition = theUnitFrom.DomainScene().GetComponent<MapComponent>().GetCanChongJiPath(theUnitFrom.Position, TargetPosition);
             //1-10 表示 10%-100%
-            double addPro = (double)this.TheUnitFrom.GetComponent<NumericComponent>().GetAsInt(NumericType.Now_JumpDisAdd) / 10;
+            double addPro = (double)numericComponent.GetAsInt(NumericType.Now_JumpDisAdd) / 10;
             float newSpeed = (float)(this.SkillConf.SkillMoveSpeed * (1 + addPro));
             float newspeedAdd = newSpeed - oldSpeed;
             this.SpeedAddValue = newspeedAdd - oldspeedAdd;
-            this.TheUnitFrom.GetComponent<NumericComponent>().Set(NumericType.Extra_Buff_Speed_Add, newspeedAdd);
+            numericComponent.Set(NumericType.Extra_Buff_Speed_Add, newspeedAdd);
             float moveDistance = ((float)this.SkillConf.SkillMoveSpeed * this.SkillConf.SkillLiveTime * 0.001f);
             Quaternion rotation = Quaternion.Euler(0, this.SkillInfo.TargetAngle, 0); //按照Z轴旋转30度的Quaterion
             this.TargetPosition = this.TheUnitFrom.Position + rotation * Vector3.forward * moveDistance;
@@ -112,10 +112,9 @@ namespace ET
             //float speed_mul = cur_mul - this.AddSpeed_Mul;
             //Log.Debug($"OnFinished {speed_mul}");
             //TheUnitFrom.GetComponent<NumericComponent>().Set(NumericType.Extra_Buff_Speed_Mul, Mathf.Max(0, speed_mul));
-
-            float curspeedAdd = this.TheUnitFrom.GetComponent<NumericComponent>().GetAsFloat(NumericType.Extra_Buff_Speed_Add);
-            curspeedAdd -= this.SpeedAddValue;
-            this.TheUnitFrom.GetComponent<NumericComponent>().Set(NumericType.Extra_Buff_Speed_Add, Mathf.Max(0, curspeedAdd));
+            NumericComponent numericComponent = this.TheUnitFrom.GetComponent<NumericComponent>();
+            float curspeedAdd = numericComponent.GetAsFloat(NumericType.Extra_Buff_Speed_Add) - this.SpeedAddValue;
+            numericComponent.Set(NumericType.Extra_Buff_Speed_Add, Mathf.Max(0, curspeedAdd));
             this.Clear();
         }
     }
