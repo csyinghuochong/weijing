b934df378a2260329d94e2f1db0c9fdd76185335
tangchunguang <394266315@qq.com>
2023-12-15 17:22:48 +0800

    no message

--- Server/Hotfix/Danger/Component/PetComponentSystem.cs
+++ Server/Hotfix/Danger/Component/PetComponentSystem.cs
@@ -936,6 +936,10 @@ namespace ET
                 List<int> unionXiuLianids = new List<int>() { xiuLian_0, xiuLian_1, xiuLian_2, xiuLian_3 };
                 for (int i = 0; i < unionXiuLianids.Count; i++)
                 {
+                    if (unionXiuLianids[i] == 0)
+                    {
+                        continue;
+                    }
                     UnionQiangHuaConfig unionQiangHuaCof = UnionQiangHuaConfigCategory.Instance.Get(unionXiuLianids[i]);
                     List<PropertyValue> jiazuProList = new List<PropertyValue>();
                     NumericHelp.GetProList(unionQiangHuaCof.EquipPropreAdd, jiazuProList);
