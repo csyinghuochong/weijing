e31395da64eca97d1f1963e039f41054bae3c5b8
FortuneCat0v0 <1142094319@qq.com>
2023-12-22 12:53:05 +0800

    🕹️

--- Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1ChouKaComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1ChouKaComponent.cs
@@ -1,6 +1,99 @@
-﻿namespace ET
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
 {
     public class UIActivityV1ChouKaComponent: Entity, IAwake
     {
+        public GameObject UIActivityV1ChouKaListNode;
+        public GameObject UIActivityV1ChouKaItem;
+        public GameObject RewardItemListNode;
+        public GameObject CostItemListNode;
+        public GameObject OpenBtn;
+
+        public List<UIActivityV1ChouKaItemComponent> UIActivityV1ChouKaItemComponents = new List<UIActivityV1ChouKaItemComponent>();
+        public List<UIItemComponent> UIItemComponents = new List<UIItemComponent>();
+    }
+
+    public class UIActivityV1ChouKaComponentAwake: AwakeSystem<UIActivityV1ChouKaComponent>
+    {
+        public override void Awake(UIActivityV1ChouKaComponent self)
+        {
+            ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
+
+            self.UIActivityV1ChouKaListNode = rc.Get<GameObject>("UIActivityV1ChouKaListNode");
+            self.UIActivityV1ChouKaItem = rc.Get<GameObject>("UIActivityV1ChouKaItem");
+            self.RewardItemListNode = rc.Get<GameObject>("RewardItemListNode");
+            self.CostItemListNode = rc.Get<GameObject>("CostItemListNode");
+            self.OpenBtn = rc.Get<GameObject>("OpenBtn");
+
+            self.UIActivityV1ChouKaItem.SetActive(false);
+            self.OpenBtn.GetComponent<Button>().onClick.AddListener(() => { self.OnOpenBtn().Coroutine(); });
+
+            self.InitInfo();
+        }
+    }
+
+    public static class UIActivityV1ChouKaComponentSystem
+    {
+        public static async ETTask OnOpenBtn(this UIActivityV1ChouKaComponent self)
+        {
+            BagComponent bagComponent = self.ZoneScene().GetComponent<BagComponent>();
+            if (bagComponent.GetLeftSpace() < 1)
+            {
+                FloatTipManager.Instance.ShowFloatTip("背包空间不足");
+                return;
+            }
+
+            if (!bagComponent.CheckNeedItem(ActivityConfigHelper.ChouKaCostItem))
+            {
+                FloatTipManager.Instance.ShowFloatTip("所需道具不足");
+                return;
+            }
+
+            C2M_ActivityChouKaRequest request = new C2M_ActivityChouKaRequest();
+            M2C_ActivityChouKaResponse response =
+                    (M2C_ActivityChouKaResponse)await self.ZoneScene().GetComponent<SessionComponent>().Session.Call(request);
+
+            if (response.Error != ErrorCode.ERR_Success)
+            {
+                return;
+            }
+        }
+
+        public static void InitInfo(this UIActivityV1ChouKaComponent self)
+        {
+            foreach (int key in ActivityConfigHelper.ChouKaNumberReward.Keys)
+            {
+                GameObject go = UnityEngine.Object.Instantiate(self.UIActivityV1ChouKaItem);
+                UIActivityV1ChouKaItemComponent component = self.AddChild<UIActivityV1ChouKaItemComponent, GameObject>(go);
+                component.OnUpdateData(key);
+                UICommonHelper.SetParent(go, self.UIActivityV1ChouKaListNode);
+                go.SetActive(true);
+            }
+
+            List<int> rewardShowItems = new List<int> { 601901001, 601901002, 601901003 };
+            for (int i = 0; i < rewardShowItems.Count; i++)
+            {
+                if (!ItemConfigCategory.Instance.Contain(rewardShowItems[i]))
+                {
+                    continue;
+                }
+
+                var path = ABPathHelper.GetUGUIPath("Main/Role/UITreasureItem");
+                var bundleGameObject = ResourcesComponent.Instance.LoadAsset<GameObject>(path);
+                GameObject itemSpace = GameObject.Instantiate(bundleGameObject);
+                UICommonHelper.SetParent(itemSpace, self.RewardItemListNode);
+                UIItemComponent uIItemComponent = self.AddChild<UIItemComponent, GameObject>(itemSpace);
+                uIItemComponent.UpdateItem(new BagInfo() { ItemID = rewardShowItems[i], ItemNum = 0 }, ItemOperateEnum.None);
+                //uIItemComponent.Label_ItemName.SetActive(false);
+                uIItemComponent.Label_ItemNum.SetActive(false);
+                itemSpace.transform.localScale = Vector3.one * 1f;
+                self.UIItemComponents.Add(uIItemComponent);
+            }
+
+            UICommonHelper.ShowItemList(ActivityConfigHelper.ChouKaCostItem, self.CostItemListNode, self, 0.8f);
+        }
     }
 }
---
+++ Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1ChouKaItemComponent.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
+{
+    public class UIActivityV1ChouKaItemComponent: Entity, IAwake<GameObject>
+    {
+        public GameObject GameObject;
+        public GameObject ClickBtn;
+        public GameObject NumText;
+        public GameObject RewardListNode;
+
+        public int Key;
+    }
+
+    public class UIActivityV1ChouKaItemComponentAwakeSystem: AwakeSystem<UIActivityV1ChouKaItemComponent, GameObject>
+    {
+        public override void Awake(UIActivityV1ChouKaItemComponent self, GameObject gameObject)
+        {
+            self.GameObject = gameObject;
+            ReferenceCollector rc = gameObject.GetComponent<ReferenceCollector>();
+
+            self.ClickBtn = rc.Get<GameObject>("ClickBtn");
+            self.NumText = rc.Get<GameObject>("NumText");
+            self.RewardListNode = rc.Get<GameObject>("RewardListNode");
+
+            self.ClickBtn.GetComponent<Button>().onClick.AddListener(() => { self.OnClickBtn().Coroutine(); });
+        }
+    }
+
+    public static class UIActivityV1ChouKaItemComponentSystem
+    {
+        public static void OnUpdateData(this UIActivityV1ChouKaItemComponent self, int key)
+        {
+            self.Key = key;
+            self.NumText.GetComponent<Text>().text = $"今日抽奖次数x{key}";
+            UICommonHelper.ShowItemList(ActivityConfigHelper.ChouKaNumberReward[key], self.RewardListNode, self, 0.8f);
+        }
+
+        public static async ETTask OnClickBtn(this UIActivityV1ChouKaItemComponent self)
+        {
+            if (!ActivityConfigHelper.ChouKaNumberReward.ContainsKey(self.Key))
+            {
+                return;
+            }
+
+            if (self.GetComponent<ActivityComponent>().ActivityV1Info.ChouKaNumberReward.Contains(self.Key))
+            {
+                FloatTipManager.Instance.ShowFloatTip("已经领取");
+                return;
+            }
+
+            C2M_ActivityRewardRequest request =
+                    new C2M_ActivityRewardRequest() { ActivityType = ActivityConfigHelper.ActivityV1_ChouKa, RewardId = self.Key };
+            M2C_ActivityRewardResponse response =
+                    (M2C_ActivityRewardResponse)await self.ZoneScene().GetComponent<SessionComponent>().Session.Call(request);
+
+            if (response.Error != ErrorCode.ERR_Success)
+            {
+                return;
+            }
+        }
+    }
+}
