931f18234b81ba5a73e833b84c7e30b5a1459e62
tangchunguang <394266315@qq.com>
2023-12-26 21:32:45 +0800

    no message

--- Server/Hotfix/Danger/Handler/Map/Bag/Make/C2M_MakeSelectHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/Bag/Make/C2M_MakeSelectHandler.cs
@@ -9,11 +9,12 @@ namespace ET
     {
         protected override async ETTask Run(Unit unit, C2M_MakeSelectRequest request, M2C_MakeSelectResponse response, Action reply)
         {
-            int makeType = request.Plan == 1 ? NumericType.MakeType_1 : NumericType.MakeType_2;
+            int makeTypeNumeric = request.Plan == 1 ? NumericType.MakeType_1 : NumericType.MakeType_2;
             int shulianduNumeric = request.Plan == 1 ? NumericType.MakeShuLianDu_1 : NumericType.MakeShuLianDu_2;
-            unit.GetComponent<UserInfoComponent>().ClearMakeListByType(makeType);
+            int oldMakeType = unit.GetComponent<NumericComponent>().GetAsInt(makeTypeNumeric);
+            unit.GetComponent<UserInfoComponent>().ClearMakeListByType(oldMakeType);
             unit.GetComponent<UserInfoComponent>().UserInfo.MakeList.AddRange(MakeHelper.GetInitMakeList(request.MakeType));
-            unit.GetComponent<NumericComponent>().ApplyValue( makeType, request.MakeType);
+            unit.GetComponent<NumericComponent>().ApplyValue( makeTypeNumeric, request.MakeType);
             unit.GetComponent<NumericComponent>().ApplyValue( shulianduNumeric, 0);
             unit.GetComponent<ChengJiuComponent>().OnSkillShuLianDu(0);
             response.MakeList = unit.GetComponent<UserInfoComponent>().UserInfo.MakeList;
