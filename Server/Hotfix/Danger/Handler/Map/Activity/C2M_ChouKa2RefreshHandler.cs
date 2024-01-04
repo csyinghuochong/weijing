fdaed975bc54ae5e2fb26e74c180d72d99287dfd
tangchunguang <394266315@qq.com>
2024-01-04 16:33:33 +0800

    no message

---
+++ Server/Hotfix/Danger/Handler/Map/Activity/C2M_ChouKa2RefreshHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ET
+{
+
+    [ActorMessageHandler]
+    public class C2M_ChouKa2RefreshHandler : AMActorLocationRpcHandler<Unit, C2M_ChouKa2RefreshRequest, M2C_ChouKa2RefreshResponse>
+    {
+        protected override async ETTask Run(Unit unit, C2M_ChouKa2RefreshRequest request, M2C_ChouKa2RefreshResponse response, Action reply)
+        {
+            ActivityComponent activityComponent = unit.GetComponent <ActivityComponent>();
+
+            activityComponent.ActivityV1Info.ChouKa2ItemList = ActivityConfigHelper.GetChouKa2RewardList();
+            activityComponent.ActivityV1Info.ChouKa2RewardIds.Clear();
+            response.ActivityV1Info = activityComponent.ActivityV1Info;
+
+            reply();
+            await ETTask.CompletedTask;
+        }
+    }
+}
