ed95f19bebe75f690ef1d891e746c97a3e4edf9b
FortuneCat0v0 <1142094319@qq.com>
2024-01-05 17:39:43 +0800

    任务

--- Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1TaskComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1TaskComponent.cs
@@ -1,7 +1,15 @@
-﻿namespace ET
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
 {
     public class UIActivityV1TaskComponent: Entity, IAwake
     {
+        public GameObject TaskListNode;
+        public GameObject UIActivityV1TaskItem;
+
+        public List<UIActivityV1TaskItemComponent> TaskList = new List<UIActivityV1TaskItemComponent>();
     }
 
     public class UIActivityV1TaskComponentAwake: AwakeSystem<UIActivityV1TaskComponent>
@@ -9,10 +17,67 @@
         public override void Awake(UIActivityV1TaskComponent self)
         {
             ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
+
+            self.TaskListNode = rc.Get<GameObject>("TaskListNode");
+            self.UIActivityV1TaskItem = rc.Get<GameObject>("UIActivityV1TaskItem");
+
+            self.UIActivityV1TaskItem.SetActive(false);
+
+            self.UpdateTaskCountrys();
         }
     }
 
     public static class UIActivityV1TaskComponentSystem
     {
+        public static void UpdateTaskCountrys(this UIActivityV1TaskComponent self)
+        {
+            List<TaskPro> taskPros = self.ZoneScene().GetComponent<TaskComponent>().TaskCountryList;
+
+            taskPros.Sort(delegate(TaskPro a, TaskPro b)
+            {
+                int commita = a.taskStatus == (int)TaskStatuEnum.Commited? 1 : 0;
+                int commitb = b.taskStatus == (int)TaskStatuEnum.Commited? 1 : 0;
+                int completea = a.taskStatus == (int)TaskStatuEnum.Completed? 1 : 0;
+                int completeb = b.taskStatus == (int)TaskStatuEnum.Completed? 1 : 0;
+
+                if (commita == commitb)
+                    return completeb - completea; //可以领取的在前
+                else
+                    return commitb - commita; //已经完成的在前
+            });
+
+            int number = 0;
+            for (int i = 0; i < taskPros.Count; i++)
+            {
+                TaskCountryConfig taskConfig = TaskCountryConfigCategory.Instance.Get(taskPros[i].taskID);
+                if (taskConfig.TaskType != TaskCountryType.ActivityV1)
+                {
+                    continue;
+                }
+
+                UIActivityV1TaskItemComponent ui_1 = null;
+                if (number < self.TaskList.Count)
+                {
+                    ui_1 = self.TaskList[number];
+                    ui_1.GameObject.SetActive(true);
+                }
+                else
+                {
+                    GameObject taskTypeItem = GameObject.Instantiate(self.UIActivityV1TaskItem);
+                    UICommonHelper.SetParent(taskTypeItem, self.TaskListNode);
+                    ui_1 = self.AddChild<UIActivityV1TaskItemComponent, GameObject>(taskTypeItem);
+                    self.TaskList.Add(ui_1);
+                    ui_1.GameObject.SetActive(true);
+                }
+
+                ui_1.OnUpdateData(taskPros[i]);
+                number++;
+            }
+
+            for (int k = number; k < self.TaskList.Count; k++)
+            {
+                self.TaskList[k].GameObject.SetActive(false);
+            }
+        }
     }
 }
---
+++ Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1TaskItemComponent.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
+{
+    public class UIActivityV1TaskItemComponent: Entity, IAwake<GameObject>, IDestroy
+    {
+        public GameObject ButtonComplete;
+        public GameObject ButtonReceive;
+        public GameObject TextHuoyueValue;
+        public GameObject TextTaskProgress;
+        public GameObject TextTaskDesc;
+        public GameObject TextTaskName;
+        public GameObject ImageIcon;
+        public GameObject ItemNumber;
+
+        public GameObject GameObject;
+        public TaskPro TaskPro;
+
+        public List<string> AssetPath = new List<string>();
+    }
+
+    public class UIActivityV1TaskItemComponentAwakeSystem: AwakeSystem<UIActivityV1TaskItemComponent, GameObject>
+    {
+        public override void Awake(UIActivityV1TaskItemComponent self, GameObject gameObject)
+        {
+            self.GameObject = gameObject;
+            ReferenceCollector rc = gameObject.GetComponent<ReferenceCollector>();
+
+            self.ButtonComplete = rc.Get<GameObject>("ButtonComplete");
+            self.ButtonReceive = rc.Get<GameObject>("ButtonReceive");
+            //self.ButtonReceive.GetComponent<Button>().onClick.AddListener(() => { self.OnBtn_Receive(); });
+            ButtonHelp.AddListenerEx(self.ButtonReceive, () => { self.OnBtn_Receive().Coroutine(); });
+
+            self.TextHuoyueValue = rc.Get<GameObject>("TextHuoyueValue");
+            self.TextTaskProgress = rc.Get<GameObject>("TextTaskProgress");
+            self.TextTaskDesc = rc.Get<GameObject>("TextTaskDesc");
+            self.TextTaskName = rc.Get<GameObject>("TextTaskName");
+            self.ImageIcon = rc.Get<GameObject>("ImageIcon");
+            self.ItemNumber = rc.Get<GameObject>("ItemNumber");
+        }
+    }
+
+    public class UIActivityV1TaskItemComponentDestroy: DestroySystem<UIActivityV1TaskItemComponent>
+    {
+        public override void Destroy(UIActivityV1TaskItemComponent self)
+        {
+            for (int i = 0; i < self.AssetPath.Count; i++)
+            {
+                if (!string.IsNullOrEmpty(self.AssetPath[i]))
+                {
+                    ResourcesComponent.Instance.UnLoadAsset(self.AssetPath[i]);
+                }
+            }
+
+            self.AssetPath = null;
+        }
+    }
+
+    public static class UIActivityV1TaskItemComponentSystem
+    {
+        public static void OnUpdateData(this UIActivityV1TaskItemComponent self, TaskPro taskPro)
+        {
+            self.TaskPro = taskPro;
+            TaskCountryConfig taskConfig = TaskCountryConfigCategory.Instance.Get(taskPro.taskID);
+
+            self.TextTaskName.GetComponent<Text>().text = taskConfig.TaskName;
+            self.TextTaskDesc.GetComponent<Text>().text = taskConfig.TaskDes;
+
+            taskPro.taskTargetNum_1 = taskPro.taskTargetNum_1 > taskConfig.TargetValue[0]? taskConfig.TargetValue[0] : taskPro.taskTargetNum_1;
+            self.TextTaskProgress.GetComponent<Text>().text = GameSettingLanguge.LoadLocalization("进度值") + ": " +
+                    string.Format("{0}/{1}", taskPro.taskTargetNum_1, taskConfig.TargetValue[0]);
+
+            //更新图标
+            string path = ABPathHelper.GetAtlasPath_2(ABAtlasTypes.TaskIcon, taskConfig.TaskIcon.ToString());
+            Sprite sp = ResourcesComponent.Instance.LoadAsset<Sprite>(path);
+            if (!self.AssetPath.Contains(path))
+            {
+                self.AssetPath.Add(path);
+            }
+
+            self.ImageIcon.GetComponent<Image>().sprite = sp;
+            //self.ImageIcon.GetComponent<Image>()
+
+            //更新金币
+            self.ItemNumber.GetComponent<Text>().text = " +" + taskConfig.RewardGold;
+
+            //活跃度
+            self.TextHuoyueValue.GetComponent<Text>().text = GameSettingLanguge.LoadLocalization("活跃度") + " +" + taskConfig.EveryTaskRewardNum;
+
+            self.ButtonComplete.SetActive(taskPro.taskStatus == (int)TaskStatuEnum.Commited);
+            self.ButtonReceive.SetActive(taskPro.taskStatus != (int)TaskStatuEnum.Commited);
+        }
+
+        public static async ETTask OnBtn_Receive(this UIActivityV1TaskItemComponent self)
+        {
+            if (self.TaskPro.taskStatus < (int)TaskStatuEnum.Completed)
+            {
+                FloatTipManager.Instance.ShowFloatTip("任务还没有完成！");
+                return;
+            }
+
+            if (self.TaskPro.taskStatus == (int)TaskStatuEnum.Commited)
+            {
+                FloatTipManager.Instance.ShowFloatTip("已经领取过奖励！");
+                return;
+            }
+
+            //发送奖励
+            long instanceid = self.InstanceId;
+            int errorCode = await self.ZoneScene().GetComponent<TaskComponent>().SendCommitTaskCountry(self.TaskPro.taskID);
+            if (instanceid != self.InstanceId)
+            {
+                return;
+            }
+
+            if (errorCode == ErrorCode.ERR_Success)
+            {
+                self.GetParent<UIActivityV1TaskComponent>().UpdateTaskCountrys();
+
+                //显示领取
+                self.ButtonComplete.SetActive(true);
+                self.ButtonReceive.SetActive(false);
+            }
+        }
+    }
+}
