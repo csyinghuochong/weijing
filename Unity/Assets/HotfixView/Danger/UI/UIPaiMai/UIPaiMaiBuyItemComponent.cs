5266a4a23432342aa57a1f4704b8030217ed4b1b
tangchunguang <394266315@qq.com>
2024-01-05 11:43:26 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIPaiMai/UIPaiMaiBuyItemComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIPaiMai/UIPaiMaiBuyItemComponent.cs
@@ -85,9 +85,14 @@ namespace ET
 
         public static async ETTask RequestBuy(this UIPaiMaiBuyItemComponent self)
         {
+            long instanceId = self.InstanceId;
             C2M_PaiMaiBuyRequest c2M_PaiMaiBuyRequest = new C2M_PaiMaiBuyRequest() { PaiMaiItemInfo = self.PaiMaiItemInfo };
             M2C_PaiMaiBuyResponse m2C_PaiMaiBuyResponse =
                     (M2C_PaiMaiBuyResponse)await self.DomainScene().GetComponent<SessionComponent>().Session.Call(c2M_PaiMaiBuyRequest);
+            if (instanceId != self.InstanceId)
+            {
+                return;
+            }
 
             //隐藏显示
 
