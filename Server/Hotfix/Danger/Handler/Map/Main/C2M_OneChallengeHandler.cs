8135512990c532dfcb14e5d4ce47c2d0b22c7dee
tangchunguang <394266315@qq.com>
2024-01-12 11:35:04 +0800

    no message

--- Server/Hotfix/Danger/Handler/Map/Main/C2M_OneChallengeHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/Main/C2M_OneChallengeHandler.cs
@@ -9,22 +9,29 @@ namespace ET
     {
         protected override async ETTask Run(Unit unit, C2M_OneChallengeRequest request, M2C_OneChallengeResponse response, Action reply)
         {
-            Unit other = unit.GetParent<UnitComponent>().Get(request.OtherId);
-            if (other == null)
+            if (request.Operatate == 1)  //发起挑战
             {
-                reply();
-                return;
-            }
-            if (unit.DomainScene().GetComponent<MapComponent>().SceneTypeEnum!= SceneTypeEnum.MainCityScene)
-            {
-                reply();
-                return;
-            }
+                Unit other = unit.GetParent<UnitComponent>().Get(request.OtherId);
+                if (other == null)
+                {
+                    reply();
+                    return;
+                }
+                if (unit.DomainScene().GetComponent<MapComponent>().SceneTypeEnum != SceneTypeEnum.MainCityScene)
+                {
+                    reply();
+                    return;
+                }
 
-            M2C_OneChallenge m2CCreateUnits = new M2C_OneChallenge();
-            m2CCreateUnits.OtherId = unit.Id;
-            m2CCreateUnits.OtherName = unit.GetComponent<UserInfoComponent>().UserName;
-            MessageHelper.SendToClient(other, m2CCreateUnits);
+                M2C_OneChallenge m2CCreateUnits = new M2C_OneChallenge();
+                m2CCreateUnits.OtherId = unit.Id;
+                m2CCreateUnits.OtherName = unit.GetComponent<UserInfoComponent>().UserName;
+                MessageHelper.SendToClient(other, m2CCreateUnits);
+            }
+            if (request.Operatate == 2) //迎接挑战
+            {
+
+            }
 
             reply();
             await ETTask.CompletedTask;
