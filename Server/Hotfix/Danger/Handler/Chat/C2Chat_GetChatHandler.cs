00bf6eac30ab03bf93476d8b4e1ff9b5e89a21ef
tangchunguang2 <2322657661@qq.com>
2024-01-15 12:09:52 +0800

    no message

--- Server/Hotfix/Danger/Handler/Chat/C2Chat_GetChatHandler.cs
+++ Server/Hotfix/Danger/Handler/Chat/C2Chat_GetChatHandler.cs
@@ -8,8 +8,18 @@ namespace ET
     {
         protected override async ETTask Run(ChatInfoUnit chatInfoUnit, C2Chat_GetChatRequest request, Chat2C_GetChatResponse response, Action reply)
         {
+            long serverTime = TimeHelper.ServerNow();
             ChatSceneComponent chatInfoUnitsComponent = chatInfoUnit.DomainScene().GetComponent<ChatSceneComponent>();
-            response.ChatInfos = chatInfoUnitsComponent.WordChatInfos;
+
+            for (int i = 0; i < chatInfoUnitsComponent.WordChatInfos.Count; i++)
+            {
+                ChatInfo chatInfo = chatInfoUnitsComponent.WordChatInfos[i];
+                if (serverTime - chatInfo.Time < TimeHelper.OneDay)
+                {
+                    response.ChatInfos.Add(chatInfo);
+                }
+            }
+            
 
             reply();
             await ETTask.CompletedTask;
