387c92969a79fea4d460252f95c5df4a001ac885
tangchunguang <394266315@qq.com>
2024-01-18 21:17:45 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -281,7 +281,10 @@ namespace ET
                 serverItems_1.Add(new ServerItem() { ServerId = 62, ServerIp = $"{ip}:20375", ServerName = "新年快乐", ServerOpenTime = 1703847600000, New = 0, Show = 1 });
                 serverItems_1.Add(new ServerItem() { ServerId = 63, ServerIp = $"{ip}:20415", ServerName = "万里华夏", ServerOpenTime = 1704427200000, New = 0, Show = 1 });
 
-                serverItems_1.Add(new ServerItem() { ServerId = 64, ServerIp = $"{ip}:20485", ServerName = "繁星之梦", ServerOpenTime = 1705057200000, New = 1, Show = 1 });
+                serverItems_1.Add(new ServerItem() { ServerId = 64, ServerIp = $"{ip}:20485", ServerName = "繁星之梦", ServerOpenTime = 1705057200000, New = 0, Show = 1 });
+
+                //2024/1/19 新区
+                serverItems_1.Add(new ServerItem() { ServerId = 65, ServerIp = $"{ip}:20495", ServerName = "新区新区", ServerOpenTime = 1705662000000, New = 1, Show = 1 });
             }
 
             return serverItems_1;
