2dc00fff2fb84081f6bab05af4313c0e1e69df05
FortuneCat0v0 <1142094319@qq.com>
2024-01-25 12:35:45 +0800

    .

--- Unity/Assets/Hotfix/Danger/Skill/Skill_ComTargetMove_RangDamge_5.cs
+++ Unity/Assets/Hotfix/Danger/Skill/Skill_ComTargetMove_RangDamge_5.cs
@@ -3,5 +3,26 @@
     [SkillHandler]
     public class Skill_ComTargetMove_RangDamge_5: Skill_Action_Common
     {
+        public override void OnInit(SkillInfo skillId, Unit theUnitFrom)
+        {
+            this.BaseOnInit(skillId, theUnitFrom);
+
+            this.OnExecute();
+        }
+
+        public override void OnExecute()
+        {
+            this.OnShowSkillIndicator(this.SkillInfo);
+            this.OnUpdate();
+        }
+
+        public override void OnUpdate()
+        {
+            this.BaseOnUpdate();
+        }
+
+        public override void OnFinished()
+        {
+        }
     }
 }
