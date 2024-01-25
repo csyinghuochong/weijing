cf78127700db2f079f837f4d2436bb40e49c2be7
tangchunguang <394266315@qq.com>
2024-01-25 12:06:02 +0800

    no message

--- Server/Hotfix/AppStart_Init.cs
+++ Server/Hotfix/AppStart_Init.cs
@@ -47,9 +47,9 @@ namespace ET
             Game.Scene.AddComponent<BuffDispatcherComponent>();
             Game.Scene.AddComponent<ShouJiChapterInfoComponent>();
 
-            long unitid = 1755199614916558848;
+            long unitid = DBHelper.DebugUnitId;
             // int n = (int)((unitid / 99) % 4);
-            Log.Warning($"n: {UnitIdStruct.GetUnitZone(unitid)}");
+            Log.Console($"n: {UnitIdStruct.GetUnitZone(unitid)}");
 
             switch (Game.Options.AppType)
             {
