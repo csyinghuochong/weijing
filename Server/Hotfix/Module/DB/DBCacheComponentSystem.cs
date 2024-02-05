316e124100245ffeb8759577b1da6b493f128c8a
tangchunguang <394266315@qq.com>
2024-02-05 23:27:29 +0800

    no message

--- Server/Hotfix/Module/DB/DBCacheComponentSystem.cs
+++ Server/Hotfix/Module/DB/DBCacheComponentSystem.cs
@@ -68,7 +68,7 @@ namespace ET
                 }
             }
 
-            int removeNumber = 40;
+            int removeNumber = 100;
             Log.Warning($"待移除缓存玩家: {self.DomainZone()} {self.WaitDeletUnit.Count}");
             for (int i = self.WaitDeletUnit.Count - 1; i >= 0; i--)
             {
