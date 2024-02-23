63b6233b5d6a32791efb5e21897c257884b87ddc
tangchunguang <394266315@qq.com>
2024-02-23 15:33:05 +0800

    no message

--- Unity/Assets/HotfixView/Danger/Help/IPHoneHelper.cs
+++ Unity/Assets/HotfixView/Danger/Help/IPHoneHelper.cs
@@ -59,7 +59,7 @@ namespace ET
 
         public static bool IsSimulator()
         { 
-            return IsSimulator_1() || IsSimulator_2();  
+            return IsSimulator_1() || IsSimulator_3();  
         }
 
         /// <summary>
