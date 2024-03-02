bb18dd738b02d1696e1b51735132cb31ecbceb86
tangchunguang <394266315@qq.com>
2024-03-02 20:23:47 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ConfigHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ConfigHelper.cs
@@ -25,7 +25,7 @@ namespace ET
         public static string TurtleWinNotice = "号选手获得了本次小龟大赛的最终胜利";
 
 
-        public static List<int> FunctionOpenIds = new List<int> { 1025, 1043, 1044, 1045, 1052, 1055, 1057, 1058, 1059 , 2000};
+        public static List<int> FunctionOpenIds = new List<int> { 1025, 1043, 1044, 1045, 1052, 1055, 1057, 1058, 1059 };
 
         /// <summary>
         /// 小龟说话
