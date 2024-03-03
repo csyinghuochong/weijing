f420282f8f4b9d3280c2a5441bc6813ba95277de
tangchunguang <394266315@qq.com>
2024-03-03 20:29:21 +0800

    no message

--- Server/Hotfix/Danger/Handler/Popularize/C2Popularize_ListHandler.cs
+++ Server/Hotfix/Danger/Handler/Popularize/C2Popularize_ListHandler.cs
@@ -9,53 +9,57 @@ namespace ET
     {
         protected override async ETTask Run(Scene scene, C2Popularize_ListRequest request, Popularize2C_ListResponse response, Action reply)
         {
-            DBPopularizeInfo dBPopularizeInfo = await DBHelper.GetComponentCache<DBPopularizeInfo>(scene.DomainZone(), request.ActorId);
-            if (dBPopularizeInfo == null)
+            using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.Popularize, request.ActorId))
             {
-                if (scene.GetChild<DBPopularizeInfo>(request.ActorId)!=null)
+                DBPopularizeInfo dBPopularizeInfo = await DBHelper.GetComponentCache<DBPopularizeInfo>(scene.DomainZone(), request.ActorId);
+                if (dBPopularizeInfo == null)
                 {
-                    reply();
-                    return;
+                    if (scene.GetChild<DBPopularizeInfo>(request.ActorId) != null)
+                    {
+                        reply();
+                        return;
+                    }
+
+                    dBPopularizeInfo = scene.AddChildWithId<DBPopularizeInfo>(request.ActorId);
+
+                    int newzone = scene.DomainZone();
+                    List<DBPopularizeInfo> dBPopularizeInfoList = await Game.Scene.GetComponent<DBComponent>().Query<DBPopularizeInfo>(newzone, d => d.Id > 0);
+                    int xuindex = dBPopularizeInfoList.Count + 1;
+
+                    //推广码生成规则
+                    dBPopularizeInfo.PopularizeCode = newzone * 1000000 + xuindex;
+
+                    await DBHelper.SaveComponent(scene.DomainZone(), request.ActorId, dBPopularizeInfo);
+                    dBPopularizeInfo.Dispose();
                 }
 
-                dBPopularizeInfo = scene.AddChildWithId<DBPopularizeInfo>(request.ActorId);
+                for (int i = 0; i < dBPopularizeInfo.MyPopularizeList.Count; i++)
+                {
+                    long unitid = dBPopularizeInfo.MyPopularizeList[i].UnitId;
+                    int oldZone = UnitIdStruct.GetUnitZone(unitid);
+                    int newZone = ServerHelper.GetNewServerId(oldZone);
+                    if (newZone < 5)
+                    {
+                        continue;
+                    }
 
-                int newzone = scene.DomainZone();
-                List<DBPopularizeInfo> dBPopularizeInfoList = await Game.Scene.GetComponent<DBComponent>().Query<DBPopularizeInfo>(newzone, d => d.Id > 0);
-                int xuindex = dBPopularizeInfoList.Count + 1;
+                    UserInfoComponent userInfoComponent = await DBHelper.GetComponentCache<UserInfoComponent>(newZone, unitid);
+                    if (userInfoComponent == null)
+                    {
+                        continue;
+                    }
 
-                //推广码生成规则
-                dBPopularizeInfo.PopularizeCode = newzone * 1000000 + xuindex;
+                    dBPopularizeInfo.MyPopularizeList[i].Nmae = userInfoComponent.UserInfo.Name;
+                    dBPopularizeInfo.MyPopularizeList[i].Level = userInfoComponent.UserInfo.Lv;
+                    dBPopularizeInfo.MyPopularizeList[i].Occ = userInfoComponent.UserInfo.Occ;
+                    dBPopularizeInfo.MyPopularizeList[i].OccTwo = userInfoComponent.UserInfo.OccTwo;
+                }
 
-                await DBHelper.SaveComponent(scene.DomainZone(), request.ActorId, dBPopularizeInfo);
-                dBPopularizeInfo.Dispose();
+                response.PopularizeCode = dBPopularizeInfo.PopularizeCode;
+                response.BePopularizeId = dBPopularizeInfo.BePopularizeId;
+                response.MyPopularizeList = dBPopularizeInfo.MyPopularizeList;
+
             }
-
-            for (int i = 0; i < dBPopularizeInfo.MyPopularizeList.Count; i++)
-            {
-                long unitid = dBPopularizeInfo.MyPopularizeList[i].UnitId;
-                int oldZone = UnitIdStruct.GetUnitZone(unitid);
-                int newZone = ServerHelper.GetNewServerId(oldZone);
-                if (newZone < 5)
-                {
-                    continue;
-                }
-
-                UserInfoComponent userInfoComponent = await DBHelper.GetComponentCache<UserInfoComponent>(newZone, unitid);
-                if (userInfoComponent == null)
-                {
-                    continue;
-                }
-
-                dBPopularizeInfo.MyPopularizeList[i].Nmae = userInfoComponent.UserInfo.Name;
-                dBPopularizeInfo.MyPopularizeList[i].Level = userInfoComponent.UserInfo.Lv;
-                dBPopularizeInfo.MyPopularizeList[i].Occ = userInfoComponent.UserInfo.Occ;
-                dBPopularizeInfo.MyPopularizeList[i].OccTwo = userInfoComponent.UserInfo.OccTwo;
-            }
-
-            response.PopularizeCode = dBPopularizeInfo.PopularizeCode;
-            response.BePopularizeId = dBPopularizeInfo. BePopularizeId;
-            response.MyPopularizeList = dBPopularizeInfo.MyPopularizeList;
 
             reply();
             await ETTask.CompletedTask;
--- Server/Hotfix/Danger/Handler/Popularize/C2Popularize_PlayerHandler.cs
+++ Server/Hotfix/Danger/Handler/Popularize/C2Popularize_PlayerHandler.cs
@@ -9,69 +9,73 @@ namespace ET
     {
         protected override async ETTask Run(Scene scene, C2Popularize_PlayerRequest request, Popularize2C_PlayerResponse response, Action reply)
         {
-            DBPopularizeInfo dBPopularizeInfo = await DBHelper.GetComponentCache<DBPopularizeInfo>(scene.DomainZone(), request.ActorId);
-            if (dBPopularizeInfo == null)
+            using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.Popularize, request.ActorId))
             {
-                reply();
-                return;
-            }
-            UserInfoComponent userInfoComponent = await DBHelper.GetComponentCache<UserInfoComponent>(scene.DomainZone(), request.ActorId);
-            if (userInfoComponent == null)
-            {
-                reply();
-                return;
-            }
+                DBPopularizeInfo dBPopularizeInfo = await DBHelper.GetComponentCache<DBPopularizeInfo>(scene.DomainZone(), request.ActorId);
+                if (dBPopularizeInfo == null)
+                {
+                    reply();
+                    return;
+                }
+                UserInfoComponent userInfoComponent = await DBHelper.GetComponentCache<UserInfoComponent>(scene.DomainZone(), request.ActorId);
+                if (userInfoComponent == null)
+                {
+                    reply();
+                    return;
+                }
 
-            int oldzone = (int)request.PopularizeId / 1000000;
-            int xuhao   = (int)request.PopularizeId % 1000000;
-            int newzone = ServerHelper.GetNewServerId(oldzone);
-            if (newzone < 5)
-            {
-                reply();
-                return;
-            }
-            if (newzone > ServerHelper.ServerItems.Count + 10)
-            {
-                Log.Warning($"C2Popularize_PlayerRequest: {request.PopularizeId}");
-                reply();
-                return;
-            }
+                int oldzone = (int)request.PopularizeId / 1000000;
+                int xuhao = (int)request.PopularizeId % 1000000;
+                int newzone = ServerHelper.GetNewServerId(oldzone);
+                if (newzone < 5)
+                {
+                    reply();
+                    return;
+                }
+                if (newzone > ServerHelper.ServerItems.Count + 10)
+                {
+                    Log.Warning($"C2Popularize_PlayerRequest: {request.PopularizeId}");
+                    reply();
+                    return;
+                }
 
-            List<DBPopularizeInfo> dBPopularizeInfoList = await Game.Scene.GetComponent<DBComponent>().Query<DBPopularizeInfo>(newzone, d => d.PopularizeCode == request.PopularizeId);
-            if (dBPopularizeInfoList.Count == 0)
-            {
-                response.Error = ErrorCode.ERR_PopularizeNot;
-                reply();
-                return;
-            }
+                List<DBPopularizeInfo> dBPopularizeInfoList = await Game.Scene.GetComponent<DBComponent>().Query<DBPopularizeInfo>(newzone, d => d.PopularizeCode == request.PopularizeId);
+                if (dBPopularizeInfoList.Count == 0)
+                {
+                    response.Error = ErrorCode.ERR_PopularizeNot;
+                    reply();
+                    return;
+                }
 
-            long puserid =  dBPopularizeInfoList[0].Id;
-            UserInfoComponent userInfoComponent_2 = await DBHelper.GetComponentCache<UserInfoComponent>(newzone, puserid);
-            if (userInfoComponent_2 == null)
-            {
-                response.Error = ErrorCode.ERR_PopularizeNot;
-                reply();
-                return;
-            }
-            if (userInfoComponent.UserInfo.AccInfoID == userInfoComponent_2.UserInfo.AccInfoID)
-            {
-                response.Error = ErrorCode.ERR_PopularizeThe;
-                reply();
-                return;
-            }
+                long puserid = dBPopularizeInfoList[0].Id;
+                UserInfoComponent userInfoComponent_2 = await DBHelper.GetComponentCache<UserInfoComponent>(newzone, puserid);
+                if (userInfoComponent_2 == null)
+                {
+                    response.Error = ErrorCode.ERR_PopularizeNot;
+                    reply();
+                    return;
+                }
+                if (userInfoComponent.UserInfo.AccInfoID == userInfoComponent_2.UserInfo.AccInfoID)
+                {
+                    response.Error = ErrorCode.ERR_PopularizeThe;
+                    reply();
+                    return;
+                }
 
-            if (dBPopularizeInfoList[0].MyPopularizeList.Count >= 10)
-            {
-                response.Error = ErrorCode.ERR_PopularizeMax;
-                reply();
-                return;
+                if (dBPopularizeInfoList[0].MyPopularizeList.Count >= 10)
+                {
+                    response.Error = ErrorCode.ERR_PopularizeMax;
+                    reply();
+                    return;
+                }
+
+                dBPopularizeInfoList[0].MyPopularizeList.Add(new PopularizeInfo() { UnitId = request.ActorId });
+                await DBHelper.SaveComponent(newzone, dBPopularizeInfoList[0].Id, dBPopularizeInfoList[0]);
+
+                dBPopularizeInfo.BePopularizeId = request.PopularizeId;
+                await DBHelper.SaveComponent(newzone, dBPopularizeInfo.Id, dBPopularizeInfo);
+
             }
-
-            dBPopularizeInfoList[0].MyPopularizeList.Add( new PopularizeInfo() { UnitId = request.ActorId } );
-            await DBHelper.SaveComponent(newzone, dBPopularizeInfoList[0].Id, dBPopularizeInfoList[0]);
-
-            dBPopularizeInfo.BePopularizeId = request.PopularizeId;
-            await DBHelper.SaveComponent(newzone, dBPopularizeInfo.Id, dBPopularizeInfo);
 
             reply();
         }
