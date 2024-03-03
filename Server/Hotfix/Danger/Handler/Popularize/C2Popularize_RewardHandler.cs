54554ad46247da6c95537c01959f0d37f3802d6c
tangchunguang <394266315@qq.com>
2024-03-03 18:54:56 +0800

    no message

--- Server/Hotfix/Danger/Handler/Popularize/C2Popularize_RewardHandler.cs
+++ Server/Hotfix/Danger/Handler/Popularize/C2Popularize_RewardHandler.cs
@@ -12,39 +12,43 @@ namespace ET
     {
         protected override async ETTask Run(Scene scene, C2Popularize_RewardRequest request, Popularize2C_RewardResponse response, Action reply)
         {
-            DBPopularizeInfo dBPopularizeInfo = await DBHelper.GetComponentCache<DBPopularizeInfo>(scene.DomainZone(), request.ActorId);
-            if (dBPopularizeInfo == null)
+            using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.Popularize, request.ActorId))
             {
-                reply();
-                return;
-            }
+                DBPopularizeInfo dBPopularizeInfo = await DBHelper.GetComponentCache<DBPopularizeInfo>(scene.DomainZone(), request.ActorId);
+                if (dBPopularizeInfo == null)
+                {
+                    reply();
+                    return;
+                }
 
-            for (int i = 0; i < dBPopularizeInfo.MyPopularizeList.Count; i++)
-            {
-                long unitid = dBPopularizeInfo.MyPopularizeList[i].UnitId;
-                int oldZone = UnitIdStruct.GetUnitZone(unitid);
-                int newZone = ServerHelper.GetNewServerId(oldZone);
-                if (newZone < 5)
+                for (int i = 0; i < dBPopularizeInfo.MyPopularizeList.Count; i++)
                 {
-                    continue;
+                    long unitid = dBPopularizeInfo.MyPopularizeList[i].UnitId;
+                    int oldZone = UnitIdStruct.GetUnitZone(unitid);
+                    int newZone = ServerHelper.GetNewServerId(oldZone);
+                    if (newZone < 5)
+                    {
+                        continue;
+                    }
+                    UserInfoComponent userInfoComponent = await DBHelper.GetComponentCache<UserInfoComponent>(newZone, unitid);
+                    if (userInfoComponent == null)
+                    {
+                        continue;
+                    }
+                    dBPopularizeInfo.MyPopularizeList[i].Nmae = userInfoComponent.UserInfo.Name;
+                    dBPopularizeInfo.MyPopularizeList[i].Level = userInfoComponent.UserInfo.Lv;
+                    dBPopularizeInfo.MyPopularizeList[i].Occ = userInfoComponent.UserInfo.Occ;
+                    dBPopularizeInfo.MyPopularizeList[i].OccTwo = userInfoComponent.UserInfo.OccTwo;
                 }
-                UserInfoComponent userInfoComponent = await DBHelper.GetComponentCache<UserInfoComponent>(newZone, unitid);
-                if (userInfoComponent == null)
+                List<RewardItem> rewardItems = PopularizeHelper.GetRewardList(dBPopularizeInfo.MyPopularizeList);
+                Popularize2M_RewardRequest rewardRequest = new Popularize2M_RewardRequest() { ReardList = rewardItems };
+                M2Popularize_RewardResponse reqEnter = (M2Popularize_RewardResponse)await ActorLocationSenderComponent.Instance.Call(request.ActorId, rewardRequest);
+                //(M2Popularize_RewardResponse)await MessageHelper.CallLocationActor(request.ActorId, rewardRequest);
+                if (reqEnter.Error == ErrorCode.ERR_Success)
                 {
-                    continue;
+                    await DBHelper.SaveComponent(scene.DomainZone(), request.ActorId, dBPopularizeInfo);
                 }
-                dBPopularizeInfo.MyPopularizeList[i].Nmae = userInfoComponent.UserInfo.Name;
-                dBPopularizeInfo.MyPopularizeList[i].Level = userInfoComponent.UserInfo.Lv;
-                dBPopularizeInfo.MyPopularizeList[i].Occ = userInfoComponent.UserInfo.Occ;
-                dBPopularizeInfo.MyPopularizeList[i].OccTwo = userInfoComponent.UserInfo.OccTwo;
-            }
-            List<RewardItem> rewardItems = PopularizeHelper.GetRewardList(dBPopularizeInfo.MyPopularizeList);
-            Popularize2M_RewardRequest  rewardRequest = new Popularize2M_RewardRequest() { ReardList = rewardItems };
-            M2Popularize_RewardResponse reqEnter = (M2Popularize_RewardResponse)await ActorLocationSenderComponent.Instance.Call(request.ActorId, rewardRequest);
-            //(M2Popularize_RewardResponse)await MessageHelper.CallLocationActor(request.ActorId, rewardRequest);
-            if (reqEnter.Error == ErrorCode.ERR_Success)
-            {
-                await DBHelper.SaveComponent(scene.DomainZone(), request.ActorId, dBPopularizeInfo);
+
             }
 
             reply();
