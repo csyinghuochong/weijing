5c3ee297f86db0a44a5f9306678d0ddcdb4d0c72
tangchunguang <394266315@qq.com>
2024-03-17 19:13:43 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -284,13 +284,13 @@ namespace ET
             serverItems_1.Add(new ServerItem() { ServerId = 78, ServerIp = $"{ip}:20495", ServerName = "烟雨云烟", ServerOpenTime = 1709550000000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
             serverItems_1.Add(new ServerItem() { ServerId = 79, ServerIp = $"{ip}:20515", ServerName = "九霄云外", ServerOpenTime = 1709895600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
             serverItems_1.Add(new ServerItem() { ServerId = 80, ServerIp = $"{ip}:20535", ServerName = "烽烟四起", ServerOpenTime = 1710154800000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
-
-
-            //2024/3/15 19:00 新增服务器--燃烧之地 81
             serverItems_1.Add(new ServerItem() { ServerId = 81, ServerIp = $"{ip}:20555", ServerName = "燃烧之地", ServerOpenTime = 1710500400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
+
+
+            //2024/3/18 19:00 新增服务器--新区新区 82
+            serverItems_1.Add(new ServerItem() { ServerId = 82, ServerIp = $"{ip}:20605", ServerName = "新区新区", ServerOpenTime = 1710759600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
+
             ///PlatformHelper.GetPlatformName(); 所有渠道ID定义
-
-
             List<int> allserverId = new List<int>();
             for (int i = 0; i < serverItems_1.Count; i++)
             {
