7e307dcdd7d0cf93429611089571c31c92946d94
tangchunguang <394266315@qq.com>
2024-03-18 14:18:53 +0800

    no message

--- Server/Hotfix/Demo/Account/Handler/C2A_LoginAccountHandler.cs
+++ Server/Hotfix/Demo/Account/Handler/C2A_LoginAccountHandler.cs
@@ -79,7 +79,7 @@ namespace ET
                 if (!ComHelp.RobotPassWord.Equals(request.Password))
                 {
                     //Log.Console($"客户端登录: {TimeHelper.DateTimeNow().ToString()} {session.RemoteAddress}");
-                    Log.Warning($"客户端登录:  {request.AccountName}  {session.RemoteAddress} ");
+                    Log.Warning($"客户端登录:{session.DomainZone()}   {request.AccountName}  {session.RemoteAddress} ");
                 }
 
                 //密码要md5
