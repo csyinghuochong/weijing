2b7d021d9cee375c5b04d496b9854f5bb32401b7
tangchunguang <394266315@qq.com>
2024-03-19 19:26:08 +0800

    no message

--- Unity/Assets/HotfixView/Demo/UI/UILobby/UILobbyComponentSystem.cs
+++ Unity/Assets/HotfixView/Demo/UI/UILobby/UILobbyComponentSystem.cs
@@ -140,8 +140,19 @@ namespace ET
         {
             PopupTipHelp.OpenPopupTip(self.ZoneScene(), "系统提示", "请问是否返回主界面?", ()=>
             {  //加载登录场景
-                EventType.ReturnLogin.Instance.ZoneScene = self.DomainScene();
-                Game.EventSystem.PublishClass(EventType.ReturnLogin.Instance);
+
+                Scene zoneScene = self.ZoneScene();
+                Session session = zoneScene.GetComponent<SessionComponent>().Session;
+                if (session != null && !session.IsDisposed)
+                {
+                    session.GetComponent<PingComponent>().DisconnectType = -1;
+                }
+                Game.Scene.GetComponent<SceneManagerComponent>().ChangeScene(self.ZoneScene(), (int)SceneTypeEnum.LoginScene, SceneTypeEnum.NONE, 1).Coroutine();
+                zoneScene.Dispose();
+                zoneScene = SceneFactory.CreateZoneScene(1, "Game", Game.Scene);
+
+                EventType.AppStartInitFinish.Instance.ZoneScene = zoneScene;
+                Game.EventSystem.PublishClass(EventType.AppStartInitFinish.Instance);
             }, null).Coroutine();
            
         }
