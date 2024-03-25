fc3b2aea2f51ead5836108afb0ac0da5b336c094
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2024-03-25 16:24:50 +0800

    提交配置

--- Unity/Assets/Hotfix/Danger/Common/Helper/ComHelp.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ComHelp.cs
@@ -755,21 +755,13 @@ namespace ET
         {
             if(createDay == 1)
             {
-                if(lv <= 18)
-                {
-                    return 18;
-                }
-                return 0;
-            }
-            if (createDay <= 2)
-            {
-                if (lv <= 20)
+                if(lv <= 20)
                 {
                     return 20;
                 }
                 return 0;
             }
-            if (createDay <= 3)
+            if (createDay <= 2)
             {
                 if (lv <= 22)
                 {
@@ -777,7 +769,7 @@ namespace ET
                 }
                 return 0;
             }
-            if (createDay <= 4)
+            if (createDay <= 3)
             {
                 if (lv <= 24)
                 {
@@ -785,11 +777,19 @@ namespace ET
                 }
                 return 0;
             }
-            if (createDay <= 5)
+            if (createDay <= 4)
             {
                 if (lv <= 26)
                 {
                     return 26;
+                }
+                return 0;
+            }
+            if (createDay <= 5)
+            {
+                if (lv <= 28)
+                {
+                    return 28;
                 }
                 return 0;
             }
