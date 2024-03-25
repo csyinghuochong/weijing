0516490dd17dc812eff6a925e1f148126d892d4c
tangchunguang <394266315@qq.com>
2024-03-25 11:35:07 +0800

    no message

--- start_banhao_2.bat
+++
@@ -1,3 +0,0 @@
-cd bin
-dotnet Robot.dll --AppType=Server --Console=1 --Title=Robot --StartConfig=StartConfig/BanHao --Process 2
-pause
--- start_banhao_localhost_2.bat
+++
@@ -1,3 +0,0 @@
-cd bin
-dotnet Server.dll --AppType=Server --Console=1 --Title=BanHao --StartConfig=StartConfig/BanHaoLocalhost --Process 2
-pause
