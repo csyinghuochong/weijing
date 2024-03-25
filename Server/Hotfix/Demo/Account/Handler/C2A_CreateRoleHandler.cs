97ac6c78058708ee5b2a62bfc78b3c009109aad9
tangchunguang <394266315@qq.com>
2024-03-25 14:31:14 +0800

    no message

--- Server/Hotfix/Demo/Account/Handler/C2A_CreateRoleHandler.cs
+++ Server/Hotfix/Demo/Account/Handler/C2A_CreateRoleHandler.cs
@@ -121,13 +121,13 @@ namespace ET
 						await DBHelper.AddDataComponent<DBMailInfo>(zone, userId, DBHelper.DBMailInfo);
 
 						//梦想之城 角色名 邵蓝璃  误删恢复
-						if (newAccount.Account.Equals(("13898488192")))
-						{
-							if (!newAccount.UserList.Contains((2289640847777529857)))
-							{
-								newAccount.UserList.Add(2289640847777529857);
-							}
-						}
+						// if (newAccount.Account.Equals(("13898488192")))
+						// {
+						// 	if (!newAccount.UserList.Contains((2289640847777529857)))
+						// 	{
+						// 		newAccount.UserList.Add(2289640847777529857);
+						// 	}
+						// }
 
 						//存储账号信息
 						newAccount.UserList.Add(userId);
