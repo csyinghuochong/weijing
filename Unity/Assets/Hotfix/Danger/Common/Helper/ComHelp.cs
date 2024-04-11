c35c15e3172eeb4f3f2111ac8964a61c14e5e9be
tangchunguang2 <2322657661@qq.com>
2024-04-11 21:14:41 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ComHelp.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ComHelp.cs
@@ -26,7 +26,7 @@ namespace ET
         /// </summary>
         public static List<int> InnerZoneList = new List<int>() { 202, 203 };
 
-        public const int Version = 20240403;
+        public const int Version = 20240412;
 
         public const string RobotPassWord = "et@#robot";
 
