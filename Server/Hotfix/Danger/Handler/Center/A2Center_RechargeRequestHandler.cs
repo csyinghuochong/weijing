9b60e9c566e487c366c0041ded05f916ad401f64
tangchunguang <394266315@qq.com>
2024-04-11 17:40:58 +0800

    no message

--- Server/Hotfix/Danger/Handler/Center/A2Center_RechargeRequestHandler.cs
+++ Server/Hotfix/Danger/Handler/Center/A2Center_RechargeRequestHandler.cs
@@ -9,13 +9,17 @@ namespace ET
     {
         protected override async ETTask Run(Scene scene, A2Center_RechargeRequest request, Center2A_RechargeResponse response, Action reply)
         {
-            int zone = scene.DomainZone();
-            Log.Warning($"A2Center_RechargeRequest: { scene.DomainZone()}");
-            List<DBCenterAccountInfo> resulets = await Game.Scene.GetComponent<DBComponent>().Query<DBCenterAccountInfo>(zone, d => d.Id == request.AccountId);
-            resulets[0].PlayerInfo.RechargeInfos.Add(request.RechargeInfo);
-            
-            Game.Scene.GetComponent<DBComponent>().Save<DBCenterAccountInfo>(scene.DomainZone(), resulets[0]).Coroutine();
-            reply();
+
+            using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.Recharge, request.AccountId))
+            {
+                int zone = scene.DomainZone();
+                Log.Warning($"A2Center_RechargeRequest: {scene.DomainZone()}");
+                List<DBCenterAccountInfo> resulets = await Game.Scene.GetComponent<DBComponent>().Query<DBCenterAccountInfo>(zone, d => d.Id == request.AccountId);
+                resulets[0].PlayerInfo.RechargeInfos.Add(request.RechargeInfo);
+                Game.Scene.GetComponent<DBComponent>().Save<DBCenterAccountInfo>(scene.DomainZone(), resulets[0]).Coroutine();
+                reply();
+            }
+
             await ETTask.CompletedTask;
         }
     }
