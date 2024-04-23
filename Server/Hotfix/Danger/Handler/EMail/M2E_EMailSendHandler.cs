15e3e14b852f29c43ea16bc51f6ccb1b7d612c12
tangchunguang2 <2322657661@qq.com>
2024-04-24 07:42:36 +0800

    no message

--- Server/Hotfix/Danger/Handler/EMail/M2E_EMailSendHandler.cs
+++ Server/Hotfix/Danger/Handler/EMail/M2E_EMailSendHandler.cs
@@ -7,59 +7,64 @@ namespace ET
     {
         protected override async ETTask Run(Scene scene, M2E_EMailSendRequest request, E2M_EMailSendResponse response, Action reply)
         {
-            //存储邮件
-            if (request.GetWay == ItemGetWay.RunRace)
+   
+            using (await CoroutineLockComponent.Instance.Wait(39, request.Id))
             {
-                Log.Warning($"家族争霸赛邮件1: {request.Id}");
-            }
-            if (request.GetWay == ItemGetWay.MiJingBoss)
-            {
-                Log.Warning($"世界BOSS邮件1: {request.Id}");
-            }
-            response.Error = await MailHelp.SendUserMail(scene.DomainZone(), request.Id, request.MailInfo);
 
-            if (request.GetWay == ItemGetWay.RunRace)
-            {
-                Log.Warning($"家族争霸赛邮件2: {response.Error}");
-            }
-            if (request.GetWay == ItemGetWay.MiJingBoss)
-            {
-                Log.Warning($"世界BOSS邮件2: {response.Error}");
-            }
-
-            if (response.Error != ErrorCode.ERR_Success)
-            {
-                response.Error = response.Error;
-                reply();
-                return;
-            }
-
-            long gateServerId = DBHelper.GetGateServerId(scene.DomainZone());
-            G2T_GateUnitInfoResponse g2M_UpdateUnitResponse = (G2T_GateUnitInfoResponse)await ActorMessageSenderComponent.Instance.Call
-                  (gateServerId, new T2G_GateUnitInfoRequest()
-                  {
-                      UserID = request.Id
-                  });
-
-            //在线直接推送
-            if (g2M_UpdateUnitResponse.PlayerState == (int)PlayerState.Game && g2M_UpdateUnitResponse.SessionInstanceId > 0)
-            {
-                M2C_UpdateMailInfo m2C_HorseNoticeInfo = new M2C_UpdateMailInfo();
-                MessageHelper.SendActor(g2M_UpdateUnitResponse.SessionInstanceId, m2C_HorseNoticeInfo);
-            }
-            if (g2M_UpdateUnitResponse.PlayerState == (int)PlayerState.None)
-            {
-                long dbCacheId = DBHelper.GetDbCacheId(scene.DomainZone());
-                D2G_GetComponent d2GGetUnit = (D2G_GetComponent)await ActorMessageSenderComponent.Instance.Call(dbCacheId, new G2D_GetComponent() { UnitId = request.Id, Component = DBHelper.ReddotComponent });
-                if (d2GGetUnit.Component != null)
+                //存储邮件
+                if (request.GetWay == ItemGetWay.RunRace)
                 {
-                    ReddotComponent reddotComponent = d2GGetUnit.Component as ReddotComponent;
-                    reddotComponent.AddReddont((int)ReddotType.Email);
-                    D2M_SaveComponent d2M_SaveComponent = (D2M_SaveComponent)await ActorMessageSenderComponent.Instance.Call(dbCacheId, new M2D_SaveComponent() { UnitId = request.Id, EntityByte = MongoHelper.ToBson(reddotComponent), ComponentType = DBHelper.ReddotComponent });
+                    Log.Warning($"家族争霸赛邮件1: {request.Id}");
                 }
+                if (request.GetWay == ItemGetWay.MiJingBoss)
+                {
+                    Log.Warning($"世界BOSS邮件1: {request.Id}");
+                }
+                response.Error = await MailHelp.SendUserMail(scene.DomainZone(), request.Id, request.MailInfo);
+
+                if (request.GetWay == ItemGetWay.RunRace)
+                {
+                    Log.Warning($"家族争霸赛邮件2: {response.Error}");
+                }
+                if (request.GetWay == ItemGetWay.MiJingBoss)
+                {
+                    Log.Warning($"世界BOSS邮件2: {response.Error}");
+                }
+
+                if (response.Error != ErrorCode.ERR_Success)
+                {
+                    response.Error = response.Error;
+                    reply();
+                    return;
+                }
+
+                long gateServerId = DBHelper.GetGateServerId(scene.DomainZone());
+                G2T_GateUnitInfoResponse g2M_UpdateUnitResponse = (G2T_GateUnitInfoResponse)await ActorMessageSenderComponent.Instance.Call
+                      (gateServerId, new T2G_GateUnitInfoRequest()
+                      {
+                          UserID = request.Id
+                      });
+
+                //在线直接推送
+                if (g2M_UpdateUnitResponse.PlayerState == (int)PlayerState.Game && g2M_UpdateUnitResponse.SessionInstanceId > 0)
+                {
+                    M2C_UpdateMailInfo m2C_HorseNoticeInfo = new M2C_UpdateMailInfo();
+                    MessageHelper.SendActor(g2M_UpdateUnitResponse.SessionInstanceId, m2C_HorseNoticeInfo);
+                }
+                if (g2M_UpdateUnitResponse.PlayerState == (int)PlayerState.None)
+                {
+                    long dbCacheId = DBHelper.GetDbCacheId(scene.DomainZone());
+                    D2G_GetComponent d2GGetUnit = (D2G_GetComponent)await ActorMessageSenderComponent.Instance.Call(dbCacheId, new G2D_GetComponent() { UnitId = request.Id, Component = DBHelper.ReddotComponent });
+                    if (d2GGetUnit.Component != null)
+                    {
+                        ReddotComponent reddotComponent = d2GGetUnit.Component as ReddotComponent;
+                        reddotComponent.AddReddont((int)ReddotType.Email);
+                        D2M_SaveComponent d2M_SaveComponent = (D2M_SaveComponent)await ActorMessageSenderComponent.Instance.Call(dbCacheId, new M2D_SaveComponent() { UnitId = request.Id, EntityByte = MongoHelper.ToBson(reddotComponent), ComponentType = DBHelper.ReddotComponent });
+                    }
+                }
+
+                reply();
             }
-            
-            reply();
         }
 
     }
--- Unity/Assets/Model/Module/CoroutineLock/CoroutineLockType.cs
+++ Unity/Assets/Model/Module/CoroutineLock/CoroutineLockType.cs
@@ -56,6 +56,8 @@ namespace ET
 
         public const int Chat = 38;
 
+        public const int EMail = 39;
+
         public const int BeiYong = 99;
         public const int Max = 100; // 这个必须最大
     }
