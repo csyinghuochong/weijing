5ce72364071fe56a99f0ac92806029a05d44e36c
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2024-05-23 20:44:03 +0800

    更新配置

--- Unity/Assets/HotfixView/Danger/Help/ItemViewHelp.cs
+++ Unity/Assets/HotfixView/Danger/Help/ItemViewHelp.cs
@@ -270,7 +270,7 @@ namespace ET
             { NumericType.Now_PetAllAdf, new NumericAttribute(){Name = "宠物全体魔防",Icon = string.Empty}},
             { NumericType.Now_PetAllHp, new NumericAttribute(){Name = "宠物全体生命",Icon = string.Empty}},
 
-            { NumericType.Now_PetAllMageAct, new NumericAttribute(){Name = "宠物魔法攻击",Icon = string.Empty}},
+            { NumericType.Now_PetAllMageActPro, new NumericAttribute(){Name = "宠物魔法攻击",Icon = string.Empty}},
             { NumericType.Now_PetAllActPro, new NumericAttribute(){Name = "宠物攻击",Icon = string.Empty}},
             { NumericType.Now_PetAllDefPro, new NumericAttribute(){Name = "宠物防御",Icon = string.Empty}},
             { NumericType.Now_PetAllAdfPro, new NumericAttribute(){Name = "宠物魔防",Icon = string.Empty}},
