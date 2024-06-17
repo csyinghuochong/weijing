412b0b470a6dcc0b6983261164bccf02d297a862
tangchunguang <394266315@qq.com>
2024-06-17 16:38:57 +0800

    no message

--- Server/Hotfix/Danger/Battle/Buff/RoleBuff_Attribute.cs
+++ Server/Hotfix/Danger/Battle/Buff/RoleBuff_Attribute.cs
@@ -132,7 +132,7 @@ namespace ET
                         }
                         if (NowBuffParameterType == NumericType.Now_Hp && this.NowBuffValue > 0 && this.TheUnitFrom.Type == UnitType.Player && (attackId == 2294038413269073920 || attackId == 2304469306957692928) )
                         {
-                            Log.Warning($"玩家(亞奇洛貝)对手 回血:{this.NowBuffValue}. skillid: {this.BuffData.SkillId}  buffid：{this.BuffData.BuffId}");
+                            Log.Warning($"玩家(亞奇洛貝)对手 回血:{this.NowBuffValue}. skillid: {this.BuffData.SkillId}  buffid：{this.BuffData.BuffId}  {this.TheUnitFrom.Id}");
                         }
 
                         heroCom.ApplyChange(TheUnitFrom, NumericType.Now_Hp, (long)this.NowBuffValue, 0, true, nowdamgeType);
