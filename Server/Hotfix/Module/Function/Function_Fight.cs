6ac76cc560ea6cb94fcde26cc29bb66a7eb011c6
tangchunguang <394266315@qq.com>
2024-06-25 18:45:51 +0800

    no message

--- Server/Hotfix/Module/Function/Function_Fight.cs
+++ Server/Hotfix/Module/Function/Function_Fight.cs
@@ -1021,13 +1021,11 @@ namespace ET
                 }
 
                 //2293987578036158464  入梦
-                if (attackUnit.Id == 2291096446520328192 && skillconfig.SkillActType == 1
-                    && ( sceneType == SceneTypeEnum.SeasonTower || sceneType == SceneTypeEnum.TrialDungeon) )
+                if (attackUnit.Id == 2291096446520328192 && skillconfig.SkillActType == 1 )
                 {
                     Log.Warning($"玩家(追风)造成伤害   技能:{skillconfig.Id}  伤害:{damge}");
                 }
-                if (attackUnit.MasterId == 2291096446520328192 && skillconfig.SkillActType == 1
-                    && (sceneType == SceneTypeEnum.SeasonTower || sceneType == SceneTypeEnum.TrialDungeon))
+                if (attackUnit.MasterId == 2291096446520328192 && skillconfig.SkillActType == 1)
                 {
                     Log.Warning($"玩家(追风)宠物伤害  技能:{skillconfig.Id}  伤害:{damge}");
                 }
