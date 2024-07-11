0d831af227431a18e821b58be2fca0fff57261ce
tangchunguang2 <2322657661@qq.com>
2024-07-11 21:22:34 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -320,10 +320,12 @@ namespace ET
             serverItems_1.Add(new ServerItem() { ServerId = 105, ServerIp = $"{ip}:20395", ServerName = "锦绣山河", ServerOpenTime = 1718967600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
             serverItems_1.Add(new ServerItem() { ServerId = 106, ServerIp = $"{ip}:20415", ServerName = "巨龙之魂", ServerOpenTime = 1719572400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
             serverItems_1.Add(new ServerItem() { ServerId = 107, ServerIp = $"{ip}:20455", ServerName = "海纳百川", ServerOpenTime = 1720177200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
-
-
-            //2024/07/08 19:00:00 1720436400000 新区 夏日之歌   108
             serverItems_1.Add(new ServerItem() { ServerId = 108, ServerIp = $"{ip}:20465", ServerName = "夏日之歌", ServerOpenTime = 1720436400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
+
+
+            //2024/07/12 19:00:00 1720782000000 新区 星辰大海   109
+            serverItems_1.Add(new ServerItem() { ServerId = 109, ServerIp = $"{ip}:20475", ServerName = "星辰大海", ServerOpenTime = 1720782000000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
+
 
             ///PlatformHelper.GetPlatformName(); 所有渠道ID定义
             List<int> allserverId = new List<int>();
