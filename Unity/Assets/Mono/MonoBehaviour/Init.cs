335c58eb85665e5a32bbdfc2c1e841e2b919de44
tangchunguang2 <2322657661@qq.com>
2024-08-16 10:41:38 +0800

    no message

--- Unity/Assets/Mono/MonoBehaviour/Init.cs
+++ Unity/Assets/Mono/MonoBehaviour/Init.cs
@@ -48,8 +48,8 @@ namespace ET
 		public bool Development;
 		public bool EditorMode;
 		public bool OueNetMode;
-		public int BigVersion = 20;      //17部分包含抖音sdk能力 18(模拟器检测) 19 3D视角  20 Tap实名  21tap设备Id
-		public int BigVersionIOS = 20;   //17部分包含抖音sdk能力 18(模拟器检测) 19 3D视角  20 Tap实名  21tap设备Id
+		public int BigVersion = 21;      //17部分包含抖音sdk能力 18(模拟器检测) 19 3D视角  20 Tap实名  21tap设备Id
+		public int BigVersionIOS = 21;   //17部分包含抖音sdk能力 18(模拟器检测) 19 3D视角  20 Tap实名  21tap设备Id
         public GameObject Updater;
 		public Action<int, bool> OnShareHandler;
 		public Action<string> OnGetPhoneNumHandler;
