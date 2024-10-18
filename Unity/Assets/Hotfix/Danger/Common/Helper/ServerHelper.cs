b90840a7d2d7de2e29d1f18579ae7d57e7579b77
tangchunguang <394266315@qq.com>
2024-10-18 09:58:07 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -347,8 +347,8 @@ namespace ET
             serverItems_1.Add(new ServerItem() { ServerId = 123, ServerIp = $"{ip}:20525", ServerName = "春花秋月", ServerOpenTime = 1728644400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
 
 
-            //2024/10/18 19:00:00 1729249200000 新区 新区新区 124
-            serverItems_1.Add(new ServerItem() { ServerId = 124, ServerIp = $"{ip}:20595", ServerName = "新区新区", ServerOpenTime = 1729249200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
+            //2024/10/18 19:00:00 1729249200000 新区 光明之旅 124
+            serverItems_1.Add(new ServerItem() { ServerId = 124, ServerIp = $"{ip}:20595", ServerName = "光明之旅", ServerOpenTime = 1729249200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 20001 } });
 
 
             ///PlatformHelper.GetPlatformName(); 所有渠道ID定义
