8e7118df8f5524987b23309f03dcf2fbd87d5773
tangchunguang <394266315@qq.com>
2025-03-26 21:11:41 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UISetting/UISettingGameComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UISetting/UISettingGameComponent.cs
@@ -472,7 +472,7 @@ namespace ET
             self.UpdateNoShowOther();
             self.UpdateAttackMode();
             self.UpdateAttackTarget();
-            self.TextVersion.GetComponent<Text>().text = GlobalHelp.GetBigVersion().ToString() + "  " + Application.version + "  1";
+            self.TextVersion.GetComponent<Text>().text = GlobalHelp.GetBigVersion().ToString() + "  " + Application.version + "  2";
             self.InputFieldCName.GetComponent<InputField>().text = self.UserInfoComponent.UserInfo.Name;
             Unit unit = UnitHelper.GetMyUnitFromZoneScene(self.ZoneScene());
             long lastTime = unit.GetComponent<NumericComponent>().GetAsLong(NumericType.LastGameTime);
