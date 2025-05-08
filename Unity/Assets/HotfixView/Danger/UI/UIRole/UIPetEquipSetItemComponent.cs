6c03278058521f3dfc5d058a78bbffcd2c16d668
tangchunguang <394266315@qq.com>
2025-05-08 17:40:16 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIRole/UIPetEquipSetItemComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIRole/UIPetEquipSetItemComponent.cs
@@ -67,6 +67,7 @@ namespace ET
     {
         public override void Destroy(UIPetEquipSetItemComponent self)
         {
+            TimerComponent.Instance.Remove(ref self.PointDownTime);
             for (int i = 0; i < self.AssetPath.Count; i++)
             {
                 if (!string.IsNullOrEmpty(self.AssetPath[i]))
