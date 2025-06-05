dfa90259f7fe53dc9e491e7b85ab6a022ba20e5d
tangchunguang <394266315@qq.com>
2025-06-05 20:45:27 +0800

    no message

--- start_merge_zone141.bat
+++
@@ -1,4 +0,0 @@
-::先备份数据库     要移除的区->目标区     在修改物理区
-cd binET
-dotnet Server.dll --AppType=MergeZone --Console=1 --StartConfig=StartConfig/Localhost --Title=MergeZone --Parameters=142_141 --Process 1
-pause
