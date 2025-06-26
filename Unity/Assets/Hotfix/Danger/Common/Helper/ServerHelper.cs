e426bb6b482b5b391daec025938868455e42abaa
tangchunguang2 <2322657661@qq.com>
2025-06-26 18:09:11 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -398,16 +398,14 @@ namespace ET
             serverItems_1.Add(new ServerItem() { ServerId = 157, ServerIp = $"{ip}:20645", ServerName = "神谕森林", ServerOpenTime = 1748602800000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
             serverItems_1.Add(new ServerItem() { ServerId = 158, ServerIp = $"{ip}:20375", ServerName = "泰坦神殿", ServerOpenTime = 1749207600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
             serverItems_1.Add(new ServerItem() { ServerId = 159, ServerIp = $"{ip}:20405", ServerName = "星辰之巅", ServerOpenTime = 1749812400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
-
-
-            //2025/06/20 19:00:00 1750417200000 合区 烽烟四起-春意阑珊 80/85
-            //2025/06/20 19:00:00 1750417200000 合区 吉祥如意-湖光月色 139/143
-            //2025/06/20 19:00:00 1750417200000 合区 紫禁之巅-春暖花开 147/149
-            //2025/06/20 19:00:00 1750417200000 合区 荣耀之路-龙息之境 153/154
-            //2025/06/20 19:00:00 1750417200000 新区 微风草原 160
             serverItems_1.Add(new ServerItem() { ServerId = 160, ServerIp = $"{ip}:20455", ServerName = "微风草原", ServerOpenTime = 1750417200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
+            
+            
+            //2025/06/27 19:00:00 1751022000000 合区 xx-xx xx/xx
+            //2025/06/27 19:00:00 1751022000000 新区 时光镇 161
+            serverItems_1.Add(new ServerItem() { ServerId = 161, ServerIp = $"{ip}:20445", ServerName = "时光镇", ServerOpenTime = 1751022000000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
 
-
+            
             ///PlatformHelper.GetPlatformName(); 所有渠道ID定义
             List<int> allserverId = new List<int>();
             for (int i = 0; i < serverItems_1.Count; i++)
