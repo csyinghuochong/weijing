3720c913c18413d3cdc027b474169d487837e055
tangchunguang <394266315@qq.com>
2025-07-03 17:33:37 +0800

    no message

--- Unity/Assets/Res/Effect/EffectResList/AllIn1VfxToolkit/Editor/AllIn1VfxGradientDrawer.cs
+++ Unity/Assets/Res/Effect/EffectResList/AllIn1VfxToolkit/Editor/AllIn1VfxGradientDrawer.cs
@@ -103,7 +103,7 @@ namespace AllIn1VfxToolkit
 				}
 			}
 			if (textureAsset == null) CreateTexture(path, name);
-			if (textureAsset.width != resolution) textureAsset.Resize(resolution, 1);
+			if (textureAsset.width != resolution) textureAsset.Reinitialize(resolution, 1);
 			return textureAsset;
 		}
 
