39731f1a90447d6495b32aa7d3c38ec3c750c530
FortuneCat0v0 <1142094319@qq.com>
2025-07-18 14:28:10 +0800

    .

--- Unity/Assets/HotfixView/Danger/UI/UIDonation/UIDonationShowItemComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIDonation/UIDonationShowItemComponent.cs
@@ -65,8 +65,8 @@ namespace ET
         {
             self.RankingInfo = rankingInfo;
 
-            self.Text_Combat.GetComponent<Text>().text = rankingInfo.Combat.ToString() + "金币";
-            self.Text_Level.GetComponent<Text>().text = rankingInfo.PlayerLv.ToString() + "级";
+            self.Text_Combat.GetComponent<Text>().text = rankingInfo.Combat.ToString() + GameSettingLanguge.LoadLocalization("金币");
+            self.Text_Level.GetComponent<Text>().text = string.Format(GameSettingLanguge.LoadLocalization("{0}级"), rankingInfo.PlayerLv.ToString());
             self.Text_Name.GetComponent<Text>().text = rankingInfo.PlayerName.ToString();
             self.Text_Rank.GetComponent<Text>().text = rank.ToString();
             UICommonHelper.ShowOccIcon(self.ImageHeadIcon, rankingInfo.Occ);
