57fef65f311824378ae9b3ee16e8a368c6cfc7c3
tangchunguang <394266315@qq.com>
2025-08-14 21:08:30 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/ServerHelper.cs
@@ -424,16 +424,15 @@ namespace ET
             serverItems_1.Add(new ServerItem() { ServerId = 164, ServerIp = $"{ip}:20545", ServerName = "翡翠仙境", ServerOpenTime = 1752836400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
             serverItems_1.Add(new ServerItem() { ServerId = 165, ServerIp = $"{ip}:20375", ServerName = "繁花谷", ServerOpenTime = 1753441200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
             serverItems_1.Add(new ServerItem() { ServerId = 166, ServerIp = $"{ip}:20455", ServerName = "萤火森林", ServerOpenTime = 1754046000000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
-
-
-            //2025/08/08 19:00:00 1754650800000 合区 时光镇-棉花糖 161/162
-            //2025/08/08 19:00:00 1754650800000 合区 雷霆万钧-神谕森林 155/157
-            //2025/08/08 19:00:00 1754650800000 合区 太平盛世-花满楼 121/131
-            //2025/08/08 19:00:00 1754650800000 新区 青云之巅 167
             serverItems_1.Add(new ServerItem() { ServerId = 167, ServerIp = $"{ip}:20555", ServerName = "青云之巅", ServerOpenTime = 1754650800000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 100, 20001 } });
 
 
-            ///PlatformHelper.GetPlatformName(); 所有渠道ID定义
+            //2025/08/15 19:00:00 1755255600000 合区 xx-xx xx/xx
+            //2025/08/15 19:00:00 1755255600000 新区 新区新区 168
+            serverItems_1.Add(new ServerItem() { ServerId = 168, ServerIp = $"{ip}:20505", ServerName = "新区新区", ServerOpenTime = 1755255600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
+
+
+            //PlatformHelper.GetPlatformName(); 所有渠道ID定义
             List<int> allserverId = new List<int>();
             for (int i = 0; i < serverItems_1.Count; i++)
             {
