49de0a39f4fc3dfa12163c8a45129e06ad126485
tangchunguang <394266315@qq.com>
2025-08-28 17:20:56 +0800

    no message

---
+++ Server/Hotfix/Danger/Handler/HttpHandler/HttpWeChatOAHandler.cs
@@ -0,0 +1,34 @@
+﻿using AlibabaCloud.SDK.Sample;
+using Alipay.AopSdk.Core;
+using Alipay.AopSdk.Core.Domain;
+using MongoDB.Bson.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ET
+{
+
+    //http://39.96.194.143:20008/wechatOARecvMessage
+    [HttpHandler(SceneType.AccountCenter, "/wechatOARecvMessage")]
+    public class HttpWeChatOAHandler : IHttpHandler
+    {
+
+        //private readonly string _token = "YourWeChatToken"; // 替换为你在公众号后台设置的Token
+
+        public async ETTask Handle(Entity entity, HttpListenerContext context)
+        {
+            Console.WriteLine($"HttpPhoneNumberLoginHandler: {TimeInfo.Instance.ToDateTime(TimeHelper.ServerNow())} {context.Request.RawUrl}");
+
+            
+
+
+            await ETTask.CompletedTask;
+        }
+    }
+}
