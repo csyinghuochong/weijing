983ce52756a29faedcc78170f2c9dd5cc8154dae
tangchunguang <394266315@qq.com>
2025-09-05 12:09:55 +0800

    no message

--- Server/Hotfix/Danger/Handler/Map/Tower/C2M_TowerFightBeginHandler.cs
+++ Server/Hotfix/Danger/Handler/Map/Tower/C2M_TowerFightBeginHandler.cs
@@ -8,13 +8,11 @@ namespace ET
     {
         protected override async ETTask Run(Unit unit, C2M_TowerFightBeginRequest request, M2C_TowerFightBeginResponse response, Action reply)
         {
-
-           
             if (request.SceneType == 0)
             {
                 int scenetype = unit.DomainScene().GetComponent<MapComponent>().SceneTypeEnum;
                 request.SceneType = scenetype;
-                Log.Error("C2M_TowerFightBeginRequest11 request.SceneType=null  {request.SceneType}");
+                Log.Error($"C2M_TowerFightBeginRequest11 request.SceneType=null  {request.SceneType}");
             }
 
             switch (request.SceneType)
