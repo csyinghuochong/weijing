ce3ce033078939dc5c863afc9bf3371f295e5224
tangchunguang2 <2322657661@qq.com>
2025-11-10 15:32:21 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIChengJiu/UIMagickaSlotComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIChengJiu/UIMagickaSlotComponent.cs
@@ -502,12 +502,13 @@ namespace ET
             self.Text_ZiZhiValue.GetComponent<Text>().text = $"{curex}/{magickaSlotConfig.NeedExp}";
             self.Text_ZiZhiName.GetComponent<Text>().text = magickaSlotConfig.GetDes();
 
-            BagInfo bagInfo = self.ZoneScene().GetComponent<BagComponent>().GetBagInfo(magickaSlotInfo!=null? magickaSlotInfo.BagInfoID  :0);
+            int subtype = ItemHelper.GetMagicItemSubType(self.Position);
+            BagInfo bagInfo = self.ZoneScene().GetComponent<BagComponent>().GetMagicEquipBySubType(ItemLocType.ItemLocBag, subtype, self.Position);
             if (bagInfo != null)
             {
                 self.UICommonItem.UpdateItem(bagInfo, ItemOperateEnum.None);
             }
-
+            self.UICommonItem.Image_ItemIcon.SetActive(bagInfo != null);   
             self.UICommonItem.Label_ItemName.GetComponent<Text>().text = magickaSlotConfig.GetName();
         }
 
