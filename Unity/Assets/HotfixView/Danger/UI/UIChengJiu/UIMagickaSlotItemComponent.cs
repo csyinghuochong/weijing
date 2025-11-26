c21f1c1b7a5a37ec3eb38e03660f1ff98f3e1c27
tangchunguang <394266315@qq.com>
2025-11-26 11:43:40 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIChengJiu/UIMagickaSlotItemComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIChengJiu/UIMagickaSlotItemComponent.cs
@@ -91,6 +91,7 @@ namespace ET
             }
             self.UICommonItem.GameObject.SetActive(true);
             self.UICommonItem.UpdateItem(bagInfo, ItemOperateEnum.MagicSlot);
+            self.UICommonItem.Image_Lock.SetActive(false);
         }
 
         public static void OnClickImage_Lock(this UIMagickaSlotItemComponent self)
