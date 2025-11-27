0c84626a50c3350509d823564e39e1b93c6c9b49
tangchunguang <394266315@qq.com>
2025-11-27 18:58:52 +0800

    no message

--- Unity/Assets/Model/Danger/Common/ServerHelper.cs
+++ Unity/Assets/Model/Danger/Common/ServerHelper.cs
@@ -461,8 +461,8 @@ namespace ET
 
 
                 //2025/11/28 19:00:00 1764327600000 合区 xx-xx   xx/xx
-                //2025/11/28 19:00:00 1764327600000 新区 新区新区 184
-                serverItems_1.Add(new ServerItem() { ServerId = 184, ServerIp = $"{ip}:20515", ServerName = "新区新区", ServerOpenTime = 1764327600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
+                //2025/11/28 19:00:00 1764327600000 新区 炽焰神殿 184
+                serverItems_1.Add(new ServerItem() { ServerId = 184, ServerIp = $"{ip}:20515", ServerName = "炽焰神殿", ServerOpenTime = 1764327600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
             }
 
             //google服务器
