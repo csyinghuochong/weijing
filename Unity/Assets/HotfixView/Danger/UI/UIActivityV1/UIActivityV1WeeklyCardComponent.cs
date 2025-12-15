faeab6663bc35edcc63da33167293cab89c3a691
tangchunguang <394266315@qq.com>
2025-12-15 11:47:05 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1WeeklyCardComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1WeeklyCardComponent.cs
@@ -11,6 +11,7 @@ namespace ET
         public GameObject UIActivityV1WeeklyCardItem;
         public GameObject TaskListNode;
         public GameObject BtnItemTypeSet;
+        public UIPageButtonComponent uIPageViewComponent;
     }
 
     public class UIActivityV1WeeklyCardComponentAwake : AwakeSystem<UIActivityV1WeeklyCardComponent>
@@ -19,15 +20,30 @@ namespace ET
         {
             ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
 
-            self.UpdateInfo();
+            GameObject BtnItemTypeSet = rc.Get<GameObject>("BtnItemTypeSet");
+
+            self.UIActivityV1WeeklyCardItem = rc.Get<GameObject>("UIActivityV1WeeklyCardItem");
+            self.TaskListNode = rc.Get<GameObject>("TaskListNode");
+
+            UI uiPage = self.AddChild<UI, string, GameObject>("BtnItemTypeSet", BtnItemTypeSet);
+            UIPageButtonComponent uIPageViewComponent = uiPage.AddComponent<UIPageButtonComponent>();
+            self.uIPageViewComponent = uIPageViewComponent;
+            uIPageViewComponent.SetClickHandler((int page) => { self.OnClickPageButton(page); });
+            uIPageViewComponent.OnSelectIndex(0);
         }
     }
 
     public static class UIActivityV1WeeklyCardComponentSystem
     {
+        public static void OnClickPageButton(this UIActivityV1WeeklyCardComponent self, int page)
+        {
+            self.UpdateInfo();
+        }
+
+
         public static void UpdateInfo(this UIActivityV1WeeklyCardComponent self)
         {
-            List<string> rewardlist = ActivityConfigHelper.ActivityV1WeeklyCardReward[ 0 ];
+            List<string> rewardlist = ActivityConfigHelper.ActivityV1WeeklyCardReward[ self.uIPageViewComponent.CurrentIndex + 1];
             foreach (string rewarditem in rewardlist)
             {
                 GameObject go = UnityEngine.Object.Instantiate(self.UIActivityV1WeeklyCardItem);
