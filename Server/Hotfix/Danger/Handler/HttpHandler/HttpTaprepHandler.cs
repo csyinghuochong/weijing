334be5ac1acda68d0cf663a90556b7d701883b7d
tangchunguang <394266315@qq.com>
2025-12-24 18:57:24 +0800

    no message

--- Server/Hotfix/Danger/Handler/HttpHandler/HttpTaprepHandler.cs
+++ Server/Hotfix/Danger/Handler/HttpHandler/HttpTaprepHandler.cs
@@ -69,4 +69,21 @@ namespace ET
             await ETTask.CompletedTask;
         }
     }
+
+    //配置链接
+    //http://39.96.194.143:20008/wjtapadtrack?idfa={IDFA}&time={TIME}&ip={IP}&org_id={ORG_ID}&org_name={ORG_NAME}&game_id={TAP_PROJECT_ID}&game_name={GAME_NAME}&adset_id={ADSET_ID}&adset_net={ADSET_NAME}&device_brand={DEVICE_BRAND}&device_model={DEVICE_MODEL}&creative_id={CREATIVE_ID}&conversion_type={CONVERSION_TYPE}&device={DEVICE}&OAID={OAID}&callback={DEEP_CALLBACK_URL}
+    //平台回传给广告主归因服务对应的示例：
+    //http://39.96.194.143:20008/wjtapadtrack?idfa=asedfstUfe&time=1605432321&ip=10.33.25.54&org_id=20&org_name=广告主名称&game_id=13&game_name=游戏名称&adset_id=132214&adset_net=计划名称&device_brand=苹果&device_model=iPhone3,2&creative_id=131232&conversion_type=TapTapAd&device=1&OAID=&callback=https%3A%2F%2Fdcc.iem.taptap.cn%2Fv1%2Fdeep%2Fcallback%3Ftap_track_id%3DxYTKx4rSFFWx%26tap_project_id%3D1111&tap_track_id=xYTKx4rSFFWx&tap_project_id=1111
+    //tap_track_id、tap_project_id 两个字段在下载回传服务中默认下发，在深度事件回传中回传该字段，也可以通过{TAP_TRACK_ID}和{TAP_PROJECT_ID}宏进行获取
+    [HttpHandler(SceneType.AccountCenter, "/wjtapadtrack")]
+    public class HttpTapAdTrackHandler : IHttpHandler
+    {
+        public async ETTask Handle(Entity entity, HttpListenerContext context)
+        {
+            Console.WriteLine($"HttpTapAdTrackHandler:  {context.Request.RawUrl}");
+
+            HttpServerHelper.ResponseEmpty(context);
+            await ETTask.CompletedTask;
+        }
+    }
 }
