3cab5a493fe3762a73dda7ee82f191f58a66a3a5
tangchunguang <394266315@qq.com>
2025-12-26 19:17:41 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1FeedComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1FeedComponent.cs
@@ -12,9 +12,20 @@ namespace ET
         public GameObject Feed1Btn;
         public GameObject UICommonItem2;
         public GameObject Feed2Btn;
+        public GameObject UICommonItem3;
+        public GameObject Feed3Btn;
 
         public UIItemComponent UIItem1Component;
         public UIItemComponent UIItem2Component;
+        public UIItemComponent UIItem3Component;
+
+        public GameObject ButtonStageDesc;
+        public GameObject UIStageDesc;
+        public GameObject StageButtonClose;
+        public GameObject UIStageDescList;
+        public GameObject UIStageDescItem;
+        public Text Text_Growth;
+        public Image ImageGrowthValue;
     }
 
     public class UIActivityV1FeedComponentAwake: AwakeSystem<UIActivityV1FeedComponent>
@@ -28,6 +39,8 @@ namespace ET
             self.Feed1Btn = rc.Get<GameObject>("Feed1Btn");
             self.UICommonItem2 = rc.Get<GameObject>("UICommonItem2");
             self.Feed2Btn = rc.Get<GameObject>("Feed2Btn");
+            self.UICommonItem3 = rc.Get<GameObject>("UICommonItem3");
+            self.Feed3Btn = rc.Get<GameObject>("Feed3Btn");
 
             self.Feed1Btn.GetComponent<Button>().onClick.AddListener(() =>
             {
@@ -37,16 +50,58 @@ namespace ET
             {
                 self.OnFeedBtn(ActivityConfigHelper.FeedItemReward.Keys.ToList()[1]).Coroutine();
             });
+            self.Feed3Btn.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                self.OnFeedBtn(ActivityConfigHelper.FeedItemReward.Keys.ToList()[2]).Coroutine();
+            });
 
             self.UIItem1Component = self.AddChild<UIItemComponent, GameObject>(self.UICommonItem1);
             self.UIItem2Component = self.AddChild<UIItemComponent, GameObject>(self.UICommonItem2);
+            self.UIItem3Component = self.AddChild<UIItemComponent, GameObject>(self.UICommonItem3);
 
+            self.UIStageDesc = rc.Get<GameObject>("UIStageDesc");
+            self.StageButtonClose = rc.Get<GameObject>("StageButtonClose");
+            self.StageButtonClose.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                self.UIStageDesc.SetActive(false);
+            });
+            self.UIStageDescList = rc.Get<GameObject>("UIStageDescList");
+            self.UIStageDescItem = rc.Get<GameObject>("UIStageDescItem");
+            self.UIStageDescItem.SetActive(false);
+            self.UIStageDesc.SetActive(false);
+            self.ButtonStageDesc = rc.Get<GameObject>("ButtonStageDesc");
+            self.ButtonStageDesc.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                self.UIStageDesc.SetActive(true);
+            });
+
+            self.Text_Growth = rc.Get<GameObject>("Text_Growth").GetComponent<Text>();
+            self.ImageGrowthValue = rc.Get<GameObject>("ImageGrowthValue").GetComponent<Image>();
+
+            self.InitUIStageDescList();
             self.UpdateInfo();
         }
     }
 
     public static class UIActivityV1FeedComponentSystem
     {
+
+        public static void InitUIStageDescList(this UIActivityV1FeedComponent self)
+        {
+            foreach (var costitem in ActivityConfigHelper.Feed1RewardList)
+            {
+                GameObject itemSpace = GameObject.Instantiate(self.UIStageDescItem);
+
+                Text text = itemSpace.transform.Find("TextGrowValue").GetComponent<Text>();
+                text.text = $"{costitem.Key}饱食度";
+
+                GameObject ItemRewardList = itemSpace.transform.Find("ItemRewardList").gameObject;
+                UICommonHelper.ShowItemList(costitem.Value, ItemRewardList, self);
+                itemSpace.SetActive(true);
+                UICommonHelper.SetParent(itemSpace, self.UIStageDescList);
+            }
+        }
+
         public static void UpdateInfo(this UIActivityV1FeedComponent self)
         {
             ActivityV1Info activityV1Info = self.ZoneScene().GetComponent<ActivityComponent>().ActivityV1Info;
@@ -67,6 +122,15 @@ namespace ET
             self.UIItem2Component.Label_ItemNum.GetComponent<Text>().text = $"{havedNum}/1";
             self.UIItem2Component.Label_ItemNum.GetComponent<Text>().color =
                     havedNum >= 1? new Color(0, 1, 0) : new Color(245f / 255f, 43f / 255f, 96f / 255f);
+
+            self.UIItem3Component.UpdateItem(new BagInfo() { ItemID = items[2] }, ItemOperateEnum.None);
+            havedNum = (int)bagComponent.GetItemNumber(items[2]);
+            self.UIItem3Component.Label_ItemNum.GetComponent<Text>().text = $"{havedNum}/1";
+            self.UIItem3Component.Label_ItemNum.GetComponent<Text>().color =
+                    havedNum >= 1 ? new Color(0, 1, 0) : new Color(245f / 255f, 43f / 255f, 96f / 255f);
+
+
+            self.Text_Growth.text = activityV1Info.BaoShiDu.ToString();
         }
 
         public static async ETTask OnFeedBtn(this UIActivityV1FeedComponent self, int itemId)
