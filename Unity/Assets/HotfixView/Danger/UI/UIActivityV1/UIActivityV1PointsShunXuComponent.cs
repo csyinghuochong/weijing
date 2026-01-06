259dcfb29aaf8cd834e02fd3150fae4e529b70a1
tangchunguang <394266315@qq.com>
2026-01-06 19:31:36 +0800

    no message

---
+++ Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1PointsShunXuComponent.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
+{
+    public class UIActivityV1PointsShunXuComponent : Entity, IAwake
+    {
+        public GameObject Text_CurWeek;
+        public GameObject UIActivityV1PointsListNode;
+        public GameObject UIActivityV1PointsShunXuItem;
+        public GameObject ConsumeNumText;
+    }
+
+    public class UIActivityV1PointsShunXuComponentAwake : AwakeSystem<UIActivityV1PointsShunXuComponent>
+    {
+        public override void Awake(UIActivityV1PointsShunXuComponent self)
+        {
+            ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
+            self.UIActivityV1PointsListNode = rc.Get<GameObject>("UIActivityV1PointsListNode");
+            self.UIActivityV1PointsShunXuItem = rc.Get<GameObject>("UIActivityV1PointsShunXuItem");
+            self.ConsumeNumText = rc.Get<GameObject>("ConsumeNumText");
+            self.Text_CurWeek = rc.Get<GameObject>("Text_CurWeek");
+
+            self.UIActivityV1PointsShunXuItem.SetActive(false);
+            self.Text_CurWeek.GetComponent<Text>().text = UICommonHelper.GetCurrentWeekRange();
+
+            self.GetParent<UI>().OnUpdateUI = () => { self.OnUpdateUI(); };
+
+            self.GetInfo().Coroutine();
+        }
+    }
+
+    public static class UIActivityV1PointsShunXuComponentSystem
+    {
+        public static void OnUpdateUI(this UIActivityV1PointsShunXuComponent self)
+        {
+            self.OnRecvHandler();
+        }
+
+
+        public static async ETTask GetInfo(this UIActivityV1PointsShunXuComponent self)
+        {
+            C2M_ActivityInfoRequest request = new C2M_ActivityInfoRequest();
+            M2C_ActivityInfoResponse response =
+                    (M2C_ActivityInfoResponse)await self.ZoneScene().GetComponent<SessionComponent>().Session.Call(request);
+            self.ZoneScene().GetComponent<ActivityComponent>().ActivityV1Info = response.ActivityV1Info;
+
+            self.InitInfo();
+        }
+
+        public static void InitInfo(this UIActivityV1PointsShunXuComponent self)
+        {
+            foreach (int key in ActivityConfigHelper.PointsRewardList.Keys)
+            {
+                //GameObject go = UnityEngine.Object.Instantiate(self.UIActivityV1PointsShunXuItem);
+                //UIActivityV1PointsShunXuItemComponent component = self.AddChild<UIActivityV1PointsShunXuItemComponent, GameObject>(go);
+                //component.OnUpdateData(key, self.OnRecvHandler);
+                //UICommonHelper.SetParent(go, self.UIActivityV1PointsListNode);
+                //go.SetActive(true);
+            }
+
+            self.OnRecvHandler();
+        }
+
+        public static void OnRecvHandler(this UIActivityV1PointsShunXuComponent self)
+        {
+            int points = (int)(self.ZoneScene().GetComponent<UserInfoComponent>().UserInfo.V1TotalPoints);
+
+            self.ConsumeNumText.GetComponent<Text>().text =
+                   string.Format(GameSettingLanguge.LoadLocalization("{0}积分"), points);
+        }
+    }
+}
---
+++ Unity/Assets/HotfixView/Danger/UI/UIActivityV1/UIActivityV1PointsShunXuItemComponent.cs
@@ -0,0 +1,110 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
+{
+    public class UIActivityV1PointsShunXuItemComponent : Entity, IAwake<GameObject>
+    {
+        public GameObject GameObject;
+        public GameObject ConsumeNumText;
+        public GameObject RewardListNode;
+        public GameObject ReceiveBtn;
+        public GameObject ReceivedImg;
+        public Action OnRecvHandler;
+
+        public int Key;
+    }
+
+    public class UIActivityV1PointsShunXuItemComponentAwake : AwakeSystem<UIActivityV1PointsShunXuItemComponent, GameObject>
+    {
+        public override void Awake(UIActivityV1PointsShunXuItemComponent self, GameObject gameObject)
+        {
+            self.GameObject = gameObject;
+            ReferenceCollector rc = gameObject.GetComponent<ReferenceCollector>();
+
+            self.ConsumeNumText = rc.Get<GameObject>("ConsumeNumText");
+            self.RewardListNode = rc.Get<GameObject>("RewardListNode");
+            self.ReceiveBtn = rc.Get<GameObject>("ReceiveBtn");
+            self.ReceivedImg = rc.Get<GameObject>("ReceivedImg");
+
+            self.ReceivedImg.SetActive(false);
+            self.ReceiveBtn.GetComponent<Button>().onClick.AddListener(() => { self.OnReceiveBtn().Coroutine(); });
+        }
+    }
+
+    public static class UIActivityV1PointsShunXuItemComponentSystem
+    {
+        public static void OnUpdateData(this UIActivityV1PointsShunXuItemComponent self, int key, Action action)
+        {
+            self.Key = key;
+            self.OnRecvHandler = action;
+            self.ConsumeNumText.GetComponent<Text>().text = string.Format(GameSettingLanguge.LoadLocalization("{0}积分"), self.Key);
+            UICommonHelper.DestoryChild(self.RewardListNode);
+            UICommonHelper.ShowItemList(ActivityConfigHelper.PointsRewardList[key], self.RewardListNode, self, 1f);
+
+            ActivityV1Info activityV1Info = self.ZoneScene().GetComponent<ActivityComponent>().ActivityV1Info;
+            if (activityV1Info.PointsReward.Contains(self.Key))
+            {
+                self.ReceiveBtn.SetActive(false);
+                self.ReceivedImg.SetActive(true);
+            }
+        }
+
+        public static async ETTask OnReceiveBtn(this UIActivityV1PointsShunXuItemComponent self)
+        {
+            if (self.ZoneScene().GetComponent<BagComponent>().GetBagLeftCell() < 1)
+            {
+                FloatTipManager.Instance.ShowFloatTip(GameSettingLanguge.LoadLocalization("背包空间不足"));
+                return;
+            }
+
+            string rewarditem = ActivityConfigHelper.PointsRewardList[self.Key];
+            int needcell = ItemHelper.GetNeedCell(rewarditem);
+            Log.ILog.Debug($"needcell:{needcell}");
+            if (self.ZoneScene().GetComponent<BagComponent>().GetBagLeftCell() < needcell)
+            {
+                FloatTipManager.Instance.ShowFloatTip(GameSettingLanguge.LoadLocalization("背包空间不足"));
+                return;
+            }
+
+            if (!ActivityConfigHelper.PointsRewardList.ContainsKey(self.Key))
+            {
+                return;
+            }
+
+            ActivityV1Info activityV1Info = self.ZoneScene().GetComponent<ActivityComponent>().ActivityV1Info;
+            if (activityV1Info.PointsReward.Contains(self.Key))
+            {
+                FloatTipManager.Instance.ShowFloatTip(GameSettingLanguge.LoadLocalization("已经领取"));
+                return;
+            }
+
+            int points = (int)(self.ZoneScene().GetComponent<UserInfoComponent>().UserInfo.V1TotalPoints);
+
+            if (points < self.Key)
+            {
+                FloatTipManager.Instance.ShowFloatTip(GameSettingLanguge.LoadLocalization("未达到条件"));
+                return;
+            }
+
+            C2M_ActivityRewardRequest request = new C2M_ActivityRewardRequest()
+            {
+                ActivityType = ActivityConfigHelper.ActivityV1_Points,
+                RewardId = self.Key
+            };
+            M2C_ActivityRewardResponse response =
+                    (M2C_ActivityRewardResponse)await self.ZoneScene().GetComponent<SessionComponent>().Session.Call(request);
+            self.ZoneScene().GetComponent<ActivityComponent>().ActivityV1Info.PointsReward.Add(self.Key);
+
+            if (response.Error != ErrorCode.ERR_Success)
+            {
+                return;
+            }
+
+            self.ReceiveBtn.SetActive(false);
+            self.ReceivedImg.SetActive(true);
+            self.OnRecvHandler?.Invoke();
+        }
+    }
+}
