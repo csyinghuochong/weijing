ae61fa0d46d8a331427a9838057fc5129e8e7dea
tangchunguang <394266315@qq.com>
2026-01-14 14:20:56 +0800

    no message

--- Server/Hotfix/Danger/Handler/EMail/G2Mail_EnterMailHandler.cs
+++ Server/Hotfix/Danger/Handler/EMail/G2Mail_EnterMailHandler.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Net;
 
 namespace ET
 {
-    [ActorMessageHandler]
+
+    [HttpHandler(SceneType.AccountCenter, "/wjtapconsolen")]
+    public class HttpTapConsolenHandler : IHttpHandler
+    {
+        public async ETTask Handle(Entity entity, HttpListenerContext context)
+        {
+            System.Collections.Specialized.NameValueCollection queryString = context.Request.QueryString;
+            string console = queryString["consolen"] ?? string.Empty;
+            string lognname = queryString["lognname"] ?? string.Empty;
+            if (!string.IsNullOrEmpty(console))
+            {
+                LogHelper.NoticeInfo(console, lognname);
+            }
+            HttpServerHelper.ResponseEmpty(context);
+            await ETTask.CompletedTask;
+        }
+    }
+
+        [ActorMessageHandler]
     public class G2Mail_EnterMailHandler : AMActorRpcHandler<Scene, G2Mail_EnterMail, Mail2G_EnterMail>
     {
         protected override async ETTask Run(Scene scene, G2Mail_EnterMail request, Mail2G_EnterMail response, Action reply)
