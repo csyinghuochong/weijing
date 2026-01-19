47c08a0d7c3766292a05bbcc3c3def3dab1ea3cf
tangchunguang <394266315@qq.com>
2026-01-19 17:19:08 +0800

    no message

--- Server/Hotfix/Danger/Custom/Player_OnPlayerDisconnect.cs
+++ Server/Hotfix/Danger/Custom/Player_OnPlayerDisconnect.cs
@@ -14,26 +14,7 @@
                 //动态删除副本
                 TransferHelper.NoticeFubenCenter(scene, 2).Coroutine();
                 scene.Dispose();
-            }
-            if (sceneTypeEnum == (int)SceneTypeEnum.TeamDungeon)
-            {
-                TeamSceneComponent teamSceneComponent = scene.GetParent<TeamSceneComponent>();
-                teamSceneComponent.OnUnitDisconnect(scene, userId);
-            }
-            if (sceneTypeEnum == (int)SceneTypeEnum.Arena)
-            {
-                ArenaDungeonComponent areneSceneComponent = scene.GetComponent<ArenaDungeonComponent>();
-                areneSceneComponent.OnUnitDisconnect(userId);
-            }
-            if (sceneTypeEnum == (int)SceneTypeEnum.JiaYuan)
-            {
-                JiaYuanSceneComponent jiayuanSceneComponent = scene.GetParent<JiaYuanSceneComponent>();
-                jiayuanSceneComponent.OnUnitLeave(scene);
-            }
-            if (sceneTypeEnum == (int)SceneTypeEnum.OneChallenge)
-            {
-                OneChallengeDungeonComponent jiayuanSceneComponent = scene.GetParent<OneChallengeDungeonComponent>();
-                jiayuanSceneComponent.OnUnitLeave(scene);
+                return;
             }
         }
 
--- Server/Hotfix/Danger/Custom/Player_OnReturnMainCity.cs
+++ Server/Hotfix/Danger/Custom/Player_OnReturnMainCity.cs
@@ -17,26 +17,7 @@
             {
                 TransferHelper.NoticeFubenCenter(scene, 2).Coroutine();
                 scene.Dispose();
-            }
-            if (sceneTypeEnum == SceneTypeEnum.TeamDungeon)
-            {
-                TeamSceneComponent teamSceneComponent = scene.GetParent<TeamSceneComponent>();
-                teamSceneComponent.OnUnitReturn(scene, userId);
-            }
-            if (sceneTypeEnum == (int)SceneTypeEnum.Arena)
-            {
-                ArenaDungeonComponent areneSceneComponent = scene.GetComponent<ArenaDungeonComponent>();
-                areneSceneComponent.OnUnitDisconnect(userId);
-            }
-            if (sceneTypeEnum == SceneTypeEnum.JiaYuan)
-            {
-                JiaYuanSceneComponent jiayuanSceneComponent = scene.GetParent<JiaYuanSceneComponent>();
-                jiayuanSceneComponent.OnUnitLeave(scene);
-            }
-            if (sceneTypeEnum == (int)SceneTypeEnum.OneChallenge)
-            {
-                OneChallengeDungeonComponent jiayuanSceneComponent = scene.GetParent<OneChallengeDungeonComponent>();
-                jiayuanSceneComponent.OnUnitLeave(scene);
+                return;
             }
         }
     }
