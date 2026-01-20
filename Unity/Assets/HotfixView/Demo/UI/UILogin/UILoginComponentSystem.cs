7ae1b069f6c79a3568ff41c0d9f72b01a653865d
tangchunguang <394266315@qq.com>
2026-01-20 15:39:30 +0800

    no message

--- Unity/Assets/HotfixView/Demo/UI/UILogin/UILoginComponentSystem.cs
+++ Unity/Assets/HotfixView/Demo/UI/UILogin/UILoginComponentSystem.cs
@@ -281,10 +281,6 @@ namespace ET
 				//self.TestTapHttp_2().Coroutine();
 #endif
 
-				if (self.Account.GetComponent<InputField>().text == "qq1DCADAC180C577AEDE05D15B788AE770")
-				{
-					//SettingHelper.ClintFindPath = true;
-				}
                 Log.ILog.Debug($"GetBigVersion.:{GlobalHelp.GetBigVersion()}  GetPlatform:{GlobalHelp.GetPlatform()} IsEditorMode:{GlobalHelp.IsEditorMode}");
 
                 if (GlobalHelp.GetBigVersion() >= 21
