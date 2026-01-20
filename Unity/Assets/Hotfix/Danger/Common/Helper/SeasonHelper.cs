6d47595e88c7170aa31acb74d77f35f596d9fa19
tangchunguang <394266315@qq.com>
2026-01-20 11:27:54 +0800

    no message

--- Unity/Assets/Hotfix/Danger/Common/Helper/SeasonHelper.cs
+++ Unity/Assets/Hotfix/Danger/Common/Helper/SeasonHelper.cs
@@ -103,6 +103,7 @@ namespace ET
             new KeyValuePairLong(){ KeyId = 5, Value = 1746028800001, Value2 = 1753977600000 },
             new KeyValuePairLong(){ KeyId = 6, Value = 1753977600001, Value2 = 1761926400000 },
             new KeyValuePairLong(){ KeyId = 7, Value = 1761926400001, Value2 = 1769875200000 },
+            new KeyValuePairLong(){ KeyId = 8, Value = 1769875200001, Value2 = 1777564800000 },
         };
 
         /// <summary>
