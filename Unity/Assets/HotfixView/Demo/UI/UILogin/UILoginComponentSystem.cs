b62a9f6e1b8a666e6af90326029c7c5d8555796f
tangchunguang <394266315@qq.com>
2026-01-23 18:30:47 +0800

    no message

--- Unity/Assets/HotfixView/Demo/UI/UILogin/UILoginComponentSystem.cs
+++ Unity/Assets/HotfixView/Demo/UI/UILogin/UILoginComponentSystem.cs
@@ -671,7 +671,7 @@ namespace ET
                     self.ThirdLoginBg.SetActive(false);
 #if !UNITY_EDITOR
 					EventType.QuDaoLoginRequest.Instance.ZoneScene = self.ZoneScene();
-                    EventType.QuDaoLoginRequest.Instance.AccesstokenHandler = (string token, string uid) => { self.OnRecvQuDaoUid(token, uid).Coroutine(); };
+                    //EventType.QuDaoLoginRequest.Instance.AccesstokenHandler = (string token, string uid) => { self.OnRecvQuDaoUid(token, uid).Coroutine(); };
                     EventSystem.Instance.PublishClass(EventType.QuDaoLoginRequest.Instance);
 #endif
                     break;
@@ -682,7 +682,7 @@ namespace ET
 						Log.ILog.Debug($"GlobalHelp.GetPlatform() == 5");
 #if !UNITY_EDITOR
 					EventType.TikTokGetAccesstoken.Instance.ZoneScene = self.ZoneScene();
-                    EventType.TikTokGetAccesstoken.Instance.AccesstokenHandler = (string text) => { self.OnRecvTikTokAccesstoken(text).Coroutine(); };
+                    //EventType.TikTokGetAccesstoken.Instance.AccesstokenHandler = (string text) => { self.OnRecvTikTokAccesstoken(text).Coroutine(); };
                     EventSystem.Instance.PublishClass(EventType.TikTokGetAccesstoken.Instance);
 #endif
 					}
@@ -691,7 +691,7 @@ namespace ET
 #if !UNITY_EDITOR
                         Log.ILog.Debug($"GlobalHelp.GetPlatform() == 8");
                         EventType.TikTokGetAuthorizeCode.Instance.ZoneScene = self.ZoneScene();
-                        EventType.TikTokGetAuthorizeCode.Instance.TikTokAuthorizeHandler = (string text1) => { self.OnTikTokAuthorizeHandler(text1).Coroutine(); };
+                        //EventType.TikTokGetAuthorizeCode.Instance.TikTokAuthorizeHandler = (string text1) => { self.OnTikTokAuthorizeHandler(text1).Coroutine(); };
                         EventSystem.Instance.PublishClass(EventType.TikTokGetAuthorizeCode.Instance);
 #endif
                     }
