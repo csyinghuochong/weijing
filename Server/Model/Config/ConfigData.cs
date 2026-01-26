7b75100c40ca9a8eef273174906714108d5b30a9
tangchunguang <394266315@qq.com>
2026-01-26 16:43:27 +0800

    no message

--- Server/Model/Config/ConfigData.cs
+++ Server/Model/Config/ConfigData.cs
@@ -21,7 +21,7 @@ namespace ET
 
         public static Dictionary<int , ServerInfo> ServerInfoList = new Dictionary<int , ServerInfo>();
 
-        public static List<int> FunctionOpenIds = new List<int> { 1025, 1043, 1044, 1045, 1052, 1055, 1057, 1058, 1059 };
+        public static List<int> FunctionOpenIds = new List<int> { 1025, 1043, 1044, 1045, 1052, 1055, 1057, 1058, 1059, 2000 };
 
 
         //公众平台上开发者设置的token, appID, EncodingAESKey
