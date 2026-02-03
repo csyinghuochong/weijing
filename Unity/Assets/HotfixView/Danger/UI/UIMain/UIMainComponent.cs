4f4cc751d244e404657303c148796aa7a24e8962
tangchunguang <394266315@qq.com>
2026-02-03 18:47:35 +0800

    no message

--- Unity/Assets/HotfixView/Danger/UI/UIMain/UIMainComponent.cs
+++ Unity/Assets/HotfixView/Danger/UI/UIMain/UIMainComponent.cs
@@ -2665,9 +2665,9 @@ namespace ET
 
             //先锋一区测试
             AccountInfoComponent accountInfoComponent = self.ZoneScene().GetComponent<AccountInfoComponent>();
-            SettingHelper.ClintFindPath = GlobalHelp.GetBigVersion() >= 26 && sceneTypeEnum == SceneTypeEnum.LocalDungeon 
-                && accountInfoComponent.ServerId == 5 && GMHelp.GmAccount.Contains(accountInfoComponent.Account)
-                && accountInfoComponent.ClintFindPath == 1;
+            SettingHelper.ClintFindPath = GlobalHelp.GetBigVersion() >= 26 && sceneTypeEnum == SceneTypeEnum.LocalDungeon && accountInfoComponent.ClintFindPath == 1;
+            //&& accountInfoComponent.ServerId == 5 && GMHelp.GmAccount.Contains(accountInfoComponent.Account)
+
         }
 
         public static void OnButtonGoToOperate(this UIMainComponent self)
