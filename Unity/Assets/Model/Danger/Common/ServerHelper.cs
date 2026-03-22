a02fffce52eac45d1bb26f1d8383a40bce75913c
tangchunguang2 <2322657661@qq.com>
2026-03-22 11:25:32 +0800

    no message

--- Unity/Assets/Model/Danger/Common/ServerHelper.cs
+++ Unity/Assets/Model/Danger/Common/ServerHelper.cs
@@ -483,13 +483,14 @@ namespace ET
                 serverItems_1.Add(new ServerItem() { ServerId = 197, ServerIp = $"{ip}:20655", ServerName = "御剑天涯", ServerOpenTime = 1772190000000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
                 serverItems_1.Add(new ServerItem() { ServerId = 198, ServerIp = $"{ip}:20665", ServerName = "马踏山河", ServerOpenTime = 1772794800000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
                 serverItems_1.Add(new ServerItem() { ServerId = 199, ServerIp = $"{ip}:20445", ServerName = "烽火逐鹿", ServerOpenTime = 1773399600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
-
-
-                //2026/03/20 19:00:00 1774004400000 合区 xx-xx xx/xx
-                //2026/03/20 19:00:00 1774004400000 新区 梦幻湖畔 200
-                //201-210为特殊区服
                 serverItems_1.Add(new ServerItem() { ServerId = 200, ServerIp = $"{ip}:20415", ServerName = "梦幻湖畔", ServerOpenTime = 1774004400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
-                //serverItems_1.Add(new ServerItem() { ServerId = 211, ServerIp = $"{ip}:20495", ServerName = "新区新区", ServerOpenTime = 1774609200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
+
+
+
+                //2026/03/27 19:00:00 1774609200000 合区 xx-xx xx/xx
+                //2026/03/27 19:00:00 1774609200000 新区 新区新区 211
+                //201-210为特殊区服
+                serverItems_1.Add(new ServerItem() { ServerId = 211, ServerIp = $"{ip}:20495", ServerName = "新区新区", ServerOpenTime = 1774609200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
             }
 
             //google服务器
