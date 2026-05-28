e5c82dfb7e42ea1f7e5cf87839e9f28db647af91
tangchunguang2 <2322657661@qq.com>
2026-05-28 19:11:17 +0800

    no message

--- Unity/Assets/Model/Danger/Common/ServerHelper.cs
+++ Unity/Assets/Model/Danger/Common/ServerHelper.cs
@@ -499,9 +499,9 @@ namespace ET
 
 
                 //2026/05/29 19:00:00 1780052400000 合区 xx - xx xx/xx
-                //2026/05/29 19:00:00 1780052400000 新区 新区新区 220
+                //2026/05/29 19:00:00 1780052400000 新区 九天揽星 220
                 //201-210为特殊区服
-                serverItems_1.Add(new ServerItem() { ServerId = 220, ServerIp = $"{ip}:20525", ServerName = "新区新区", ServerOpenTime = 1780052400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
+                serverItems_1.Add(new ServerItem() { ServerId = 220, ServerIp = $"{ip}:20525", ServerName = "九天揽星", ServerOpenTime = 1780052400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
             }
 
             //google服务器
