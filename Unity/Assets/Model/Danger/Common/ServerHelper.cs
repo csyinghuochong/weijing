ef9eb31f51ca252211739c83d9706aed344cd0bc
tangchunguang2 <2322657661@qq.com>
2026-06-04 19:55:51 +0800

    no message

--- Unity/Assets/Model/Danger/Common/ServerHelper.cs
+++ Unity/Assets/Model/Danger/Common/ServerHelper.cs
@@ -496,15 +496,13 @@ namespace ET
                 serverItems_1.Add(new ServerItem() { ServerId = 217, ServerIp = $"{ip}:20655", ServerName = "惊鸿", ServerOpenTime = 1778238000000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
                 serverItems_1.Add(new ServerItem() { ServerId = 218, ServerIp = $"{ip}:20445", ServerName = "观星台", ServerOpenTime = 1778842800000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
                 serverItems_1.Add(new ServerItem() { ServerId = 219, ServerIp = $"{ip}:20495", ServerName = "星月同辉", ServerOpenTime = 1779426000000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
-
-
-                
                 serverItems_1.Add(new ServerItem() { ServerId = 220, ServerIp = $"{ip}:20525", ServerName = "九天揽星", ServerOpenTime = 1780052400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
 
-                //2026/05/29 19:00:00 1780052400000 合区 xx - xx xx/xx
-                //2026/05/29 19:00:00 1780052400000 新区 九天揽星 220
-                //201-210为特殊区服
 
+                //2026/06/05 19:00:00 1780657200000 合区 xx - xx xx/xx
+                //2026/06/05 19:00:00 1780657200000 新区 世外桃源 221
+                //201-210为特殊区服
+                serverItems_1.Add(new ServerItem() { ServerId = 221, ServerIp = $"{ip}:20555", ServerName = "世外桃源", ServerOpenTime = 1780657200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
             }
 
             //google服务器
