39b377417b34d47ef7c6d3654601f2128a3c7f3f
tangchunguang2 <2322657661@qq.com>
2026-06-25 17:26:32 +0800

    no message

--- Unity/Assets/Model/Danger/Common/ServerHelper.cs
+++ Unity/Assets/Model/Danger/Common/ServerHelper.cs
@@ -499,13 +499,13 @@ namespace ET
                 serverItems_1.Add(new ServerItem() { ServerId = 220, ServerIp = $"{ip}:20525", ServerName = "九天揽星", ServerOpenTime = 1780052400000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
                 serverItems_1.Add(new ServerItem() { ServerId = 221, ServerIp = $"{ip}:20555", ServerName = "世外桃源", ServerOpenTime = 1780657200000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
                 serverItems_1.Add(new ServerItem() { ServerId = 222, ServerIp = $"{ip}:20615", ServerName = "绿茵世界", ServerOpenTime = 1781262000000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
-
-
-                //2026/06/19 19:00:00 1781866800000 合区 xx-xx    xx/xx
-                //2026/06/19 19:00:00 1781866800000 新区 飞龙在天 223
-                //201-210为特殊区服
                 serverItems_1.Add(new ServerItem() { ServerId = 223, ServerIp = $"{ip}:20545", ServerName = "飞龙在天", ServerOpenTime = 1781866800000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
 
+
+                //2026/06/26 19:00:00 1782471600000 合区 xx-xx    xx/xx
+                //2026/06/26 19:00:00 1782471600000 新区 永恒秘境 224
+                //201-210为特殊区服
+                serverItems_1.Add(new ServerItem() { ServerId = 224, ServerIp = $"{ip}:20545", ServerName = "永恒秘境", ServerOpenTime = 1782471600000, New = 0, Show = 1, PlatformList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 8, 100, 20001 } });
             }
 
             //google服务器
